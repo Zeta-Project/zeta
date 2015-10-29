3406a5705c158f4d9a85f8849b87c921bbdafb16
Tobias Droth <todroth@htwg-konstanz.de>
2015-10-29 10:47:03 +0100

    Hilfsmethoden zum Hinzufügen, Entfernen und Ändern von Attributen im Graph fertiggestellt. Das Hinzufügen/Entfernen/Ändern an sich muss noch implementiert werden.

--- server/app/modigen/util/graph/MetamodelGraphDiff.scala
+++ server/app/modigen/util/graph/MetamodelGraphDiff.scala
@@ -1,37 +1,10 @@
 package modigen.util.graph
 
 import models.MetaModelData
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.{JsArray, JsObject, Json}
 
 import scala.collection.Set
 
-/*
- * Die Klasse kuemmert sich um den Fall, dass Elemente ausserhalb des grafischen Metamodell-Editors zum Metamodell
- * hinzugefuegt, aus dem Metamodell entfernt, oder Attribute eines Elementes veraendert wurden.
- *
- * Die Attribute eines Elementes wurden veraendert:
- *  1. Attribut wurde hinzugefuegt => Attribut im Graph hinzufuegen
- *  2. Attribut wurde entfernt => Attribut im Graph entfernen
- *  3. Wert eines Attributes wurde veraendert => Wert des Attributes auch im Graph veraendern
- *
- * Die folgenden Faelle sind momentan noch nicht relevant, koennen dies in Zukunft aber evtl. werden:
- *
- * Ein Element wurde aus dem Metamodell entfernt:
- *  1. Element ist Verbindung => diese aus dem Graph entfernen.
- *  2. Element ist Klasse => dieses aus dem Graph entfernen, und alle Links auf Source bzw. Target mit
- *     dieser Klasse pruefen und Attribut entfernen.
- *
- * Ein Element wurde zum Metamodell hinzugefuegt:
- *  1. Element ist Verbindung => Element im Graph anlegen, Source- und Target-Elemente finden und eintragen.
- *     Problem: Verbindungstyp muss anhand der Werte von sourceDeletionDeletesTarget und targetDeletionDeletesSource
- *     herausgefunden werden. Attribute eintragen.
- *  2. Element ist Klasse => Klasse in Graph eintragen. Typ aus "abstract=true/false" erkennen. Attribute eintragen.
- *
- * Sonderfaelle:
- *  1. Einer Klasse wurde beim Attribut superTypes ein Wert hinzugefuegt oder entfernt => Link mit Typ Generalization
- *     in Graph eintragen bzw. davon entfernen.
- *
- */
 class MetamodelGraphDiff(val metaModelData: MetaModelData) {
   val metaModel = Json.parse(metaModelData.data).as[JsObject]
   var graph = Json.parse(metaModelData.graph).as[JsObject]
@@ -41,38 +14,77 @@ class MetamodelGraphDiff(val metaModelData: MetaModelData) {
 
     def fixAttributes() = {
       metaModel.keys.foreach { elementKey =>
-        println(elementKey)
-        println("Graph only: ")
-        println(graphOnlyAttributes(elementKey))
-        println("MetaModel only: ")
-        println(metaModelOnlyAttributes(elementKey))
-        println("Changed: ")
-        println(changedAttributes(elementKey))
+        metaModelOnlyAttributes(elementKey).foreach(attribute => removeFromGraph(elementKey, attributeName(attribute)))
+        graphOnlyAttributes(elementKey).foreach(attribute => addToGraph(elementKey, attribute))
+        changedAttributes(elementKey).foreach(attribute => changeInGraph(elementKey, attribute))
       }
-
-      def metaModelAttributes(elementKey: String): Set[JsObject] = ((metaModel \ elementKey) \ "mAttributes").as[JsObject].values.map(_.as[JsObject])
 
       def graphCells: Set[JsObject] = (graph \ "cells").as[Set[JsObject]]
 
-      def graphAttributes(elementKey: String): Set[JsObject] = (graphCells.filter(cell => (cell \ "name").as[String] == elementKey).head \ "m_attributes").as[Set[JsObject]]
+      def graphCell(elementKey: String): Option[JsObject] = graphCells.find(cell => (cell \ "name").as[String] == elementKey)
 
-      def metaModelOnlyAttributes(elementKey: String): Set[JsObject] = metaModelAttributes(elementKey).filter(attr => !graphAttributes(elementKey).exists(obj => (obj \ "name").as[String] == (attr \ "name").as[String]))
+      def metaModelCell(elementKey: String): Option[JsObject] = (metaModel \ elementKey).asOpt[JsObject]
 
-      def graphOnlyAttributes(elementKey: String): Set[JsObject] = graphAttributes(elementKey).filter(attr => !metaModelAttributes(elementKey).exists(obj => (obj \ "name").as[String] == (attr \ "name").as[String]))
+      def metaModelAttributes(elementKey: String): Set[JsObject] = {
+        metaModelCell(elementKey) match {
+          case Some(cell) =>
+            val mAttributes = (cell \ "mAttributes").asOpt[JsObject]
+            mAttributes match {
+              case Some(attributes) => attributes.values.map(_.as[JsObject])
+              case None => Set.empty
+            }
+          case None => Set.empty
+        }
+      }
+
+      def graphAttributes(elementKey: String): Set[JsObject] = {
+        graphCell(elementKey) match {
+          case Some(cell) =>
+            (cell \ "m_attributes").asOpt[JsArray] match {
+              case Some(attribute) => attribute.as[Set[JsObject]]
+              case None => Set.empty
+            }
+          case None => Set.empty
+        }
+      }
+
+      def graphAttributeNames(elementKey: String): Set[String] = graphAttributes(elementKey).map(attributeName)
+
+      def metaModelAttributeNames(elementKey: String): Set[String] = metaModelAttributes(elementKey).map(attributeName)
+
+      def attributeName(attribute: JsObject): String = (attribute \ "name").as[String]
+
+      def metaModelOnlyAttributes(elementKey: String): Set[JsObject] = {
+        val diff = metaModelAttributeNames(elementKey) -- graphAttributeNames(elementKey)
+        diff.map(attrName => metaModelAttributes(elementKey).filter(attr => attributeName(attr) == attrName).head)
+      }
+
+      def graphOnlyAttributes(elementKey: String): Set[JsObject] = {
+        val diff = graphAttributeNames(elementKey) -- metaModelAttributeNames(elementKey)
+        diff.map(attrName => graphAttributes(elementKey).filter(attr => attributeName(attr) == attrName).head)
+      }
 
       def changedAttributes(elementKey: String): Set[JsObject] = {
-        // TODO
-        graphAttributes(elementKey)
+        var changedAttrs = Set[JsObject]()
+        metaModelAttributes(elementKey).foreach { metaModelAttribute =>
+          val graphAttribute = graphAttributes(elementKey).filter(attr => attributeName(attr) == attributeName(metaModelAttribute)).head
+          if (metaModelAttribute != graphAttribute) {
+            changedAttrs = changedAttrs + metaModelAttribute
+          }
+        }
+        changedAttrs
       }
 
-      def removeAttribute(elementKey: String, attr: JsObject): JsObject = {
-        // TODO
-        graph
+      def removeFromGraph(elementKey: String, attributeKey: String) = {
+
       }
 
-      def addAttribute(elementKey: String, attr: JsObject): JsObject = {
-        // TODO
-        graph
+      def addToGraph(elementKey: String, attribute: JsObject) = {
+
+      }
+
+      def changeInGraph(elementKey: String, attribute: JsObject) = {
+
       }
 
 
