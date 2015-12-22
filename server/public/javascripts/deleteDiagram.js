fe4633a9aa065b1a492198574ba86c903c9c831f
Tobias Droth <todroth@htwg-konstanz.de>
2015-12-22 12:33:51 +0100

    Moved a lot of stuff from /public to /assets.

--- server/public/javascripts/deleteDiagram.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$(".deleteDiagram").click(function(event) {
-		event.preventDefault();
-		$.delete($(this).attr('href'), function() {
-			window.location.reload();
-		});
-	});
-});
