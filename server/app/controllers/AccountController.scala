a8131edb1b88a048c8145068a4f86b00f56e2b18
Tobias Droth <todroth@htwg-konstanz.de>
2016-01-12 15:58:30 +0100

    Merged new Play Version.

--- server/app/controllers/AccountController.scala
+++ server/app/controllers/AccountController.scala
@@ -1,15 +1,17 @@
 package controllers
 
+import javax.inject.Inject
+
 import models.SecureSocialUser
 import play.api.data.Form
-import play.api.data.Forms.{mapping, text, tuple}
-import play.api.mvc.AnyContent
-import securesocial.core.RuntimeEnvironment
+import play.api.data.Forms.{default, mapping, text, tuple}
+import play.api.i18n.{Messages, I18nSupport, MessagesApi}
+import util.definitions.UserEnvironment
 
 case class AccountFormData(firstName: String = "", lastName: String = "", fullName: String = "", eMail: String = "", password: (String, String) = ("", ""))
 
-class AccountController(override implicit val env: RuntimeEnvironment[SecureSocialUser])
-  extends securesocial.core.SecureSocial[SecureSocialUser] {
+class AccountController @Inject() (override implicit val env: UserEnvironment, val messagesApi: MessagesApi)
+  extends securesocial.core.SecureSocial with I18nSupport {
 
 
   val accountDataForm = Form(
@@ -19,8 +21,8 @@ class AccountController(override implicit val env: RuntimeEnvironment[SecureSoci
       "fullName" -> text,
       "eMail" -> text,
       "password" -> tuple(
-        "main" -> text,
-        "confirm" -> text
+        "main" -> default(text, ""),
+        "confirm" -> default(text, "")
       ).verifying("Passwords don't match", password => password._1 == password._2)
     )(AccountFormData.apply)(AccountFormData.unapply)
   )
@@ -29,15 +31,15 @@ class AccountController(override implicit val env: RuntimeEnvironment[SecureSoci
     Ok(views.html.webpage.AccountOverview.render(request.user))
   }
 
-  def edit() = SecuredAction { request =>
+  def edit() = SecuredAction { implicit request =>
     val filledForm = accountDataForm.fill(accountData(request.user))
-    Ok(views.html.webpage.AccountEdit.render(request.user, filledForm))
+    Ok(views.html.webpage.AccountEdit.render(request.user, filledForm, implicitly[Messages]))
   }
 
   def saveChanges() = SecuredAction { implicit request =>
     accountDataForm.bindFromRequest.fold(
       formWithErrors => {
-        BadRequest(views.html.webpage.AccountEdit.render(request.user, formWithErrors))
+        BadRequest(views.html.webpage.AccountEdit.render(request.user, formWithErrors, implicitly[Messages]))
       },
       userData => {
         Redirect(controllers.routes.AccountController.overview())
