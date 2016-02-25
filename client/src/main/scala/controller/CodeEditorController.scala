ff28cd4ffd5010634d6e096ca60083768e096827
Tobias Droth <todroth@htwg-konstanz.de>
2016-02-25 17:34:44 +0100

    Started using Rest API in Code Editor.

--- client/src/main/scala/controller/CodeEditorController.scala
+++ client/src/main/scala/controller/CodeEditorController.scala
@@ -1,14 +1,25 @@
 package controller
 
-import java.util.UUID
+import java.util.{Calendar, Date, UUID}
 
+import org.scalajs.jquery._
 import scalot._
 import shared.CodeEditorMessage._
 import view.CodeEditorView
 
+import scala.scalajs.js
+import scala.scalajs.js.Dynamic.literal
+import scala.scalajs.js.JSON
+
 case class CodeEditorController(dslType: String, metaModelUuid: String) {
 
+  case class AccessToken(token: String, expires: Int, dateLoaded: Date)
+
+  case class TokenInformation(token: String, refreshed: Boolean, error: Option[String])
+
   val autoSave = true
+
+  var accessToken: AccessToken = null
 
   val view = new CodeEditorView(controller = this, metaModelUuid = metaModelUuid, dslType = dslType, autoSave = autoSave)
   val ws = new WebSocketConnection(controller = this, metaModelUuid = metaModelUuid, dslType = dslType)
@@ -39,7 +50,80 @@ case class CodeEditorController(dslType: String, metaModelUuid: String) {
   }
 
   def saveCode() = {
-    ws.sendMessage(SaveCode(dslType, metaModelUuid, document.str))
+
+    def fnSave(tokenInformation: TokenInformation) : Unit = {
+
+      if (tokenInformation.error.isDefined) {
+        println(s"Error: ${tokenInformation.error}")
+        return
+      }
+
+      jQuery.ajax(literal(
+        `type` = "PUT",
+        url = s"/metamodels/$metaModelUuid/$dslType",
+        contentType = "application/json; charset=utf-8",
+        dataType = "json",
+        data = js.JSON.stringify(js.JSON.parse("{\"code\" : \"" + document.str + "\"}")),
+        headers = literal(
+          Authorization = s"Bearer ${tokenInformation.token}"
+        ),
+        success = { (data: js.Dynamic, textStatus: String, jqXHR: JQueryXHR) =>
+          println("Saved successfully")
+        },
+        error = { (jqXHR : JQueryXHR, textStatus: String, errorThrown : String) =>
+          if (!tokenInformation.refreshed) {
+            println("Error: Force Refresh")
+            authorized(fnSave, forceRefresh = true)
+          } else {
+             println(s"Cannot save: $errorThrown")
+          }
+        }
+      ).asInstanceOf[JQueryAjaxSettings])
+
+    }
+
+    authorized(fnSave, forceRefresh = false)
+
+    // ws.sendMessage(SaveCode(dslType, metaModelUuid, document.str))
+
+  }
+
+  def authorized(fnThen: TokenInformation => Unit, forceRefresh : Boolean) = {
+    println("authorized")
+
+    var refresh = forceRefresh
+
+    if (accessToken == null) {
+      println("accessToken is null")
+      refresh = true
+    }
+
+    if (refresh) {
+      println("Refresh")
+      refreshAccessToken(fnThen)
+    } else {
+      fnThen(TokenInformation(accessToken.token, refreshed = false, None))
+    }
+  }
+
+  def refreshAccessToken(fnThen : TokenInformation => Unit) = {
+    jQuery.ajax(literal(
+      `type` = "POST",
+      url = "/oauth/accessTokenLocal",
+      data = literal(
+        client_id = "modigen-browser-app1",
+        grant_type = "implicit"
+      ),
+      success = { (data: js.Dynamic, textStatus: String, jqXHR: JQueryXHR) =>
+        accessToken = AccessToken(data.access_token.asInstanceOf[String], data.expires_in.asInstanceOf[Int], new Date())
+        fnThen(TokenInformation(accessToken.token, refreshed = true, None))
+      },
+      error = { (jqXHR : JQueryXHR, textStatus: String, errorThrown : String) =>
+        accessToken = null
+        fnThen(TokenInformation("", refreshed = true, Some(errorThrown)))
+      }
+    ).asInstanceOf[JQueryAjaxSettings]
+    )
   }
 
   /** Apply changes to the corresponding doc */
