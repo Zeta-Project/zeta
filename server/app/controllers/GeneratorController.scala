c6edeaa0e26cca812f2bcacfb518653669486e1d
Nicolai Löffler <nicolai.loeffler92@googlemail.com>
2016-05-29 18:51:06 +0200

    changed output location of generated js files

--- server/app/controllers/GeneratorController.scala
+++ server/app/controllers/GeneratorController.scala
@@ -8,31 +8,39 @@ import generator.parser.{Cache, ShapeSketch, SprayParser}
 import generator.generators.spray.SprayGenerator
 import generator.generators.style.StyleGenerator
 import generator.generators.shape.ShapeGenerator
+import generator.model.style.Style
 import play.api.Play.current
 
 import scala.concurrent.duration._
 import scala.concurrent.Await
+import java.nio.file.{Files, Paths}
+
+import generator.model.shapecontainer.shape.Shape
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 class GeneratorController @Inject()(override implicit val env: UserEnvironment) extends securesocial.core.SecureSocial {
 
-  private val generatorOutputLocation = current.path.toString + "/app/generator/output/"
 
   def generate(metaModelUuid: String) = SecuredAction { implicit request =>
 
     val result = Await.result(MetaModelDaoImpl.findById(metaModelUuid), 30 seconds)
     if (result.isDefined && result.get.definition.concept.elements.nonEmpty) {
+      val generatorOutputLocation = current.path.toString + "/app/assets/modelEditor/editor/model_specific/" + metaModelUuid + "/"
+      Files.createDirectories(Paths.get(generatorOutputLocation))
+
       val hierarchyContainer = Cache()
       val parser = new SprayParser(hierarchyContainer, result.get.definition)
 
       val styles = parser.parseStyle(result.get.definition.style.get.code)
-      StyleGenerator.doGenerate(styles, generatorOutputLocation )
+      StyleGenerator.doGenerate(styles, generatorOutputLocation)
 
-      parser.parseAbstractShape(result.get.definition.shape.get.code)
+      val shapes = parser.parseAbstractShape(result.get.definition.shape.get.code)
       ShapeGenerator.doGenerate(hierarchyContainer, generatorOutputLocation)
 
       val diagrams = parser.parseDiagram(result.get.definition.diagram.get.code)
       SprayGenerator.doGenerate(diagrams.head.get, generatorOutputLocation)
-
 
       Ok("Generation successful")
     } else {
