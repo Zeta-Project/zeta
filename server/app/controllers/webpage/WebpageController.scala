8e1556d389107cbe9ca2458df17724b8241ff154
Nicolai Löffler <nicolai.loeffler92@googlemail.com>
2016-06-20 13:01:17 +0200

    use new model rest api: display model instances in diagram overview, also made model instances deleteable via button

--- server/app/controllers/webpage/WebpageController.scala
+++ server/app/controllers/webpage/WebpageController.scala
@@ -3,14 +3,16 @@ package controllers.webpage
 import javax.inject.Inject
 
 import dao.metaModel.ZetaMetaModelDao
+import dao.model.ZetaModelDao
 import models.modelDefinitions.metaModel.MetaModelEntity
+import models.modelDefinitions.model.ModelShortInfo
 import play.api.Logger
 import util.definitions.UserEnvironment
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class WebpageController @Inject()(metaModelDao: ZetaMetaModelDao, override implicit val env: UserEnvironment) extends securesocial.core.SecureSocial {
+class WebpageController @Inject()(metaModelDao: ZetaMetaModelDao, modelDao: ZetaModelDao, override implicit val env: UserEnvironment) extends securesocial.core.SecureSocial {
 
   val log = Logger(this getClass() getName())
 
@@ -23,14 +25,15 @@ class WebpageController @Inject()(metaModelDao: ZetaMetaModelDao, override impli
     val metaModels = Await.result(metaModelDao.findMetaModelsByUser(request.user.uuid.toString), 30 seconds)
 
     var metaModel: Option[MetaModelEntity] = None
-
+    var models: Seq[ModelShortInfo] = Seq.empty[ModelShortInfo]
     if (uuid != null) {
       val hasAccess = Await.result(metaModelDao.hasAccess(uuid, request.user.uuid.toString), 30 seconds)
       if (hasAccess.isDefined && hasAccess.get) {
         metaModel = Await.result(metaModelDao.findById(uuid), 30 seconds)
+        models = Await.result(modelDao.findModelsByUser(request.user.uuid.toString), 30 seconds).filter(m => m.metaModelId == uuid)
       }
     }
 
-    Ok(views.html.webpage.WebpageDiagramsOverview(Some(request.user), metaModels, metaModel))
+    Ok(views.html.webpage.WebpageDiagramsOverview(Some(request.user), metaModels, metaModel, models))
   }
 }
