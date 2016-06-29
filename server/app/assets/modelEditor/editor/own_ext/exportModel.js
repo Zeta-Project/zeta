41bfe70f01982e722024251fe62d2c7813946242
Nicolai Löffler <nicolai.loeffler92@googlemail.com>
2016-06-29 17:05:54 +0200

    moved export Model functionality to own file

---
+++ server/app/assets/modelEditor/editor/own_ext/exportModel.js
@@ -0,0 +1,140 @@
+var modelExporter = (function modelExporter () {
+    'use strict';
+    
+    var exportModel;
+    var _buildElements;
+    var _getAttributeValue;
+    var _graph;
+
+    exportModel = function(graph) {
+        _graph = graph;
+        var elements = _buildElements();
+        var uiState = JSON.stringify(_graph.toJSON());
+        var fnSave = function (accessToken, tokenRefreshed, error) {
+            if (error) {
+                alert("Error saving model: " + error);
+                return;
+            }
+
+            var data = JSON.stringify({
+                name: window._global_model_name,
+                elements: elements,
+                uiState: uiState
+            });
+
+            $.ajax({
+                type: 'PUT',
+                url: '/models/' + window._global_uuid + "/definition",
+                contentType: "application/json; charset=utf-8",
+                data: data,
+                headers: {
+                    Authorization: "Bearer " + accessToken
+                },
+                success: function (data, textStatus, jqXHR) {
+                    alert("Saved model");
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (!tokenRefreshed) {
+                        accessToken.authorized(fnSave, true);
+                    } else {
+                        alert("Error saving meta model: " + errorThrown);
+                    }
+                }
+            });
+        };
+
+        accessToken.authorized(fnSave);
+
+    };
+
+
+    _buildElements = function() {
+        var elements = [];
+
+        _graph.getElements().forEach(function(ele) {
+            var element = {
+                id: ele.id,
+                mClass: ele.attributes.mClass,
+                outputs: {},
+                inputs: {},
+                attributes: {}
+            };
+
+            if(ele.attributes.hasOwnProperty('mClassAttributes')) {
+                ele.attributes.mClassAttributes.forEach(function (attr) {
+
+                    var attrInfo = ele.attributes.mClassAttributeInfo.find(function(info) {
+                        return info.name === attr.type;
+                    });
+                    var value = _getAttributeValue(attr.value, attrInfo.type);
+                    if (element.attributes.hasOwnProperty(attr.type)) {
+                        element.attributes[attr.type].push(value);
+                    } else {
+                        element.attributes[attr.type] = [value];
+                    }
+                });
+            }
+
+            ele.attributes.mClassAttributeInfo.forEach(function(info) {
+                if(!element.attributes.hasOwnProperty(info.name)) {
+                    element.attributes[name.name] = [];
+                }
+            });
+
+            _graph.getConnectedLinks(ele, {outbound: true}).forEach(function(link) {
+                if(element.outputs.hasOwnProperty(link.attributes.mReference)) {
+                    element.outputs[link.attributes.mReference].push(link.attributes.id);
+                } else {
+                    element.outputs[link.attributes.mReference] = [link.attributes.id];
+                }
+            });
+
+            _graph.getConnectedLinks(ele, {inbound: true}).forEach(function(link) {
+                if(element.inputs.hasOwnProperty(link.attributes.mReference)) {
+                    element.inputs[link.attributes.mReference].push(link.attributes.id);
+                } else {
+                    element.inputs[link.attributes.mReference] = [link.attributes.id];
+                }
+            });
+            elements.push(element);
+        });
+
+        _graph.getLinks().forEach(function(link) {
+            var element = {
+                id: link.id,
+                mReference: link.attributes.mReference,
+                source: {},
+                target: {},
+                attributes: {}
+            };
+            // In the Metamodel a connection can have multiple sources/targets
+            // but in joint.js this is not possible
+            element.source[link.attributes.sourceAttribute] = [link.attributes.source.id];
+            element.target[link.attributes.targetAttribute] = [link.attributes.target.id];
+            elements.push(element);
+
+        });
+        return elements;
+    };
+
+    _getAttributeValue = function(value, type) {
+        var ret = value;
+        switch(type) {
+            case 'Bool':
+                ret = value.toLowerCase() === 'true';
+                break;
+            case 'Int':
+                ret = parseInt(value);
+                break;
+            case 'Double':
+                ret = parseFloat(value);
+                break;
+        }
+        return ret;
+    };
+
+    return {
+        exportModel : exportModel
+    };
+
+})();
--- server/app/assets/modelEditor/editor/rappid/src/main.js
+++ server/app/assets/modelEditor/editor/rappid/src/main.js
@@ -439,7 +439,9 @@ var Rappid = Backbone.Router.extend({
 
         this.initializeToolbarTooltips();
 
-        $('#btn-export-model').on('click', _.bind(this.exportModel, this));
+        $('#btn-export-model').on('click', _.bind(function() {
+            modelExporter.exportModel(this.graph);
+        }, this));
         $('#btn-undo').on('click', _.bind(this.commandManager.undo, this.commandManager));
         $('#btn-redo').on('click', _.bind(this.commandManager.redo, this.commandManager));
         $('#btn-clear').on('click', _.bind(this.graph.clear, this.graph));
@@ -593,133 +595,6 @@ var Rappid = Backbone.Router.extend({
         var roomUrl = location.href.replace(location.hash, '') + '#' + room;
         $('.statusbar-container .rt-colab').html('Send this link to a friend to <b>collaborate in real-time</b>: <a href="' + roomUrl + '" target="_blank">' + roomUrl + '</a>');
     },
-
-    exportModel: function() {
-        var elements = this.buildElements();
-        var uiState = JSON.stringify(this.graph.toJSON());
-        var fnSave = function (accessToken, tokenRefreshed, error) {
-            if (error) {
-                alert("Error saving model: " + error);
-                return;
-            }
-
-            var data = JSON.stringify({
-                name: window._global_model_name,
-                elements: elements,
-                uiState: uiState
-            });
-
-            $.ajax({
-                type: 'PUT',
-                url: '/models/' + window._global_uuid + "/definition",
-                contentType: "application/json; charset=utf-8",
-                data: data,
-                headers: {
-                    Authorization: "Bearer " + accessToken
-                },
-                success: function (data, textStatus, jqXHR) {
-                    alert("Saved model");
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    if (!tokenRefreshed) {
-                        accessToken.authorized(fnSave, true);
-                    } else {
-                        alert("Error saving meta model: " + errorThrown);
-                    }
-                }
-            });
-        };
-
-         accessToken.authorized(fnSave);
-         
-    },
-
-    
-    buildElements: function() {
-        var elements = [];
-        var graph = this.graph;
-        var getAttributeValue = function(value, type) {
-            var ret = value;
-            switch(type) {
-                case 'Bool':
-                    ret = value.toLowerCase() === 'true';
-                    break;
-                case 'Int':
-                    ret = parseInt(value);
-                    break;
-                case 'Double':
-                    ret = parseFloat(value);
-                    break;
-            }
-            return ret;
-        };
-        
-        graph.getElements().forEach(function(ele) {
-            var element = {
-                id: ele.id,
-                mClass: ele.attributes.mClass,
-                outputs: {},
-                inputs: {},
-                attributes: {}
-            };
-
-            if(ele.attributes.hasOwnProperty('mClassAttributes')) {
-                ele.attributes.mClassAttributes.forEach(function (attr) {
-
-                    var attrInfo = ele.attributes.mClassAttributeInfo.find(function(info) {
-                        return info.name === attr.type;
-                    });
-                    var value = getAttributeValue(attr.value, attrInfo.type);
-                    if (element.attributes.hasOwnProperty(attr.type)) {
-                        element.attributes[attr.type].push(value);
-                    } else {
-                        element.attributes[attr.type] = [value];
-                    }
-                });
-            }
-
-            ele.attributes.mClassAttributeInfo.forEach(function(info) {
-                if(!element.attributes.hasOwnProperty(info.name)) {
-                    element.attributes[name.name] = [];
-                }
-            });
-
-            graph.getConnectedLinks(ele, {outbound: true}).forEach(function(link) {
-                if(element.outputs.hasOwnProperty(link.attributes.mReference)) {
-                    element.outputs[link.attributes.mReference].push(link.attributes.id);
-                } else {
-                    element.outputs[link.attributes.mReference] = [link.attributes.id];
-                }
-            });
-
-            graph.getConnectedLinks(ele, {inbound: true}).forEach(function(link) {
-                if(element.inputs.hasOwnProperty(link.attributes.mReference)) {
-                    element.inputs[link.attributes.mReference].push(link.attributes.id);
-                } else {
-                    element.inputs[link.attributes.mReference] = [link.attributes.id];
-                }
-            });
-            elements.push(element);
-        });
-
-        graph.getLinks().forEach(function(link) {
-            var element = {
-                id: link.id,
-                mReference: link.attributes.mReference,
-                source: {},
-                target: {},
-                attributes: {}
-            };
-            // In the Metamodel a connection can have multiple sources/targets
-            // but in joint.js this is not possible
-            element.source[link.attributes.sourceAttribute] = [link.attributes.source.id];
-            element.target[link.attributes.targetAttribute] = [link.attributes.target.id];
-            elements.push(element);
-
-        });
-        return elements;
-    },
-
 
     initializeToggleSidePanels: function() {
         $(".stencil-toggle-icon-wrapper").on("click", function() {
