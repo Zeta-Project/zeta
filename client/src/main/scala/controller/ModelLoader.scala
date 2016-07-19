70db1b6f5cafc773453521f648772174741fa8ad
Felix Laegeler <felixlaegeler@gmail.com>
2016-07-19 17:39:38 +0200

    added methods to load and parse metamodel definitions using the existing REST api

--- client/src/main/scala/controller/ModelLoader.scala
+++ client/src/main/scala/controller/ModelLoader.scala
@@ -1,8 +1,67 @@
 package controller
 
+import controller.AccessToken.TokenInformation
+
 import scala.scalajs.js
 import scala.scalajs.js.Dynamic.literal
 import org.scalajs.jquery._
+import scala.scalajs.js.annotation.JSExport
+
+//@js.native
+/* remarks: annotation above does not exist prior to scala.js 0.6.5, so we can't use it (currently in project: 0.6.4)
+   but: will be required in scala.js 1.0.0
+   see: https://www.scala-js.org/doc/interoperability/facade-types.html
+*/
+trait MetaElementData extends js.Object {
+  val mType: String = js.native
+  val name: String = js.native
+}
+
+//@js.native
+trait MetaModelData extends js.Object {
+  val name: String = js.native
+  val elements: scalajs.js.Array[MetaElementData] = js.native
+}
+
+case class MetaModel(
+  id: String,
+  name: String,
+  mClasses: scalajs.js.Array[MetaElementData],
+  mReferences: scalajs.js.Array[MetaElementData]
+)
+
+@JSExport
+class MetaModelLoader(metaModelId: String, andThen: MetaModel => Unit) {
+
+  @JSExport
+  def fetch(): Unit = {
+    AccessToken.authorized(loadMetaModel, forceRefresh = false)
+  }
+
+  private def loadMetaModel(tokenInformation: TokenInformation): Unit = {
+    jQuery.ajax(literal(
+      `type` = "GET",
+      dataType = "json",
+      url = s"/metamodels/$metaModelId/definition",
+      headers = literal(
+        Authorization = s"Bearer ${tokenInformation.token}"
+      ),
+      success = { (data: js.Dynamic, textStatus: String, jqXHR: JQueryXHR) =>
+        val m = data.asInstanceOf[MetaModelData]
+        val out = MetaModel(
+          metaModelId,
+          m.name,
+          m.elements.filter(_.mType == "mClass"),
+          m.elements.filter(_.mType == "mReference")
+        )
+        andThen(out)
+      },
+      error = { (jqXHR: JQueryXHR, textStatus: String, errorThrown: String) =>
+        println(s"Cannot load meta model: $errorThrown")
+      }
+    ).asInstanceOf[JQueryAjaxSettings])
+  }
+}
 
 
 case class ModelLoader(modelId: String) {
