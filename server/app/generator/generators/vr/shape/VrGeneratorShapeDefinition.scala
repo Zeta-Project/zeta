5970fd0b08c29566b9c41db5ca8bfdb88f896688
magoeke2 <magoeke@htwg-konstanz.de>
2016-11-12 15:29:40 +0100

    VrGeneratorShapeDefinition.scala is able to generate simple ellipse and rectangle but no imports yet.

--- server/app/generator/generators/vr/shape/VrGeneratorShapeDefinition.scala
+++ server/app/generator/generators/vr/shape/VrGeneratorShapeDefinition.scala
@@ -1,6 +1,8 @@
 package generator.generators.vr.shape
 
 import java.nio.file.{Files, Paths}
+
+import generator.model.shapecontainer.shape.geometrics._
 import generator.model.shapecontainer.shape.{Compartment, Shape}
 
 /**
@@ -12,17 +14,20 @@ object VrGeneratorShapeDefinition {
   }
 
   def generateFile(shape: Shape, packageName: String, DEFAULT_SHAPE_LOCATION: String) = {
-    val FILENAME = "vr-" + shape.name + ".html"
+    if(shape.name != "rootShape") {
+      val FILENAME = "vr-" + shape.name + ".html"
 
-    val polymerElement = generatePolymerElement(shape)
+      val polymerElement = generatePolymerElement(shape)
 
-    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + FILENAME), polymerElement.getBytes())
+      Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + FILENAME), polymerElement.getBytes())
+    }
   }
 
   def generatePolymerElement(shape: Shape) = {
     s"""
     <dom-module id="vr-${shape.name}">
       <template>
+        ${generateHtmlTemplate(shape.shapes.getOrElse(List()))}
       </template>
     </dom-module>
 
@@ -31,8 +36,32 @@ object VrGeneratorShapeDefinition {
       is: "vr-${shape.name}",
     });
     </script>
+    """
+  }
 
-     """
+  def generateHtmlTemplate(geometrics: List[GeometricModel]) : String = {
+    (for(g : GeometricModel <- geometrics) yield {
+      g match {
+        case g: Line => "Line"
+        // caution: Order is important because ellipse extends rectangle
+        case g: Ellipse => {
+          val (x, y) = g.position.getOrElse((0,0))
+          s"""<vr-ellipse x="${x}" y="${y}" width="${g.size_width}" height="${g.size_width}">
+              ${generateHtmlTemplate(g.children)}
+          </vr-ellipse>"""
+        }
+        case g: Rectangle => {
+          val (x, y) = g.position.getOrElse((0,0))
+          s"""<vr-box x-pos="${x}" y-pos="${y}" width="${g.size_width}" height="${g.size_width}">
+              ${generateHtmlTemplate(g.children)}
+          </vr-box>"""
+        }
+        case g: Polygon => "Polygon"
+        case g: PolyLine => "PolyLine"
+        case g: RoundedRectangle => "RoundedRectangle"
+        case g: Text => "Text"
+      }
+    }).mkString
   }
 
 }
