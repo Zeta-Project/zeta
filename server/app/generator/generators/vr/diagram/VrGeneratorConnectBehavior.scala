b4702d044054c34b0095f0453ff1a773a661a92b
magoeke <magoeke@htwg-konstanz.de>
2017-01-16 21:33:59 +0100

    Added id to connections.

--- server/app/generator/generators/vr/diagram/VrGeneratorConnectBehavior.scala
+++ server/app/generator/generators/vr/diagram/VrGeneratorConnectBehavior.scala
@@ -23,14 +23,25 @@ object VrGeneratorConnectBehavior {
           return Object.keys(this.connectionMap);
         },
 
-        pushNewConnection: function(type, connection) {
+        pushNewConnection: function(type, connection, id) {
           if(type in this.connectionMap) {
             var element = this.connectionMap[type]();
+            element.id = id != null ? id : guid();
             element.from = connection.from;
             element.to = connection.to;
             if(this.parentNode) Polymer.dom(this.parentNode.root).appendChild(element);
           } else {
             console.error("No valid connection found! Caution generated Code.");
+          }
+
+          function guid() {
+            function s4() {
+              return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+              s4() + '-' + s4() + s4() + s4();
           }
         }
       }];
