f30b01a426326a635e5dde38f9b87e6c0c2e70aa
deparlak <deparlak@googlemail.com>
2017-03-11 23:21:27 +0100

    removed not required zeta-gen

--- api/misc/zeta-gen/src/main/scala/zeta/generator/domain/model/ModelShortInfo.scala
+++
@@ -1,9 +0,0 @@
-package zeta.generator.domain.model
-
-import play.api.libs.json.Json
-import zeta.generator.network.HLink
-
-case class ModelShortInfo(name: String, id: String, links: Seq[HLink])
-object ModelShortInfo {
-  implicit val reads = Json.reads[ModelShortInfo]
-}
--- api/misc/zeta-gen/src/main/scala/zeta/generator/network/HLink.scala
+++
@@ -1,8 +0,0 @@
-package zeta.generator.network
-
-import play.api.libs.json.Json
-
-case class HLink(rel: String, href: String, method: String)
-object HLink {
-  implicit val reads = Json.reads[HLink]
-}
