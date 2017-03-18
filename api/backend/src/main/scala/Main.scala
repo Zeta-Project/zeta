12a734a52143c7f71a59ad165cd8bd7946ed26b4
Philipp Daniels <philipp.daniels@gmail.com>
2017-03-18 15:02:18 +0100

    Refactor Main into more modular parts

--- api/backend/src/main/scala/Main.scala
+++ api/backend/src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import actors.developer.{ DeveloperManager, Mediator }
 import actors.frontend.DeveloperDummy
 import actors.master.Master
-import actors.worker.{ DockerWorkExecutor, DummyWorkerExecutor, Worker }
+import actors.worker.{ DockerWorkExecutor, Worker }
 import akka.actor.{ ActorIdentity, ActorPath, ActorSystem, Identify, PoisonPill, Props }
 import akka.cluster.sharding.{ ClusterSharding, ClusterShardingSettings }
 import akka.cluster.singleton.{ ClusterSingletonManager, ClusterSingletonManagerSettings }
@@ -12,182 +12,44 @@ import akka.stream.ActorMaterializer
 import cluster.ClusterManager
 import com.typesafe.config.ConfigFactory
 import models.session.SyncGatewaySession
-import org.rogach.scallop.ScallopConf
+import org.rogach.scallop.{ ScallopConf, ScallopOption }
+import org.slf4j.LoggerFactory
 import play.api.libs.ws.ahc.AhcWSClient
 
 import scala.concurrent.duration._
 import scala.language.implicitConversions
 
-class Commands(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val workers = opt[Int]()
-  val master = opt[Int]()
-  val num = opt[Int]()
-  val developer = opt[Int]()
-  val dummy = opt[Int]()
-  val seeds = opt[List[String]](default = Some(List()))
-  val test = opt[Boolean]()
+object Main extends App {
+  val cmd = new Commands(args)
+
+  MasterStarter().initiate(cmd)
+  WorkersStarter().initiate(cmd)
+  DeveloperStarter().initiate(cmd)
+  DummyStarter().initiate(cmd)
+}
+
+protected class Commands(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val workers: ScallopOption[Int] = opt[Int]()
+  val master: ScallopOption[Int] = opt[Int]()
+  val num: ScallopOption[Int] = opt[Int]()
+  val developer: ScallopOption[Int] = opt[Int]()
+  val dummy: ScallopOption[Int] = opt[Int]()
+  val seeds: ScallopOption[List[String]] = opt[List[String]](default = Some(List()))
+  val test: ScallopOption[Boolean] = opt[Boolean]()
   verify()
 }
 
-object Main extends App {
-  case class WorkersConf(numberOfWorkers: Int, seeds: List[String])
-  case class MasterConf(port: Int, seeds: List[String], num: Int)
-  case class DeveloperConf(port: Int, seeds: List[String])
-  case class DummyConf(port: Int, seeds: List[String])
+protected trait Starter {
+  def initiate(cmd: Commands): Unit
 
-  val cmd = new Commands(args)
-
-  val worker = for {
-    workers <- cmd.workers
-    seeds <- cmd.seeds
-  } yield WorkersConf(workers, seeds)
-
-  val master = for {
-    port <- cmd.master
-    seeds <- cmd.seeds
-    num <- cmd.num
-  } yield MasterConf(port, seeds, num)
-
-  val developer = for {
-    port <- cmd.developer
-    seeds <- cmd.seeds
-  } yield DeveloperConf(port, seeds)
-
-  val dummy = for {
-    port <- cmd.dummy
-    seeds <- cmd.seeds
-  } yield DummyConf(port, seeds)
-
-  worker foreach { startWorkers }
-  master foreach { startMaster }
-  developer foreach { startDeveloper }
-  dummy foreach { startDummy }
-
-  def name = "ClusterSystem"
-
-  /**
-   * The interval in which a worker register itself to the master
-   */
-  def registerInterval = 30.seconds
-
-  /**
-   * The time after which a worker will be marked as unreachable
-   */
-  def workerTimeout = 5.minutes
-
-  /**
-   * The time after which a work (execution of a generator, filter, etc..) will be cancelled
-   */
-  def workTimeout = 4.minutes
-
-  /**
-   * This time specify how long the session is, to access the database in a docker container (to execute generator, filter, etc..)
-   * Note: This time should be longer as the workTimeout, because if workTimeout was reached, the system should be
-   * able to store the log of the docker container
-   */
-  def sessionDuration = workTimeout.plus(2.minutes)
-
-  def startDummy(dummy: DummyConf): Unit = {
-    val roles = List("dummy")
-
-    //Thread.sleep(30000)
-
-    val config = ClusterManager.getClusterJoinConfig(roles, dummy.seeds, dummy.port).withFallback(ConfigFactory.load())
-    val system = ActorSystem(name, config)
-
-    ClusterSharding(system).startProxy(
-      typeName = Mediator.shardRegionName,
-      role = Some(Mediator.locatedOnNode),
-      extractEntityId = Mediator.extractEntityId,
-      extractShardId = Mediator.extractShardId
-    )
-
-    system.actorOf(DeveloperDummy.props(), "dummy")
-
-    //val journalAddress = ClusterManager.getJournalPath(dummy.port, dummy.seeds)
-    //setSharedJournal(system, journalAddress)
-  }
-
-  def startDeveloper(developer: DeveloperConf): Unit = {
-    val roles = List(Mediator.locatedOnNode)
-
-    // Before we start we wait to make sure that the other actors started in the system
-    Thread.sleep(20000)
-
-    val config = ClusterManager.getClusterJoinConfig(roles, developer.seeds, developer.port).withFallback(ConfigFactory.load())
-    val system = ActorSystem(name, config)
-
-    ClusterSharding(system).start(
-      typeName = Mediator.shardRegionName,
-      entityProps = Props[Mediator],
-      settings = ClusterShardingSettings(system),
-      extractEntityId = Mediator.extractEntityId,
-      extractShardId = Mediator.extractShardId
-    )
-
-    system.actorOf(DeveloperManager.props(), "developer")
-
-    val journalAddress = ClusterManager.getJournalPath(developer.port, developer.seeds)
-    setSharedJournal(system, journalAddress)
-  }
-
-  def startMaster(master: MasterConf): Unit = {
-    val roles = List("backend")
-
-    if (master.num == 2) {
-      Thread.sleep(10000)
-    }
-
-    val config = ClusterManager.getClusterJoinConfig(roles, master.seeds, master.port).withFallback(ConfigFactory.load())
-    implicit val system = ActorSystem(name, config)
-    implicit val mat = ActorMaterializer()
-    implicit val client = AhcWSClient()
-    val sessionManager = SyncGatewaySession()
-
-    system.actorOf(
-      ClusterSingletonManager.props(
-        Master.props(workerTimeout, sessionDuration, sessionManager),
-        PoisonPill,
-        ClusterSingletonManagerSettings(system).withRole("backend")
-      ),
-      "master"
-    )
-
-    // no seeds? Then this is the first node on which we start the shared level db store
-    if (master.seeds.isEmpty) {
-      system.actorOf(Props[SharedLeveldbStore], "store")
-    }
-
-    val journalAddress = ClusterManager.getJournalPath(master.port, master.seeds)
-
-    println("Journal Address : " + journalAddress.toString)
-    setSharedJournal(system, journalAddress)
-  }
-
-  def startWorkers(workers: WorkersConf) = {
-    Thread.sleep(20000)
-
-    println(s"Start ${workers.numberOfWorkers} workers")
-    (1 to workers.numberOfWorkers) foreach (i => startWorker(workers, i, 0))
-  }
-
-  def startWorker(workers: WorkersConf, number: Int, port: Int): Unit = {
-    val roles = List("worker")
-    val config = ClusterManager.getClusterJoinConfig(roles, workers.seeds, port).withFallback(ConfigFactory.load())
-    val system = ActorSystem(name, config)
-
-    val executor = system.actorOf(DockerWorkExecutor.props(), s"work-executor-${number}")
-    //val executor = system.actorOf(DummyWorkerExecutor.props())
-
-    system.actorOf(Worker.props(executor, registerInterval, workTimeout), s"worker-${number}")
-  }
-
-  def setSharedJournal(system: ActorSystem, path: ActorPath): Unit = {
+  protected def setSharedJournal(system: ActorSystem, path: ActorPath): Unit = {
     import system.dispatcher
     implicit val timeout = Timeout(15.seconds)
-    val f = (system.actorSelection(path) ? Identify(None))
+    val f = system.actorSelection(path) ? Identify(None)
     f.onSuccess {
-      case ActorIdentity(_, Some(ref)) => SharedLeveldbJournal.setStore(ref, system)
+      case ActorIdentity(_, Some(ref)) =>
+
+        SharedLeveldbJournal.setStore(ref, system)
       case _ =>
         system.log.error("Shared journal not started at {}", path)
         system.terminate()
@@ -198,4 +60,208 @@ object Main extends App {
         system.terminate()
     }
   }
+
+  protected def createActorSystem(role: String, seeds: List[String], port: Int): ActorSystem = {
+    val roles = List(role)
+    val config = ClusterManager.getClusterJoinConfig(roles, seeds, port).withFallback(ConfigFactory.load())
+    ActorSystem(Starter.Name, config)
+  }
+}
+
+protected object Starter {
+  val Name = "ClusterSystem"
+  /**
+   * The time after which a work (execution of a generator, filter, etc..) will be cancelled
+   */
+  val WorkTimeout: FiniteDuration = 4.minutes
+}
+
+protected class MasterStarter extends Starter {
+  case class Config(port: Int, seeds: List[String], num: Int)
+  private val logger = LoggerFactory.getLogger(MasterStarter.getClass)
+
+  def initiate(cmd: Commands): Unit = {
+    val config = for {
+      port <- cmd.master
+      seeds <- cmd.seeds
+      num <- cmd.num
+    } yield Config(port, seeds, num)
+
+    config foreach { start }
+  }
+
+  private def start(config: Config): Unit = {
+    logger.debug(MasterStarter.LogStart, config.toString)
+    if (config.num == 2) {
+      Thread.sleep(MasterStarter.MilliSecToWaitForFirstMaster)
+    }
+
+    val system = createActor(config)
+    startSharedLevelDbOnFirstMaster(config, system)
+
+    val journalAddress = ClusterManager.getJournalPath(config.port, config.seeds)
+    logger.debug(MasterStarter.LogJournalAddress, journalAddress.toString)
+
+    setSharedJournal(system, journalAddress)
+  }
+
+  private def createActor(config: Config) = {
+    implicit val system = createActorSystem(MasterStarter.ActorRole, config.seeds, config.port)
+    implicit val mat = ActorMaterializer()
+    implicit val client = AhcWSClient()
+    val sessionManager = SyncGatewaySession()
+
+    system.actorOf(
+      ClusterSingletonManager.props(
+        Master.props(MasterStarter.WorkerTimeout, MasterStarter.SessionDuration, sessionManager),
+        PoisonPill,
+        ClusterSingletonManagerSettings(system).withRole(MasterStarter.ActorRole)
+      ),
+      MasterStarter.ActorName
+    )
+
+    system
+  }
+
+  private def startSharedLevelDbOnFirstMaster(config: Config, system: ActorSystem) = {
+    if (config.seeds.isEmpty) {
+      system.actorOf(Props[SharedLeveldbStore], MasterStarter.ActorSharedLevelDbStore)
+    }
+  }
+}
+
+protected object MasterStarter {
+  val ActorName = "master"
+  val ActorRole = "backend"
+  val ActorSharedLevelDbStore = "store"
+  val LogJournalAddress = "Journal Address : {}"
+  val LogStart = "Start master actor: {}"
+  val MilliSecToWaitForFirstMaster = 10000
+  /**
+   * This time specify how long the session is, to access the database in a docker container (to execute generator, filter, etc..)
+   * Note: This time should be longer as the workTimeout, because if workTimeout was reached, the system should be
+   * able to store the log of the docker container
+   */
+  val SessionDuration: FiniteDuration = Starter.WorkTimeout.plus(2.minutes)
+  /**
+   * The time after which a worker will be marked as unreachable
+   */
+  val WorkerTimeout: FiniteDuration = 5.minutes
+
+  def apply(): MasterStarter = new MasterStarter()
+}
+
+protected class WorkersStarter extends Starter {
+  case class Config(numberOfWorkers: Int, seeds: List[String])
+  private val logger = LoggerFactory.getLogger(WorkersStarter.getClass)
+
+  def initiate(cmd: Commands): Unit = {
+    val config = for {
+      workers <- cmd.workers
+      seeds <- cmd.seeds
+    } yield Config(workers, seeds)
+
+    config foreach { start }
+  }
+
+  private def start(config: Config): Unit = {
+    Thread.sleep(WorkersStarter.MilliSecWaitForMaster)
+
+    logger.debug(WorkersStarter.LogStart, config.numberOfWorkers, config.toString)
+    (1 to config.numberOfWorkers) foreach (i => startWorker(config, i, 0))
+  }
+
+  private def startWorker(config: Config, number: Int, port: Int) {
+    val system = createActorSystem(WorkersStarter.ActorRole, config.seeds, port)
+    val executor = system.actorOf(DockerWorkExecutor.props(), s"work-executor-$number")
+    system.actorOf(Worker.props(executor, WorkersStarter.RegisterInterval, Starter.WorkTimeout), s"worker-$number")
+  }
+}
+
+protected object WorkersStarter {
+  val ActorRole = "worker"
+  val LogStart = "Start `{}` worker actor: {}"
+  /**
+   * The interval in which a worker register itself to the master
+   */
+  val RegisterInterval: FiniteDuration = 30.seconds
+  val MilliSecWaitForMaster = 20000
+
+  def apply(): WorkersStarter = new WorkersStarter()
+}
+
+protected class DummyStarter extends Starter {
+  case class Config(port: Int, seeds: List[String])
+  private val logger = LoggerFactory.getLogger(DummyStarter.getClass)
+
+  def initiate(cmd: Commands): Unit = {
+    val config = for {
+      port <- cmd.dummy
+      seeds <- cmd.seeds
+    } yield Config(port, seeds)
+
+    config foreach { start }
+  }
+
+  private def start(dummy: Config): Unit = {
+    logger.debug(DummyStarter.LogStart, dummy.toString)
+    val system = createActorSystem(DummyStarter.ActorRole, dummy.seeds, dummy.port)
+
+    ClusterSharding(system).startProxy(
+      typeName = Mediator.shardRegionName,
+      role = Some(Mediator.locatedOnNode),
+      extractEntityId = Mediator.extractEntityId,
+      extractShardId = Mediator.extractShardId
+    )
+
+    system.actorOf(DeveloperDummy.props(), DummyStarter.ActorName)
+  }
+}
+
+protected object DummyStarter {
+  val ActorName = "dummy"
+  val ActorRole = "dummy"
+  val LogStart = "Start dummy actor: {}"
+
+  def apply(): DummyStarter = new DummyStarter()
+}
+
+protected class DeveloperStarter extends Starter {
+  case class Config(port: Int, seeds: List[String])
+  private val logger = LoggerFactory.getLogger(DeveloperStarter.getClass)
+
+  def initiate(cmd: Commands): Unit = {
+    val developer = for {
+      port <- cmd.developer
+      seeds <- cmd.seeds
+    } yield Config(port, seeds)
+
+    developer foreach { start }
+  }
+
+  private def start(developer: Config): Unit = {
+    logger.debug(DeveloperStarter.LogStart, developer.toString)
+    Thread.sleep(DeveloperStarter.MilliSecWaitForOtherActors)
+    val system = createActorSystem(Mediator.locatedOnNode, developer.seeds, developer.port)
+
+    ClusterSharding(system).start(
+      typeName = Mediator.shardRegionName,
+      entityProps = Props[Mediator],
+      settings = ClusterShardingSettings(system),
+      extractEntityId = Mediator.extractEntityId,
+      extractShardId = Mediator.extractShardId
+    )
+
+    system.actorOf(DeveloperManager.props(), DeveloperStarter.ActorName)
+
+    val journalAddress = ClusterManager.getJournalPath(developer.port, developer.seeds)
+    setSharedJournal(system, journalAddress)
+  }
+}
+
+protected object DeveloperStarter {
+  val ActorName = "developer"
+  val LogStart = "Start developer actor: {}"
+  val MilliSecWaitForOtherActors = 20000
+  def apply(): DeveloperStarter = new DeveloperStarter()
 }
