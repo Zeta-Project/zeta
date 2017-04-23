c652e79903aa7179f1cbb71102bf52c236ed59d0
aullik <nicolaswehrle@gmx.net>
2017-04-23 21:47:15 +0200

    move doGenerate to doGenerateFile

--- api/server/app/generator/generators/shape/ShapeGenerator.scala
+++ api/server/app/generator/generators/shape/ShapeGenerator.scala
@@ -6,40 +6,53 @@ import generator.model.diagram.node.Node
 import generator.parser.Cache
 
 /**
- * The ShapeGenerator Object
- */
+  * The ShapeGenerator Object
+  */
 object ShapeGenerator {
 
   val JOINTJS_SHAPE_FILENAME = "shape.js"
-  val JOINTJS_INSPECTOR_FILENAME = "inspector.js"
   val JOINTJS_CONNECTION_FILENAME = "connectionstyle.js"
+  val JOINTJS_INSPECTOR_FILENAME = "inspector.js"
   val JOINTJS_SHAPE_AND_INLINE_STYLE_FILENAME = "elementAndInlineStyle.js"
 
-  /** creates the files shape.js, inspector.js, connectionstyle.js and elementAndInlineStyle.js*/
-  def doGenerate(cache: Cache, location: String, nodes: List[Node]) {
+  /** creates the files shape.js, inspector.js, connectionstyle.js and elementAndInlineStyle.js */
+  def doGenerate(cache: Cache, location: String, nodes: List[Node]): Unit = {
     val DEFAULT_SHAPE_LOCATION = location
+    val shapeGen = doGenerateFile(cache, nodes)
+
+    // Shapes
+    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_SHAPE_FILENAME), shapeGen.shape.getBytes)
+
+    //ConnectionStyle
+    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_CONNECTION_FILENAME), shapeGen.connectionStyle.getBytes)
+
+    // Inspector
+    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_INSPECTOR_FILENAME), shapeGen.inspector.getBytes)
+
+    // ElementAndInlineStyle
+    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_SHAPE_AND_INLINE_STYLE_FILENAME), shapeGen.elementAndInlineStyle.getBytes)
+  }
+
+  def doGenerateFile(cache: Cache, nodes: List[Node]): ShapeGenerator = {
     val attrs = GeneratorShapeDefinition.attrsInspector
     val packageName = "zeta"
     val shapes = cache.shapeHierarchy.nodeView.values.map(s => s.data)
 
     // Shapes
-    val jointJSShapeContent = GeneratorShapeDefinition.generate(shapes, packageName)
-    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_SHAPE_FILENAME), jointJSShapeContent.getBytes)
+    val shape = GeneratorShapeDefinition.generate(shapes, packageName)
 
     //ConnectionStyle
-    val jointJsConnectionContent = GeneratorConnectionDefinition.generate(cache.connections.values)
-    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_CONNECTION_FILENAME), jointJsConnectionContent.getBytes)
+    val connectionStyle = GeneratorConnectionDefinition.generate(cache.connections.values)
 
     // Inspector
-    val jointJSInspectorContent = GeneratorInspectorDefinition.generate(shapes, packageName, attrs, nodes)
-
-    //  Generate InspectorFile
-    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_INSPECTOR_FILENAME), jointJSInspectorContent.getBytes)
+    val inspector = GeneratorInspectorDefinition.generate(shapes, packageName, attrs, nodes)
 
     // ElementAndInlineStyle
-    val jointJSShapeAndInlineStyleContent = GeneratorShapeAndInlineStyle.generate(shapes, packageName, attrs)
-    Files.write(Paths.get(DEFAULT_SHAPE_LOCATION + JOINTJS_SHAPE_AND_INLINE_STYLE_FILENAME), jointJSShapeAndInlineStyleContent.getBytes)
+    val elementAndInlineStyle = GeneratorShapeAndInlineStyle.generate(shapes, packageName, attrs)
 
+    ShapeGenerator(shape, connectionStyle, inspector, elementAndInlineStyle)
   }
 
 }
+
+case class ShapeGenerator(shape: String, connectionStyle: String, inspector: String, elementAndInlineStyle: String)
