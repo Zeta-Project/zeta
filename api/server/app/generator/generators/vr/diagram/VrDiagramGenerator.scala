de14c015d0a48a8e057c38ac49a7d2fa23bc400a
aullik <nicolaswehrle@gmx.net>
2017-04-23 21:48:26 +0200

    move doGenerate to doGenerateFile

--- api/server/app/generator/generators/vr/diagram/VrDiagramGenerator.scala
+++ api/server/app/generator/generators/vr/diagram/VrDiagramGenerator.scala
@@ -1,38 +1,54 @@
 package generator.generators.vr.diagram
 
-import java.nio.file.{ Files, Paths }
+import java.nio.file.{Files, Paths}
 
 import generator.model.diagram.Diagram
 import generator.model.diagram.edge.Edge
+import generator.model.shapecontainer.connection.Connection
 
 /**
- * Created by max on 12.11.16.
- */
+  * Created by max on 12.11.16.
+  */
 object VrDiagramGenerator {
-  private val EXTENDED_NEW_BEHAVIOR = "vr-new-extended.html"
-  private val EXTENDED_CONNECT_BEHAVIOR = "vr-connect-extended.html"
-  private val SCENE = "vr-scene.html"
-  private val SAVE_BEHAVIOR = "vr-save.html"
+  val EXTENDED_NEW_BEHAVIOR = "vr-new-extended.html"
+  val EXTENDED_CONNECT_BEHAVIOR = "vr-connect-extended.html"
+  val SCENE = "vr-scene.html"
+  val SAVE_BEHAVIOR = "vr-save.html"
 
   def doGenerate(diagram: Diagram, location: String) {
     val DEFAULT_DIAGRAM_LOCATION = location
+    val vrDiaGen = doGenerateFiles(diagram)
 
+    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + EXTENDED_NEW_BEHAVIOR), vrDiaGen.vrNew.getBytes)
+    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + EXTENDED_CONNECT_BEHAVIOR), vrDiaGen.vrConnect.getBytes)
+    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + SCENE), vrDiaGen.vrScene.getBytes)
+    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + SAVE_BEHAVIOR), vrDiaGen.vrSave.getBytes)
+  }
+
+
+  def doGenerateFiles(diagram: Diagram): VrDiagramGenerator = {
     val nodes = diagram.nodes
-    val connections = diagram.edges.map(getConnection(_)).groupBy(_.name).map(_._2.head)
+    val connections = diagram.edges.map(getConnection).groupBy(_.name).map(_._2.head)
 
-    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + EXTENDED_NEW_BEHAVIOR), VrGeneratorNewBehavior.generate(nodes).getBytes)
-    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + EXTENDED_CONNECT_BEHAVIOR), VrGeneratorConnectBehavior.generate(connections, diagram.edges).getBytes)
-    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + SCENE), VrGeneratorScene.generate(nodes, connections).getBytes)
-    Files.write(Paths.get(DEFAULT_DIAGRAM_LOCATION + SAVE_BEHAVIOR), VrGeneratorSaveBehavior.generate(nodes, connections, diagram).getBytes)
+    VrDiagramGenerator(
+      vrNew = VrGeneratorNewBehavior.generate(nodes),
+      vrConnect = VrGeneratorConnectBehavior.generate(connections, diagram.edges),
+      vrScene = VrGeneratorScene.generate(nodes, connections),
+      vrSave = VrGeneratorSaveBehavior.generate(nodes, connections, diagram)
+    )
   }
 
   // copied from ValidatorGenerator
-  def getConnection(edge: Edge) = {
+  def getConnection(edge: Edge): Connection = {
     val connectionReference = edge.connection.referencedConnection
-    if (connectionReference isDefined) {
+    if (connectionReference.isDefined) {
       connectionReference.get
     } else {
       throw new NoSuchElementException("No connection defined for edge " + edge.name)
     }
   }
 }
+
+case class VrDiagramGenerator(vrNew: String, vrConnect: String, vrScene: String, vrSave: String)
+
+
