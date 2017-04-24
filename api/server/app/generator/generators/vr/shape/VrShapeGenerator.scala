f0acf4d50272d68f9a6f11219808e3d7e3c4efad
aullik <nicolaswehrle@gmx.net>
2017-04-24 04:09:19 +0200

    doGenerateFile in VrShapeGenerator now returns List of Files

--- api/server/app/generator/generators/vr/shape/VrShapeGenerator.scala
+++ api/server/app/generator/generators/vr/shape/VrShapeGenerator.scala
@@ -1,10 +1,13 @@
 package generator.generators.vr.shape
 
-/**
- * Created by steffen on 25/10/16.
- */
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
 import generator.model.diagram.node.Node
 import generator.parser.Cache
+import models.file.File
+
 
 /**
  * The ShapeGenerator Object
@@ -12,11 +15,17 @@ import generator.parser.Cache
 object VrShapeGenerator {
 
   def doGenerate(cache: Cache, location: String, nodes: List[Node]): Unit = {
-    val packageName = "zeta"
-    val shapes = cache.shapeHierarchy.nodeView.values.map(s => s.data)
-    VrGeneratorShapeDefinition.generate(shapes, packageName, location)
-
-    VrGeneratorConnectionDefinition.generate(cache.connections.values, location)
+    doGenerateFile(cache, location, nodes).map(f => Files.write(Paths.get(f.name), f.content.getBytes()))
   }
+
+  def doGenerateFile(cache: Cache, location: String, nodes: List[Node]): List[File] = {
+    val shapes = cache.shapeHierarchy.nodeView.values.map(s => s.data).toList
+
+    val shapeDefinition = VrGeneratorShapeDefinition.doGenerateFile(shapes, location)
+    val connectionDefinition = VrGeneratorConnectionDefinition.doGenerateFile(cache.connections.values, location)
+
+    shapeDefinition ::: connectionDefinition
+  }
+
 
 }
