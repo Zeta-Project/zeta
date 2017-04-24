1e891f20b367d79dab2e998be4112402b0343a2f
aullik <nicolaswehrle@gmx.net>
2017-04-24 12:06:58 +0200

    reworked GeneratorController

--- api/server/app/controllers/GeneratorController.scala
+++ api/server/app/controllers/GeneratorController.scala
@@ -1,69 +1,188 @@
 package controllers
 
-import javax.inject.Inject
-
-import generator.parser.Cache
-import generator.parser.SprayParser
-import generator.generators.diagram.DiagramGenerator
-import generator.generators.style.StyleGenerator
-import generator.generators.shape.ShapeGenerator
-import generator.generators.vr.shape.VrShapeGenerator
 import java.nio.file.Files
 import java.nio.file.Paths
+import javax.inject.Inject
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import com.mohiva.play.silhouette.api.Silhouette
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
+import generator.generators.diagram.DiagramGenerator
+import generator.generators.shape.ShapeGenerator
+import generator.generators.style.StyleGenerator
 import generator.generators.vr.diagram.VrDiagramGenerator
+import generator.generators.vr.shape.VrShapeGenerator
 import generator.model.diagram.Diagram
+import generator.model.style.Style
+import generator.parser.Cache
+import generator.parser.SprayParser
 import models.document.MetaModelEntity
 import models.document.Repository
+import models.file.File
+import models.modelDefinitions.metaModel.Dsl
+import models.modelDefinitions.metaModel.Shape
+import models.modelDefinitions.metaModel.{Diagram => DslDiagram}
+import models.modelDefinitions.metaModel.{Style => DslStyle}
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
 import play.api.mvc.Controller
 import utils.auth.DefaultEnv
 import utils.auth.RepositoryFactory
 
-class GeneratorController @Inject() (implicit repositoryFactory: RepositoryFactory, silhouette: Silhouette[DefaultEnv]) extends Controller {
+import scala.annotation.tailrec
 
-  def repository[A]()(implicit request: SecuredRequest[DefaultEnv, A]): Repository =
+//import scala.concurrent.ExecutionContext.Implicits.global
+
+class GeneratorController @Inject()(implicit repositoryFactory: RepositoryFactory, silhouette: Silhouette[DefaultEnv]) extends Controller {
+
+  private def repository[A](request: SecuredRequest[DefaultEnv, A]): Repository =
     repositoryFactory.fromSession(request)
 
-  def generate(metaModelUuid: String) = silhouette.SecuredAction.async { implicit request =>
+  def generate(metaModelUuid: String): Action[AnyContent] = silhouette.SecuredAction.async(req => {
 
-    repository.get[MetaModelEntity](metaModelUuid).map { result =>
-      val generatorOutputLocation = System.getenv("PWD") + "/server/model_specific/" + metaModelUuid + "/"
-      val vrGeneratorOutputLocation = System.getenv("PWD") + "/server/model_specific/vr/" + metaModelUuid + "/"
+    repository(req).get[MetaModelEntity](metaModelUuid)
+      .map(createGenerators(_) match {
+             case None => Ok("Generation successful")
+             case Some(error) => BadRequest(error)
+           }).recover {
+                        case _: Exception => NotFound("Metamodel with id: " + metaModelUuid + " was not found")
+                      }
+  }
 
-      Files.createDirectories(Paths.get(generatorOutputLocation))
-      Files.createDirectories(Paths.get(vrGeneratorOutputLocation))
+  )
 
-      val hierarchyContainer = Cache()
-      val parser = new SprayParser(hierarchyContainer, result)
-      var diagram: Option[Diagram] = None
-      var error: Option[String] = None
+  private type ErrorMessage = String
 
-      try {
-        parser.parseStyle(result.dsl.style.get.code)
-        parser.parseShape(result.dsl.shape.get.code)
-        diagram = parser.parseDiagram(result.dsl.diagram.get.code).head
-      } catch {
-        case e: Throwable => error = Some("There occurred an error during parsing")
+  private def createGeneratorsOld(metaModel: MetaModelEntity): Option[ErrorMessage] = {
+    val metaModelUuid = metaModel._id
+    val generatorOutputLocation: String = System.getenv("PWD") + "/server/model_specific/" + metaModelUuid + "/"
+    val vrGeneratorOutputLocation = System.getenv("PWD") + "/server/model_specific/vr/" + metaModelUuid + "/"
+
+    Files.createDirectories(Paths.get(generatorOutputLocation))
+    Files.createDirectories(Paths.get(vrGeneratorOutputLocation))
+
+    val hierarchyContainer = Cache()
+    val parser = new SprayParser(hierarchyContainer, metaModel)
+
+    val (diagram, error) = try {
+      parser.parseStyle(metaModel.dsl.style.get.code)
+      parser.parseShape(metaModel.dsl.shape.get.code)
+      val diagram = parser.parseDiagram(metaModel.dsl.diagram.get.code).head
+      (diagram, None)
+    } catch {
+      case _: Throwable =>
+        (None, Some("There occurred an error during parsing"))
+    }
+
+    error.orElse {
+                   try {
+                     StyleGenerator.doGenerate((for {style <- hierarchyContainer.styleHierarchy.nodeView} yield style._2.data).toList, generatorOutputLocation)
+                     ShapeGenerator.doGenerate(hierarchyContainer, generatorOutputLocation, diagram.get.nodes)
+                     DiagramGenerator.doGenerate(diagram.get, generatorOutputLocation)
+
+                     // Generate files for the VR - Editor
+                     VrShapeGenerator.doGenerate(hierarchyContainer, vrGeneratorOutputLocation, diagram.get.nodes)
+                     VrDiagramGenerator.doGenerate(diagram.get, vrGeneratorOutputLocation)
+                     None
+                   } catch {
+                     case _: Throwable => Some("There occurred an error during generation");
+                   }
+                 }
+    error
+  }
+
+  def createGenerators(metaModel: MetaModelEntity): Option[ErrorMessage] = {
+
+    val metaModelUuid = metaModel._id
+    val generatorOutputLocation: String = System.getenv("PWD") + "/server/model_specific/" + metaModelUuid + "/"
+    val vrGeneratorOutputLocation = System.getenv("PWD") + "/server/model_specific/vr/" + metaModelUuid + "/"
+
+    Files.createDirectories(Paths.get(generatorOutputLocation))
+    Files.createDirectories(Paths.get(vrGeneratorOutputLocation))
+
+    val hierarchyContainer = Cache()
+    val parser = new SprayParser(hierarchyContainer, metaModel)
+
+    def tryParse[E, R](get: Dsl => Option[E], parse: E => List[R], name: String): Either[List[R], ErrorMessage] = {
+      get(metaModel.dsl) match {
+        case None => Right(s"$name not available")
+        case Some(e) =>
+          try {
+            Left(parse(e))
+          } catch {
+            case _: Throwable => Right(s"$name failed parsing")
+          }
       }
-      if (error.isEmpty) {
-        try {
-          StyleGenerator.doGenerate((for {style <- hierarchyContainer.styleHierarchy.nodeView} yield style._2.data).toList, generatorOutputLocation)
-          ShapeGenerator.doGenerate(hierarchyContainer, generatorOutputLocation, diagram.get.nodes)
-          DiagramGenerator.doGenerate(diagram.get, generatorOutputLocation)
+    }
 
-          // Generate files for the VR - Editor
-          VrShapeGenerator.doGenerate(hierarchyContainer, vrGeneratorOutputLocation, diagram.get.nodes)
-          VrDiagramGenerator.doGenerate(diagram.get, vrGeneratorOutputLocation)
+    tryParse[DslStyle, Style](_.style, (s: DslStyle) => parser.parseStyle(s.code), "Style") match {
+      case Left(_) =>
+      case Right(err) => return Some(err)
+    }
+
+    tryParse[Shape, AnyRef](_.shape, s => parser.parseShape(s.code), "Shape") match {
+      case Left(_) =>
+      case Right(err) => return Some(err)
+    }
+
+    val diagramName = "Diagram"
+    val diagram = tryParse[DslDiagram, Option[Diagram]](_.diagram, s => parser.parseDiagram(s.code), diagramName) match {
+      case Right(err) => return Some(err)
+      case Left(Some(dia) :: _) => dia
+      case Left(_) => return Some(s"No $diagramName available")
+    }
+
+    def tryRecoverSingle(block: () => File, onFailure: String): () => Either[List[File], ErrorMessage] =
+      tryRecover(() => List(block()), onFailure)
+
+
+    def tryRecover(block: () => List[File], onFailure: String): () => Either[List[File], ErrorMessage] = {
+      def execute() = {
+        try {
+          Left(block())
         } catch {
-          case e: Throwable => error = Some("There occurred an error during generation");
+          case _: Throwable => Right(s"failed on: $onFailure")
         }
       }
-      if (error.isDefined) BadRequest(error.get) else Ok("Generation successful")
-    }.recover {
-      case e: Exception => NotFound("Metamodel with id: " + metaModelUuid + " was not found")
+
+      () => execute()
     }
+
+    val styles = hierarchyContainer.styleHierarchy.nodeView.values.map(_.data).toList
+
+    val generators = List(
+      tryRecoverSingle(() => StyleGenerator.doGenerateFile(styles, generatorOutputLocation), "StyleGenerator"),
+      tryRecover(() => ShapeGenerator.doGenerateFile(hierarchyContainer, generatorOutputLocation, diagram.nodes), "ShapeGenerator"),
+      tryRecover(() => DiagramGenerator.doGenerateFile(diagram, generatorOutputLocation), "DiagramGenerator"),
+
+      // Generate files for the VR - Editor
+      tryRecover(() => VrShapeGenerator.doGenerateFile(hierarchyContainer, vrGeneratorOutputLocation, diagram.nodes), "VrShapeGenerator"),
+      tryRecover(() => VrDiagramGenerator.doGenerateFiles(diagram, vrGeneratorOutputLocation), "VrDiagramGenerator")
+    )
+
+    @tailrec
+    def generate(generators: List[() => Either[List[File], ErrorMessage]], carry: List[File] = Nil): Either[List[File], ErrorMessage] =
+      generators match {
+        case Nil => Left(carry)
+        case head :: tail => head() match {
+          case r: Right[_, _] => r
+          case Left(list) => generate(tail, carry ::: list)
+        }
+      }
+
+
+    val res = generate(generators)
+    res match {
+      case Left(files) =>
+        files.foreach(f => Files.write(Paths.get(f.name), f.content.getBytes))
+        None
+      case Right(error) =>
+        Some(error)
+    }
+
+
   }
+
+
 }
+
+
