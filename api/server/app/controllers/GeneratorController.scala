23f5d63f8d08637dd412124354f92db79241a21b
aullik <nicolaswehrle@gmx.net>
2017-04-26 03:58:02 +0200

    GeneratorController now uses Result class

--- api/server/app/controllers/GeneratorController.scala
+++ api/server/app/controllers/GeneratorController.scala
@@ -22,6 +22,9 @@ import models.modelDefinitions.metaModel.Dsl
 import models.modelDefinitions.metaModel.Shape
 import models.modelDefinitions.metaModel.{Diagram => DslDiagram}
 import models.modelDefinitions.metaModel.{Style => DslStyle}
+import models.result.Failure
+import models.result.Result
+import models.result.Success
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.Controller
@@ -37,90 +40,42 @@ class GeneratorController @Inject()(implicit repositoryFactory: RepositoryFactor
     repositoryFactory.fromSession(request)
 
   def generate(metaModelUuid: String): Action[AnyContent] = silhouette.SecuredAction.async(req => {
-
     repository(req).get[MetaModelEntity](metaModelUuid)
       .map(createGenerators(_) match {
-        case Left(_) => Ok("Generation successful")
-        case Right(error) => BadRequest(error)
+        case Success(_) => Ok("Generation successful")
+        case Failure(error) => BadRequest(error)
       })(Implicits.global)
       .recover {
         case _: Exception => NotFound("Metamodel with id: " + metaModelUuid + " was not found")
       }(Implicits.global)
-  }
+  })
 
-  )
 
-  private type ErrorMessage = String
-
-  def createGenerators(metaModel: MetaModelEntity): Either[List[File], ErrorMessage] = {
+  def createGenerators(metaModel: MetaModelEntity): Result[List[File]] = {
     val hierarchyContainer = Cache()
-    parseMetaModel(metaModel, hierarchyContainer) match {
-      case Right(error) => Right(error)
-      case Left(dia) => createGeneratorFile(metaModel, dia, hierarchyContainer)
-    }
+    parseMetaModel(metaModel, hierarchyContainer).flatMap(dia => createGeneratorFile(metaModel, dia, hierarchyContainer))
   }
 
-
-  private def parseMetaModel(metaModel: MetaModelEntity, hierarchyContainer: Cache): Either[Diagram, ErrorMessage] = {
+  private def parseMetaModel(metaModel: MetaModelEntity, hierarchyContainer: Cache): Result[Diagram] = {
     val parser = new SprayParser(hierarchyContainer, metaModel)
 
-    def tryParse[E, R](get: Dsl => Option[E], parse: E => List[R], name: String): Either[List[R], ErrorMessage] = {
+    def tryParse[E, R](get: Dsl => Option[E], parse: E => List[R], name: String): Result[List[R]] = {
       get(metaModel.dsl) match {
-        case None => Right(s"$name not available")
-        case Some(e) =>
-          try {
-            Left(parse(e))
-          } catch {
-            case _: Throwable => Right(s"$name failed parsing")
-          }
+        case None => Failure(s"$name not available")
+        case Some(e) => Result(() => parse(e), s"$name failed parsing")
       }
     }
 
-    tryParse[DslStyle, Style](_.style, (s: DslStyle) => parser.parseStyle(s.code), "Style") match {
-      case Right(err) => Right(err)
-      case Left(_) =>
-        tryParse[Shape, AnyRef](_.shape, s => parser.parseShape(s.code), "Shape") match {
-          case Right(err) => Right(err)
-          case Left(_) =>
-            val diagramName = "Diagram"
-            tryParse[DslDiagram, Option[Diagram]](_.diagram, s => parser.parseDiagram(s.code), diagramName) match {
-              case Right(err) => Right(err)
-              case Left(Some(dia) :: _) => Left(dia)
-              case Left(_) => Right(s"No $diagramName available")
-            }
-        }
-    }
+    tryParse[DslStyle, Style](_.style, (s: DslStyle) => parser.parseStyle(s.code), "Style").
+      flatMap(_ => tryParse[Shape, AnyRef](_.shape, s => parser.parseShape(s.code), "Shape")).
+      flatMap(_ => tryParse[DslDiagram, Option[Diagram]](_.diagram, s => parser.parseDiagram(s.code), GeneratorController.DiagramName)).
+      flatMap {
+        case Some(dia) :: _ => Success(dia)
+        case _ => Failure(s"No ${GeneratorController.DiagramName} available")
+      }
   }
 
-
-  private def tryRecoverSingle(block: () => File, onFailure: String): () => Either[List[File], ErrorMessage] =
-    tryRecover(() => List(block()), onFailure)
-
-
-  private def tryRecover(block: () => List[File], onFailure: String): () => Either[List[File], ErrorMessage] = {
-    def execute() = {
-      try {
-        Left(block())
-      } catch {
-        case _: Throwable => Right(s"failed on: $onFailure")
-      }
-    }
-
-    () => execute()
-  }
-
-  @tailrec
-  private def generate(generators: List[() => Either[List[File], ErrorMessage]], carry: List[File] = Nil): Either[List[File], ErrorMessage] =
-    generators match {
-      case Nil => Left(carry)
-      case head :: tail => head() match {
-        case r: Right[_, _] => r
-        case Left(list) => generate(tail, carry ::: list)
-      }
-    }
-
-
-  private def createGeneratorFile(metaModel: MetaModelEntity, diagram: Diagram, hierarchyContainer: Cache): Either[List[File], ErrorMessage] = {
+  private def createGeneratorFile(metaModel: MetaModelEntity, diagram: Diagram, hierarchyContainer: Cache): Result[List[File]] = {
     val metaModelUuid = metaModel._id
     val currentDir = System.getenv("PWD")
     val generatorOutputLocation: String = currentDir + "/server/model_specific/" + metaModelUuid + "/"
@@ -132,27 +87,32 @@ class GeneratorController @Inject()(implicit repositoryFactory: RepositoryFactor
 
     val styles = hierarchyContainer.styleHierarchy.nodeView.values.map(_.data).toList
 
-    val generators = List(
-      tryRecoverSingle(() => StyleGenerator.doGenerateFile(styles, generatorOutputLocation), "StyleGenerator"),
-      tryRecover(() => ShapeGenerator.doGenerateFile(hierarchyContainer, generatorOutputLocation, diagram.nodes), "ShapeGenerator"),
-      tryRecover(() => DiagramGenerator.doGenerateFile(diagram, generatorOutputLocation), "DiagramGenerator"),
+    val generators2: List[() => Result[List[File]]] = List(
+      () => Result(() => List(StyleGenerator.doGenerateFile(styles, generatorOutputLocation)), "StyleGenerator"),
+      () => Result(() => ShapeGenerator.doGenerateFile(hierarchyContainer, generatorOutputLocation, diagram.nodes), "ShapeGenerator"),
+      () => Result(() => DiagramGenerator.doGenerateFile(diagram, generatorOutputLocation), "DiagramGenerator"),
 
       // Generate files for the VR - Editor
-      tryRecover(() => VrShapeGenerator.doGenerateFile(hierarchyContainer, vrGeneratorOutputLocation, diagram.nodes), "VrShapeGenerator"),
-      tryRecover(() => VrDiagramGenerator.doGenerateFiles(diagram, vrGeneratorOutputLocation), "VrDiagramGenerator")
+      () => Result(() => VrShapeGenerator.doGenerateFile(hierarchyContainer, vrGeneratorOutputLocation, diagram.nodes), "VrShapeGenerator"),
+      () => Result(() => VrDiagramGenerator.doGenerateFiles(diagram, vrGeneratorOutputLocation), "VrDiagramGenerator")
     )
 
-
-    val res = generate(generators)
-    res match {
-      case Left(files) =>
-        files.foreach(f => Files.write(Paths.get(f.name), f.content.getBytes))
-      case _ =>
-    }
-
+    val res = generate(generators2)
+    res.foreach(_.foreach(f => Files.write(Paths.get(f.name), f.content.getBytes)))
     res
   }
 
+  @tailrec
+  private def generate(generators: List[() => Result[List[File]]], carry: List[File] = Nil): Result[List[File]] =
+    generators match {
+      case Nil => Success(carry)
+      case head :: tail => head() match {
+        case f: Failure => f
+        case Success(list) => generate(tail, carry ::: list)
+      }
+    }
 }
 
-
+object GeneratorController {
+  private[GeneratorController] val DiagramName = "Diagram"
+}
