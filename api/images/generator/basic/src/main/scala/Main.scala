1ce83b89ccbb1a6a553fc743688b62fe4ea3bee6
aullik <nicolaswehrle@gmx.net>
2017-04-27 04:36:02 +0200

    fix some warnings

--- api/images/generator/basic/src/main/scala/Main.scala
+++ api/images/generator/basic/src/main/scala/Main.scala
@@ -2,7 +2,6 @@ import generator.Error
 import generator.Result
 import generator.Success
 import generator.Transformer
-
 import models.document.Filter
 import models.document.Generator
 import models.document.GeneratorImage
@@ -19,63 +18,78 @@ import scala.concurrent.Future
  * Main class of basic generator
  */
 object Main extends Template[CreateOptions, String] {
+
+  /**
+   * Create assets for the model transformer
+   *
+   * @param options   The Options for the creation of the generator
+   * @param image     The id of the image for the generator
+   * @param documents Access to the Documents repository
+   * @param files     Access to the Files repository
+   * @param remote    Access to docker container
+   * @return The result of the generator creation
+   */
   override def createTransformer(options: CreateOptions, image: String)(implicit documents: Documents, files: Files, remote: Remote): Future[Result] = {
     for {
       image <- documents.get[GeneratorImage](image)
       generator <- documents.create[Generator](Generator(user, options.name, image))
       created <- files.create(generator, createFile(Settings.generatorFile))
-    } yield Success()
+    } yield {
+      Success()
+    }
   }
 
-  def createFile(name: String): File = {
+  private def createFile(name: String): File = {
     val content =
-      """
+      s"""
         |class MyTransformer() extends Transformer {
-        | private val logger = LoggerFactory.getLogger(getClass)
+        |  private val logger = LoggerFactory.getLogger(getClass)
         |
-        | def transform(entity: ModelEntity)(implicit documents: Documents, files: Files, remote: Remote) : Future[Transformer] = {
-        |   logger.info(s"User : ${entity.id}")
-        |   entity.model.elements.values.foreach { element => element match {
-        |     case node: Node => logger.info(node.`type`.name)
-        |     case edge: Edge => logger.info(edge.`type`.name)
-        |   }}
-        |   Future.successful(this)
-        | }
+        |  def transform(entity: ModelEntity)(implicit documents: Documents, files: Files, remote: Remote) : Future[Transformer] = {
+        |    logger.info(s"User : $${entity.id}")
+        |    entity.model.elements.values.foreach { element => element match {
+        |      case node: Node => logger.info(node.`type`.name)
+        |      case edge: Edge => logger.info(edge.`type`.name)
+        |    }}
+        |    Future.successful(this)
+        |  }
         |
-        | def exit()(implicit documents: Documents, files: Files, remote: Remote) : Future[Result] = {
-        |   val result = Success("The generator finished")
-        |   Future.successful(result)
-        | }
+        |  def exit()(implicit documents: Documents, files: Files, remote: Remote) : Future[Result] = {
+        |    val result = Success("The generator finished")
+        |    Future.successful(result)
+        |  }
         |}
         |
       """.stripMargin
     File(name, content)
   }
 
-  def compiledGenerator(file: File) = {
-    val content = s"""
-      import scala.concurrent.Future
-      import models.modelDefinitions.model.elements.{Node, Edge}
-      import models.document.ModelEntity
-      import models.document.{Repository => Documents}
-      import models.file.{Repository => Files}
-      import models.remote.Remote
-      import generator._
-      import org.slf4j.LoggerFactory
-
-      ${file.content}
-
-      new MyTransformer()
+  private def compiledGenerator(file: File): Future[Transformer] = {
+    val content =
+      s"""
+        |import scala.concurrent.Future
+        |import models.modelDefinitions.model.elements.{Node, Edge}
+        |import models.document.ModelEntity
+        |import models.document.{Repository => Documents}
+        |import models.file.{Repository => Files}
+        |import models.remote.Remote
+        |import generator._
+        |import org.slf4j.LoggerFactory
+        |
+        |${file.content}
+        |
+        |new MyTransformer()
       """
     compile[Transformer](content)
   }
 
-  /**
-   * Initialize the generator
+  /** Initialize the generator
    *
-   * @param file The file which was loaded for the generator
+   * @param file      The file which was loaded for the generator
+   * @param filter    not used.
    * @param documents Access to the Documents repository
-   * @param files Access to the Files repository
+   * @param files     Access to the Files repository
+   * @param remote    unused.
    * @return A Generator
    */
   override def getTransformer(file: File, filter: Filter)(implicit documents: Documents, files: Files, remote: Remote): Future[Transformer] = {
@@ -86,8 +100,10 @@ object Main extends Template[CreateOptions, String] {
    * Initialize the generator
    *
    * @param file      The file which was loaded for the generator
+   * @param model     the modelEntity
    * @param documents Access to the Documents repository
    * @param files     Access to the Files repository
+   * @param remote    Access to docker container
    * @return A Generator
    */
   override def getTransformer(file: File, model: ModelEntity)(implicit documents: Documents, files: Files, remote: Remote): Future[Transformer] = {
@@ -97,6 +113,10 @@ object Main extends Template[CreateOptions, String] {
   /**
    * Initialize the generator
    *
+   * @param options   the options for the generator
+   * @param documents Access to the Documents repository
+   * @param files     Access to the Files repository
+   * @param remote    Access to docker container
    * @return A Generator
    */
   override def runGeneratorWithOptions(options: String)(implicit documents: Documents, files: Files, remote: Remote): Future[Result] = {
