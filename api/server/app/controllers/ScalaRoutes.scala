1f3bda35a6e274bb5f069b7363c4a0518ae7b6b7
aullik <nicolaswehrle@gmx.net>
2017-05-18 08:46:37 +0200

    ScalaRoutes base implementation

--- api/server/app/controllers/ScalaRoutes.scala
+++ api/server/app/controllers/ScalaRoutes.scala
@@ -1,10 +1,14 @@
 package controllers
 
+import java.util.UUID
 import javax.inject.Inject
 
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import com.mohiva.play.silhouette.api.Silhouette
+import controllers.restApi.MetaModelRestApi
+import controllers.restApi.ModelRestApi
+import controllers.webpage.WebpageController
 import de.htwg.zeta.server.authentication.BasicAction
 import de.htwg.zeta.server.authentication.UnAuthenticatedAction
 import de.htwg.zeta.server.authentication.AuthenticatedAction
@@ -12,13 +16,24 @@ import de.htwg.zeta.server.authentication.BasicWebSocket
 import de.htwg.zeta.server.authentication.AuthenticatedWebSocket
 import de.htwg.zeta.server.authentication.UnAuthenticatedWebSocket
 import play.api.i18n.MessagesApi
+import play.api.inject.Injector
+import play.api.libs.json.JsValue
 import play.api.mvc.Controller
+import play.api.mvc.WebSocket
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
 import utils.auth.ZetaEnv
 
 /**
  * All routes are managed in this class
  */
-class ScalaRoutes @Inject()(messagesApi: MessagesApi, silhouette: Silhouette[ZetaEnv], system: ActorSystem, mat: Materializer) extends Controller {
+class ScalaRoutes @Inject()(
+    messagesApi: MessagesApi,
+    silhouette: Silhouette[ZetaEnv],
+    system: ActorSystem,
+    mat: Materializer,
+    injector: Injector // TODO don't inject Injector. Replace with multiple provider
+) extends Controller {
 
   private object AuthenticatedGet extends AuthenticatedAction(messagesApi, silhouette)
 
@@ -41,6 +56,179 @@ class ScalaRoutes @Inject()(messagesApi: MessagesApi, silhouette: Silhouette[Zet
   private object BasicSocket extends BasicWebSocket(system, silhouette, mat)
 
 
+  // TODO replace injector with provider
+
+  private lazy val BackendController: BackendController = injector.instanceOf[BackendController]
+  private lazy val ApplicationController: ApplicationController = injector.instanceOf[ApplicationController]
+  private lazy val SocialAuthController: SocialAuthController = injector.instanceOf[SocialAuthController]
+  private lazy val SignUpController: SignUpController = injector.instanceOf[SignUpController]
+  private lazy val SignInController: SignInController = injector.instanceOf[SignInController]
+  private lazy val ForgotPasswordController: ForgotPasswordController = injector.instanceOf[ForgotPasswordController]
+  private lazy val ResetPasswordController: ResetPasswordController = injector.instanceOf[ResetPasswordController]
+  private lazy val ChangePasswordController: ChangePasswordController = injector.instanceOf[ChangePasswordController]
+  private lazy val ActivateAccountController: ActivateAccountController = injector.instanceOf[ActivateAccountController]
+  private lazy val WebpageController: WebpageController = injector.instanceOf[WebpageController]
+  private lazy val MetaModelController: MetaModelController = injector.instanceOf[MetaModelController]
+  private lazy val ModelController: ModelController = injector.instanceOf[ModelController]
+  private lazy val GeneratorController: GeneratorController = injector.instanceOf[GeneratorController]
+  private lazy val CodeEditorController: CodeEditorController = injector.instanceOf[CodeEditorController]
+  private lazy val WebJarAssets: WebJarAssets = injector.instanceOf[WebJarAssets]
+  private lazy val DynamicFileController: DynamicFileController = injector.instanceOf[DynamicFileController]
+
+
+  def backendDeveloper: WebSocket = BackendController.developer
+
+  def backendGenerator(id: String): WebSocket = BackendController.generator(id)
+
+  def backendController(model: String): WebSocket = BackendController.user(model)
+
+
+  // # Home page
+  def appIndex(): Action[AnyContent] = ApplicationController.index
+
+  def user(): Action[AnyContent] = ApplicationController.user
+
+  def signOut: Action[AnyContent] = ApplicationController.signOut
+
+  def authenticate(provider: String): Action[AnyContent] = SocialAuthController.authenticate(provider)
+
+  def signUpView(): Action[AnyContent] = SignUpController.view
+
+  def signUp(): Action[AnyContent] = SignUpController.submit
+
+  def signInView(): Action[AnyContent] = SignInController.view
+
+  def signIn(): Action[AnyContent] = SignInController.submit
+
+  def forgotPasswordView(): Action[AnyContent] = ForgotPasswordController.view
+
+  def forgotPassword(): Action[AnyContent] = ForgotPasswordController.submit
+
+  def resetPasswordView(token: UUID): Action[AnyContent] = ResetPasswordController.view(token: java.util.UUID)
+
+  def resetPassword(token: UUID): Action[AnyContent] = ResetPasswordController.submit(token: java.util.UUID)
+
+  def changePasswordView(): Action[AnyContent] = ChangePasswordController.view
+
+  def changePassword(): Action[AnyContent] = ChangePasswordController.submit
+
+  def sendActivateAccount(email: String): Action[AnyContent] = ActivateAccountController.send(email) // TODO send email per API??
+
+  def activateAccount(token: UUID): Action[AnyContent] = ActivateAccountController.activate(token: java.util.UUID)
+
+
+  // ### Webpage
+  def webpageIndex(): Action[AnyContent] = WebpageController.index()
+
+  def diagramsOverview(): Action[AnyContent] = WebpageController.diagramsOverview(uuid = null)
+
+  def diagramsOverview(uuid: String): Action[AnyContent] = WebpageController.diagramsOverview(uuid)
+
+
+  // # metamodel editor
+  def metaModelEditor(metaModelUuid: String): Action[AnyContent] = MetaModelController.metaModelEditor(metaModelUuid)
+
+  def metaModelSocket(metaModelUuid: String): WebSocket = MetaModelController.metaModelSocket(metaModelUuid)
+
+
+  // ### model editor
+  def modelEditor(metaModelUuid: String, modelUuid: String): Action[AnyContent] = ModelController.modelEditor(metaModelUuid, modelUuid)
+
+  def modelSocket(instanceId: String, graphType: String): WebSocket = ModelController.modelSocket(instanceId, graphType)
+
+  def modelValidator(): Action[AnyContent] = ModelController.modelValidator()
+
+
+  // ### vr
+  def vrModelEditor(metaModelUuid: String, modelUuid: String): Action[AnyContent] = ModelController.vrModelEditor(metaModelUuid, modelUuid)
+
+
+  // # temporary
+  def generate(metaModelUuid: String): Action[AnyContent] = GeneratorController.generate(metaModelUuid)
+
+
+  // ### MetaModel REST API
+  object MetaModelRest {
+    private lazy val MetaModelRestApi: MetaModelRestApi = injector.instanceOf[MetaModelRestApi]
+
+
+    def showForUser: Action[AnyContent] = MetaModelRestApi.showForUser
+
+    def insert: Action[JsValue] = MetaModelRestApi.insert
+
+    def update(metaModelId: String): Action[JsValue] = MetaModelRestApi.update(metaModelId)
+
+    def get(metaModelId: String): Action[AnyContent] = MetaModelRestApi.get(metaModelId)
+
+    def delete(metaModelId: String): Action[AnyContent] = MetaModelRestApi.delete(metaModelId)
+
+    def getMetaModelDefinition(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getMetaModelDefinition(metaModelId)
+
+    def updateMetaModelDefinition(metaModelId: String): Action[JsValue] = MetaModelRestApi.updateMetaModelDefinition(metaModelId)
+
+    def getMClasses(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getMClasses(metaModelId)
+
+    def getMReferences(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getMReferences(metaModelId)
+
+    def getMClass(metaModelId: String, mClassName: String): Action[AnyContent] = MetaModelRestApi.getMClass(metaModelId, mClassName)
+
+    def getMReference(metaModelId: String, mReferenceName: String): Action[AnyContent] = MetaModelRestApi.getMReference(metaModelId, mReferenceName)
+
+    def getShape(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getShape(metaModelId)
+
+    def updateShape(metaModelId: String): Action[JsValue] = MetaModelRestApi.updateShape(metaModelId)
+
+    def getStyle(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getStyle(metaModelId)
+
+    def updateStyle(metaModelId: String): Action[JsValue] = MetaModelRestApi.updateStyle(metaModelId)
+
+    def getDiagram(metaModelId: String): Action[AnyContent] = MetaModelRestApi.getDiagram(metaModelId)
+
+    def updateDiagram(metaModelId: String): Action[JsValue] = MetaModelRestApi.updateDiagram(metaModelId)
+  }
+
+
+  // ### Model REST API
+  object ModelsRest {
+    private lazy val ModelRestApi: ModelRestApi = injector.instanceOf[ModelRestApi]
+
+
+    def showForUser: Action[AnyContent] = ModelRestApi.showForUser
+
+    def insert: Action[JsValue] = ModelRestApi.insert
+
+    def update(modelId: String): Action[JsValue] = ModelRestApi.update(modelId)
+
+    def get(modelId: String): Action[AnyContent] = ModelRestApi.get(modelId)
+
+    def getModelDefinition(modelId: String): Action[AnyContent] = ModelRestApi.getModelDefinition(modelId)
+
+    def updateModel(modelId: String): Action[JsValue] = ModelRestApi.updateModel(modelId)
+
+    def getNodes(modelId: String): Action[AnyContent] = ModelRestApi.getNodes(modelId)
+
+    def getNode(modelId: String, nodeName: String): Action[AnyContent] = ModelRestApi.getNode(modelId, nodeName)
+
+    def getEdges(modelId: String): Action[AnyContent] = ModelRestApi.getEdges(modelId)
+
+    def getEdge(modelId: String, edgeName: String): Action[AnyContent] = ModelRestApi.getEdge(modelId, edgeName)
+
+    def delete(modelId: String): Action[AnyContent] = ModelRestApi.delete(modelId)
+  }
+
+
+  // ### Code Editor
+  def codeEditor(metaModelUuid: String, dslType: String): Action[AnyContent] = CodeEditorController.codeEditor(metaModelUuid, dslType)
+
+  def codeEditorSocket(metaModelUuid: String, dslType: String): WebSocket = CodeEditorController.codeSocket(metaModelUuid, dslType)
+
+
+  // # Map static resources from the /public folder to the /assets URL path
+  def assetsAt(file: String): Action[AnyContent] = Assets.at(path = "/public", file)
+
+  def webJarAssetsAt(file: String): Action[AnyContent] = WebJarAssets.at(file)
+
+  def serveDynamicFile(file: String): Action[AnyContent] = DynamicFileController.serveFile(file)
 
 
 }
