f8adb60a05e523bd6a51a656f9e9c47f78a5350e
aullik <nicolaswehrle@gmx.net>
2017-05-25 15:25:19 +0200

    create WebController.scala.

---
+++ api/server/app/controllers/WebController.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import de.htwg.zeta.server.controller.BackendController
+import de.htwg.zeta.server.controller.ApplicationController
+import de.htwg.zeta.server.controller.SocialAuthController
+import de.htwg.zeta.server.controller.SignUpController
+import de.htwg.zeta.server.controller.SignInController
+import de.htwg.zeta.server.controller.ForgotPasswordController
+import de.htwg.zeta.server.controller.ResetPasswordController
+import de.htwg.zeta.server.controller.ChangePasswordController
+import de.htwg.zeta.server.controller.ActivateAccountController
+import de.htwg.zeta.server.controller.MetaModelController
+import de.htwg.zeta.server.controller.ModelController
+import de.htwg.zeta.server.controller.GeneratorController
+import de.htwg.zeta.server.controller.CodeEditorController
+import de.htwg.zeta.server.controller.DynamicFileController
+import de.htwg.zeta.server.controller.restApi.MetaModelRestApi
+import de.htwg.zeta.server.controller.restApi.ModelRestApi
+import de.htwg.zeta.server.controller.webpage.WebpageController
+
+/**
+ */
+trait WebController {
+
+  protected val webCont: WebControllerContainer
+
+  protected lazy val BackendController: BackendController = webCont.backendController.get()
+  protected lazy val ApplicationController: ApplicationController = webCont.applicationController.get()
+  protected lazy val SocialAuthController: SocialAuthController = webCont.socialAuthController.get()
+  protected lazy val SignUpController: SignUpController = webCont.signUpController.get()
+  protected lazy val SignInController: SignInController = webCont.signInController.get()
+  protected lazy val ForgotPasswordController: ForgotPasswordController = webCont.forgotPasswordController.get()
+  protected lazy val ResetPasswordController: ResetPasswordController = webCont.resetPasswordController.get()
+  protected lazy val ChangePasswordController: ChangePasswordController = webCont.changePasswordController.get()
+  protected lazy val ActivateAccountController: ActivateAccountController = webCont.activateAccountController.get()
+  protected lazy val WebpageController: WebpageController = webCont.webpageController.get()
+  protected lazy val MetaModelController: MetaModelController = webCont.metaModelController.get()
+  protected lazy val ModelController: ModelController = webCont.modelController.get()
+  protected lazy val GeneratorController: GeneratorController = webCont.generatorController.get()
+  protected lazy val CodeEditorController: CodeEditorController = webCont.codeEditorController.get()
+  protected lazy val WebJarAssets: WebJarAssets = webCont.webJarAssets.get()
+  protected lazy val DynamicFileController: DynamicFileController = webCont.dynamicFileController.get()
+  protected lazy val MetaModelRestApi: MetaModelRestApi = webCont.metaModelRestApi.get()
+  protected lazy val ModelRestApi: ModelRestApi = webCont.modelRestApi.get()
+}
---
+++ api/server/app/controllers/WebControllerContainer.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import javax.inject.Inject
+
+import com.google.inject.Provider
+import de.htwg.zeta.server.controller.CodeEditorController
+import de.htwg.zeta.server.controller.ModelController
+import de.htwg.zeta.server.controller.GeneratorController
+import de.htwg.zeta.server.controller.BackendController
+import de.htwg.zeta.server.controller.MetaModelController
+import de.htwg.zeta.server.controller.ActivateAccountController
+import de.htwg.zeta.server.controller.ChangePasswordController
+import de.htwg.zeta.server.controller.ResetPasswordController
+import de.htwg.zeta.server.controller.ForgotPasswordController
+import de.htwg.zeta.server.controller.SignInController
+import de.htwg.zeta.server.controller.SignUpController
+import de.htwg.zeta.server.controller.SocialAuthController
+import de.htwg.zeta.server.controller.ApplicationController
+import de.htwg.zeta.server.controller.DynamicFileController
+import de.htwg.zeta.server.controller.restApi.MetaModelRestApi
+import de.htwg.zeta.server.controller.restApi.ModelRestApi
+import de.htwg.zeta.server.controller.webpage.WebpageController
+
+/**
+ */
+class WebControllerContainer @Inject() private(
+    val backendController: Provider[BackendController],
+    val applicationController: Provider[ApplicationController],
+    val socialAuthController: Provider[SocialAuthController],
+    val signUpController: Provider[SignUpController],
+    val signInController: Provider[SignInController],
+    val forgotPasswordController: Provider[ForgotPasswordController],
+    val resetPasswordController: Provider[ResetPasswordController],
+    val changePasswordController: Provider[ChangePasswordController],
+    val activateAccountController: Provider[ActivateAccountController],
+    val webpageController: Provider[WebpageController],
+    val metaModelController: Provider[MetaModelController],
+    val modelController: Provider[ModelController],
+    val generatorController: Provider[GeneratorController],
+    val codeEditorController: Provider[CodeEditorController],
+    val webJarAssets: Provider[WebJarAssets],
+    val dynamicFileController: Provider[DynamicFileController],
+    val metaModelRestApi: Provider[MetaModelRestApi],
+    val modelRestApi: Provider[ModelRestApi])
