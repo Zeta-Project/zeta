537f145c6d057bcdb278b9a26baa07a6e739d09d
Tobias Droth <todroth@htwg-konstanz.de>
2017-06-01 14:54:30 +0200

    added tests for EdgeAttributeScalarTypes und EdgeAttributesGlobalUnique

---
+++ api/server/test/de/htwg/zeta/server/model/modelValidator/validator/rules/metaModelDependent/EdgeAttributeScalarTypesTest.scala
@@ -0,0 +1,47 @@
+package de.htwg.zeta.server.model.modelValidator.validator.rules.metaModelDependent
+
+import scala.collection.immutable.Seq
+
+import models.modelDefinitions.metaModel.elements.MAttribute
+import models.modelDefinitions.metaModel.elements.MLinkDef
+import models.modelDefinitions.metaModel.elements.MReference
+import models.modelDefinitions.metaModel.elements.ScalarType
+import models.modelDefinitions.metaModel.elements.ScalarValue.MInt
+import models.modelDefinitions.metaModel.elements.ScalarValue.MString
+import models.modelDefinitions.model.elements.Attribute
+import models.modelDefinitions.model.elements.Edge
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class EdgeAttributeScalarTypesTest extends FlatSpec with Matchers {
+
+  val mReference = MReference("reference", sourceDeletionDeletesTarget = false, targetDeletionDeletesSource = false, Seq[MLinkDef](), Seq[MLinkDef](), Seq[MAttribute]())
+  val rule = new EdgeAttributeScalarTypes("reference", "attributeType", ScalarType.String)
+
+  "the rule" should "be true for valid edges" in {
+    val attribute = Attribute(name = "attributeType", value = Seq(MString("value")))
+    val edge = Edge.apply2("edgeId", mReference, Seq(), Seq(), Seq(attribute))
+
+    rule.isValid(edge).get should be (true)
+  }
+
+  it should "be false for invalid edges" in {
+    val attribute = Attribute(name = "attributeType", value = Seq(MInt(42)))
+    val edge = Edge.apply2("edgeId", mReference, Seq(), Seq(), Seq(attribute))
+
+    rule.isValid(edge).get should be (false)
+  }
+
+  it should "return None for non-matching edges" in {
+    val differentMReference = MReference("differentMReference", sourceDeletionDeletesTarget = false, targetDeletionDeletesSource = false, Seq[MLinkDef](), Seq[MLinkDef](), Seq[MAttribute]())
+    val attribute = Attribute(name = "attributeType", value = Seq(MString("value")))
+    val edge = Edge.apply2("edgeId", differentMReference, Seq(), Seq(), Seq(attribute))
+
+    rule.isValid(edge) should be (None)
+  }
+
+  "dslStatement" should "return the correct string" in {
+    rule.dslStatement should be ("""Attributes ofType "attributeType" inEdges "reference" areOfScalarType "String"""")
+  }
+
+}
---
+++ api/server/test/de/htwg/zeta/server/model/modelValidator/validator/rules/metaModelDependent/EdgeAttributesGlobalUniqueTest.scala
@@ -0,0 +1,79 @@
+package de.htwg.zeta.server.model.modelValidator.validator.rules.metaModelDependent
+
+import scala.collection.immutable.Seq
+
+import models.modelDefinitions.metaModel.elements.MAttribute
+import models.modelDefinitions.metaModel.elements.MLinkDef
+import models.modelDefinitions.metaModel.elements.MReference
+import models.modelDefinitions.metaModel.elements.ScalarValue.MString
+import models.modelDefinitions.model.elements.Attribute
+import models.modelDefinitions.model.elements.Edge
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class EdgeAttributesGlobalUniqueTest extends FlatSpec with Matchers {
+
+  val mReference = MReference("edgeType", sourceDeletionDeletesTarget = false, targetDeletionDeletesSource = false, Seq[MLinkDef](), Seq[MLinkDef](), Seq[MAttribute]())
+  def rule = new EdgeAttributesGlobalUnique("edgeType", "attributeType")
+
+  "check" should "return success validation results on correct attributes" in {
+
+    val attributeOne = Attribute(name = "attributeType", value = Seq(MString("value")))
+    val edgeOne = Edge.apply2("edge1Id", mReference, Seq(), Seq(), Seq(attributeOne))
+
+    val attributeTwo = Attribute(name = "attributeType", value = Seq(MString("differentValue")))
+    val edgeTwo = Edge.apply2("edge2Id", mReference, Seq(), Seq(), Seq(attributeTwo))
+
+    val results = rule.check(Seq(edgeOne, edgeTwo))
+
+    results.size should be (2)
+    results.forall(_.valid) should be (true)
+
+    val attributeThree = Attribute(name = "attributeType", value = Seq(MString("anotherValue"), MString("yetAnotherValue")))
+    val edgeThree = Edge.apply2("edge3Id", mReference, Seq(), Seq(), Seq(attributeThree))
+
+    val secondResults = rule.check(Seq(edgeOne, edgeTwo, edgeThree))
+
+    secondResults.size should be (3)
+    secondResults.forall(_.valid) should be (true)
+
+  }
+
+  it should "return failure validation results on invalid attributes" in {
+    val attributeOne = Attribute(name = "attributeType", value = Seq(MString("duplicateValue")))
+    val edgeOne = Edge.apply2("edge1Id", mReference, Seq(), Seq(), Seq(attributeOne))
+
+    val attributeTwo = Attribute(name = "attributeType", value = Seq(MString("duplicateValue")))
+    val edgeTwo = Edge.apply2("edge2Id", mReference, Seq(), Seq(), Seq(attributeTwo))
+
+    val results = rule.check(Seq(edgeOne, edgeTwo))
+
+    results.size should be (2)
+    results.head.valid should be (false)
+    results.last.valid should be (false)
+
+    val attributeThree = Attribute(name = "attributeType", value = Seq(MString("duplicateValue"), MString("anotherValue")))
+    val edgeThree = Edge.apply2("edge3Id", mReference, Seq(), Seq(), Seq(attributeThree))
+
+    val secondResults = rule.check(Seq(edgeOne, edgeTwo, edgeThree))
+
+    secondResults.size should be (3)
+    secondResults.forall(!_.valid) should be (true)
+
+    val attributeFour = Attribute(name = "attributeType", value = Seq(MString("differentValue")))
+    val edgeFour = Edge.apply2("edge4Id", mReference, Seq(), Seq(), Seq(attributeFour))
+
+    val thirdResults = rule.check(Seq(edgeOne, edgeTwo, edgeThree, edgeFour))
+
+    thirdResults.size should be (4)
+    thirdResults.head.valid should be (false)
+    thirdResults(1).valid should be (false)
+    thirdResults(2).valid should be (false)
+    thirdResults(3).valid should be (true)
+  }
+
+  "dslStatement" should "return the correct string" in {
+    rule.dslStatement should be ("""Attributes ofType "attributeType" inEdges "edgeType" areGlobalUnique ()""")
+  }
+
+}
