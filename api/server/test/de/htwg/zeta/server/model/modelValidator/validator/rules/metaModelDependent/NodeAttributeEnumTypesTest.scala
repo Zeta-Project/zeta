2f088ce96440e5702ec7053be8114d695a1ee574
Tobias Droth <todroth@htwg-konstanz.de>
2017-06-02 11:47:50 +0200

    added more tests

---
+++ api/server/test/de/htwg/zeta/server/model/modelValidator/validator/rules/metaModelDependent/NodeAttributeEnumTypesTest.scala
@@ -0,0 +1,8 @@
+package de.htwg.zeta.server.model.modelValidator.validator.rules.metaModelDependent
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class NodeAttributeEnumTypesTest extends FlatSpec with Matchers {
+
+}
