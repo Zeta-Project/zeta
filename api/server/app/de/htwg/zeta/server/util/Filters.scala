ad62ec0046ed4053b809953916965b539341b248
Patrick Leber <patrick.leber@freenet.de>
2017-06-08 13:11:34 +0200

    Add a LoggingFilter to log all requests

--- api/server/app/de/htwg/zeta/server/util/Filters.scala
+++ api/server/app/de/htwg/zeta/server/util/Filters.scala
@@ -1,15 +1,49 @@
 package de.htwg.zeta.server.util
 
+
 import javax.inject.Inject
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import akka.stream.Materializer
+import play.api.Logger
 import play.api.http.HttpFilters
 import play.api.mvc.EssentialFilter
+import play.api.mvc.Filter
+import play.api.mvc.RequestHeader
+import play.api.mvc.Result
 import play.filters.csrf.CSRFFilter
 import play.filters.headers.SecurityHeadersFilter
 
 /**
  * Provides filters.
  */
-class Filters @Inject() (csrfFilter: CSRFFilter, securityHeadersFilter: SecurityHeadersFilter) extends HttpFilters {
-  override def filters: Seq[EssentialFilter] = Seq(csrfFilter, securityHeadersFilter)
+class Filters @Inject() (csrfFilter: CSRFFilter, securityHeadersFilter: SecurityHeadersFilter, mat: Materializer, ec: ExecutionContext) extends HttpFilters {
+
+  override def filters: Seq[EssentialFilter] = Seq(csrfFilter, securityHeadersFilter, new LoggingFilter()(mat, ec))
+
+}
+
+/** Simple Filter to log all requests.
+ *
+ * @param mat Materializer
+ * @param ec ExecutionContext
+ */
+class LoggingFilter()(implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
+
+  def apply(nextFilter: RequestHeader => Future[Result])
+    (requestHeader: RequestHeader): Future[Result] = {
+
+    val startTime = System.currentTimeMillis
+
+    nextFilter(requestHeader).map { result =>
+      val endTime = System.currentTimeMillis
+      val requestTime = endTime - startTime
+
+      Logger.info(s"${requestHeader.method} ${requestHeader.uri} (took ${requestTime}ms and returned ${result.header.status})")
+      result.withHeaders("Request-Time" -> requestTime.toString)
+    }
+  }
+
 }
