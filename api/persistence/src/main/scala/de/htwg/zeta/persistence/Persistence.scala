fa1aade786403c2807db99684f20be0e922e1617
Patrick Leber <patrick.leber@freenet.de>
2017-06-17 14:47:27 +0200

    change the default repository implementation to MongoRepository with an overlying ActorCacheRepository

--- api/persistence/src/main/scala/de/htwg/zeta/persistence/Persistence.scala
+++ api/persistence/src/main/scala/de/htwg/zeta/persistence/Persistence.scala
@@ -3,6 +3,7 @@ package de.htwg.zeta.persistence
 import java.util.UUID
 
 import de.htwg.zeta.persistence.accessRestricted.AccessRestrictedRepository
+import de.htwg.zeta.persistence.actorCache.ActorCacheRepository
 import de.htwg.zeta.persistence.general.Repository
 import de.htwg.zeta.persistence.general.TokenCache
 import de.htwg.zeta.persistence.mongo.MongoRepository
@@ -12,10 +13,10 @@ import de.htwg.zeta.persistence.transient.TransientTokenCache
 /** Persistence. */
 object Persistence {
 
-  /** The current implementation of the PersistenceService. */
-  val fullAccessRepository: Repository = new MongoRepository("localhost:27017", "zeta")
+  /** The current implementation of the Repository. */
+  val fullAccessRepository: Repository = new ActorCacheRepository(new MongoRepository("localhost:27017", "zeta"))
 
-  /** The current implementation of the PersistenceService with a overlaying Access-Restriction Layer.
+  /** The current implementation of the Repository with an overlying Access-Restriction Layer.
    *
    * @param ownerID The id of the assigned user to the restriction
    * @return PersistenceService
