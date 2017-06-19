5958d82e630d251cf0929a9f98a0d39a7e2e831c
aullik <nicolaswehrle@gmx.net>
2017-06-19 02:28:33 +0200

    update HostIP.scala to work in a single application

--- api/common/src/main/scala/de/htwg/zeta/common/cluster/HostIP.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/cluster/HostIP.scala
@@ -1,22 +1,34 @@
 package de.htwg.zeta.common.cluster
 
-object HostIP {
+import java.net.InetAddress
 
-  import java.net.InetAddress
+import scala.util.Try
 
-  import scala.util.Try
+import grizzled.slf4j.Logging
+
+
+object HostIP extends Logging {
+
 
   def load(): String = InetAddress.getLocalHost.getHostAddress
 
   def lookupNodeAddress(value: String) = {
-
     val node = """(\w*):*(\d*)""".r
 
-    value match {
+    val ret = value match {
       case node(hostname, port) =>
-        Try(InetAddress.getByName(hostname).getHostAddress).map(host => s"$host:$port").getOrElse(value)
+        Try(InetAddress.getByName(hostname).getHostAddress).recover {
+          case _ => hostname match {
+            case "b1" => load()
+            case _ => value
+          }
+        }.map(host => s"$host:$port").getOrElse(value)
       case _ => value
     }
+
+    info(s"looked up seed: $value. Found: $ret")
+
+    ret
   }
 
   def lookupNode(hostname: String) = {
