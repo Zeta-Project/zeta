80fd2579075790b1fa17d383d4bbf4742939c360
aullik <nicolaswehrle@gmx.net>
2017-06-26 16:18:22 +0200

    redid exceptions in HostIP

--- api/common/src/main/scala/de/htwg/zeta/common/cluster/HostIP.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/cluster/HostIP.scala
@@ -1,6 +1,7 @@
 package de.htwg.zeta.common.cluster
 
 import java.net.InetAddress
+import java.net.UnknownHostException
 
 import grizzled.slf4j.Logging
 
@@ -13,33 +14,40 @@ object HostIP extends Logging {
   @SuppressWarnings(Array("org.wartremover.warts.Throw"))
   def lookupNodeAddress(address: String): String = {
     val ret = address match {
-      case IpAddress(parsedAddress) => parsedAddress
+      case AddressParser(addr, port) =>
+        IpPort.parseIpPort(addr, port) match {
+          case Some(parsedAddress) => parsedAddress
+          case None => throw new IllegalArgumentException(s"cannot lookup node address: $address. Could not resolve Host: ${addr}")
+        }
+
       case _ =>
-        throw new IllegalArgumentException(s"cannot lookup node address: $address. address must have format: ip:port")
+        val msg = s"cannot lookup node address: $address. address must have format: ip:port. port must be in range 0 to ${AddressParser.maxPortNumber}"
+        throw new IllegalArgumentException(msg)
     }
     info(s"looked up seed: $address. Found: $ret")
     ret
   }
 
   def lookupNodeAddressOption(address: String): Option[String] = {
-    IpAddress.unapply(address)
+    AddressParser.unapply(address).flatMap(address => IpPort.unapply(address))
   }
 
-  object IpAddress {
-    private val delimiter = ":"
-    private val maxPortNumber = 65535
 
-    private def parseAddress(addressName: String): Option[String] = {
+  sealed trait AddressParser {
+    protected val delimiter = ":"
+    protected[HostIP] val maxPortNumber = 65535
+
+    protected def parseAddress(addressName: String): Option[String] = {
       val address =
         if (addressName == "localhost") {
           HostIP.load()
         } else {
           InetAddress.getByName(addressName).getHostAddress
         }
-      Some(address)
+      Option(address)
     }
 
-    private def parsePort(portString: String): Option[Int] = {
+    protected def parsePort(portString: String): Option[Int] = {
       val port = portString.toInt
       if (port < 0 || port > maxPortNumber) {
         None
@@ -48,18 +56,34 @@ object HostIP extends Logging {
         Some(port)
       }
     }
+  }
 
-    def unapply(ip: String): Option[String] = try {
+  object AddressParser extends AddressParser {
+    def unapply(ip: String): Option[(String, Int)] = {
       val s = ip.split(delimiter, -1).toList
       s.reverse match {
         // list size must be at least 2. tail can be Nil
         case stringPort :: head :: tail =>
-          parsePort(stringPort).flatMap(port => parseAddress((head :: tail).reverse.mkString(delimiter)).map(address => s"$address$delimiter$port"))
+          parsePort(stringPort).map(port => ((head :: tail).reverse.mkString(delimiter), port))
+      }
+    }
+  }
+
+  object IpPort extends AddressParser {
+    def unapply(pair: (String, Int)): Option[String] = try {
+      pair match {
+        case (ip, port) if port >= 0 && port <= maxPortNumber => parseIpPort(ip, port)
         case _ => None
       }
     } catch {
-      case _: Throwable => None
+      case _: UnknownHostException => None
+      case _: NoSuchElementException => None
     }
+
+    def parseIpPort(address: String, port: Int): Option[String] = {
+      parseAddress(address).map(ip => s"$ip$delimiter$port")
+    }
+
   }
 
 }
