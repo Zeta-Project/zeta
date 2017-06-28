ab62b4f322ec0d056dbcaadabd3a5bc6dbfe6044
aullik <nicolaswehrle@gmx.net>
2017-06-28 16:58:53 +0200

    move Mediator functionality to AbstractMediatorActor.scala

--- api/server/app/de/htwg/zeta/server/model/codeEditor/CodeDocManagingActor.scala
+++ api/server/app/de/htwg/zeta/server/model/codeEditor/CodeDocManagingActor.scala
@@ -4,11 +4,9 @@ import java.util.UUID
 
 import scala.collection.mutable
 
-import akka.actor.Actor
 import akka.actor.ActorLogging
-import akka.actor.ActorRef
 import akka.actor.Props
-import akka.actor.Terminated
+import de.htwg.zeta.server.actor.AbstractMediatorActor
 import scalot.Server
 import scalot.Operation
 import shared.CodeEditorMessage.DocAdded
@@ -26,19 +24,16 @@ object CodeDocManagingActor {
 /**
  * This Actor takes care of applying the changed to the documents.
  */
-class CodeDocManagingActor extends Actor with ActorLogging {
+class CodeDocManagingActor extends AbstractMediatorActor with ActorLogging {
 
   private val codeDocuments: mutable.Set[CodeDocument] = mutable.Set.empty
-  private val subscriberCache: mutable.HashMap[String, mutable.HashSet[ActorRef]] = mutable.HashMap.empty
 
-
-  def receive: Receive = {
+  override def childReceive: Receive = {
     case TextOperation(op, docId) => handleTextOperation(op, docId)
     case newDoc: DocAdded => handleDocAdded(newDoc)
     case msg: DocDeleted => handleDocDeleted(msg)
-    case CodeDocManagingActor.SubscribeTo(dslType) => handleSubscribeTo(dslType)
-    case Terminated(deadRef) => handleSubscriberDeath(deadRef)
   }
+
 
   private def handleTextOperation(op: Operation, docId: UUID): Unit = {
     codeDocuments.find(_.id == docId).fold() { doc =>
@@ -75,33 +70,6 @@ class CodeDocManagingActor extends Actor with ActorLogging {
     codeDocuments.find(_.id == msg.id).fold()(codeDocuments.remove)
     publish(msg.dslType, MediatorMessage(msg, sender()))
     sender() ! MediatorMessage(msg, sender())
-  }
-
-
-  private def publish(dslType: String, msg: Any): Unit = {
-    subscriberCache.get(dslType).foreach(_.foreach(_.tell(msg, self)))
-  }
-
-  private def handleSubscribeTo(dslType: String): Unit = {
-    val set = subscriberCache.getOrElseUpdate(dslType, mutable.HashSet.empty)
-    val sub = sender()
-    context.watch(sub)
-    set.add(sender())
-  }
-
-  private def handleSubscriberDeath(deadRef: ActorRef): Unit = {
-    var toDelete: List[String] = Nil
-    subscriberCache.foreach(pair => {
-      val key = pair._1
-      val set = pair._2
-
-      if (set.remove(deadRef) && set.isEmpty) {
-        // safe to var. Do not alter map while iterating over it.
-        toDelete = key :: toDelete
-      }
-    })
-
-    toDelete.foreach(subscriberCache.remove)
   }
 
 }
