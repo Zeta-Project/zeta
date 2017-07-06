df8231c8ad12121764d5e675395d873ccf4f32c8
Philipp Daniels <philipp.daniels@gmail.com>
2017-07-07 00:05:53 +0200

    Extend GeneratorImage model with description and options

--- api/persistence/src/test/scala/de/htwg/zeta/persistence/fixtures/GeneratorImageFixtures.scala
+++ api/persistence/src/test/scala/de/htwg/zeta/persistence/fixtures/GeneratorImageFixtures.scala
@@ -3,6 +3,9 @@ package de.htwg.zeta.persistence.fixtures
 import java.util.UUID
 
 import de.htwg.zeta.common.models.entity.GeneratorImage
+import de.htwg.zeta.common.models.entity.GeneratorImageOptions
+import de.htwg.zeta.common.models.entity.GeneratorOptionProperties
+import de.htwg.zeta.common.models.entity.GeneratorNameProperty
 
 
 object GeneratorImageFixtures {
@@ -10,13 +13,27 @@ object GeneratorImageFixtures {
   val entity1 = GeneratorImage(
     id = UUID.randomUUID,
     name = "filterImage1",
-    dockerImage = "dockerImage1"
+    description = "desc",
+    dockerImage = "dockerImage1",
+    options = GeneratorImageOptions(
+      schema = "http://json-schema.org/schema#",
+      title = "option title",
+      optionType = "object",
+      properties = GeneratorOptionProperties()
+    )
   )
 
   val entity2 = GeneratorImage(
     id = UUID.randomUUID,
     name = "filterImage2",
-    dockerImage = "dockerImage2"
+    description = "desc",
+    dockerImage = "dockerImage2",
+      options = GeneratorImageOptions(
+      schema = "http://json-schema.org/schema#",
+      title = "option title",
+      optionType = "object",
+      properties = GeneratorOptionProperties()
+    )
   )
 
   val entity2Updated: GeneratorImage = entity2.copy(name = "filterImage2Updated")
@@ -24,7 +41,20 @@ object GeneratorImageFixtures {
   val entity3 = GeneratorImage(
     id = UUID.randomUUID,
     name = "filterImage3",
-    dockerImage = "dockerImage3"
+    description = "desc",
+    dockerImage = "dockerImage3",
+      options = GeneratorImageOptions(
+      schema = "http://json-schema.org/schema#",
+      title = "option title",
+      optionType = "object",
+      properties = GeneratorOptionProperties(
+        name = Some(GeneratorNameProperty(
+          title = "Name",
+          propertyType = "String",
+          required = true
+        ))
+      )
+    )
   )
 
 }
