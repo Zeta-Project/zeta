069250c20f147b40e7d0f4aec4eb621ac0dcb817
Philipp Daniels <philipp.daniels@gmail.com>
2017-07-11 09:06:54 +0200

    Replace implicit Writes with handcrafted Writes

--- api/common/src/main/scala/de/htwg/zeta/common/models/frontend/Developer.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/models/frontend/Developer.scala
@@ -6,13 +6,14 @@ import scala.collection.immutable.Queue
 
 import de.htwg.zeta.common.models.worker.Job
 import grizzled.slf4j.Logging
+import play.api.libs.json.JsArray
 import play.api.libs.json.JsObject
-import play.api.libs.json.JsString
 import play.api.libs.json.Json
-import play.api.libs.json.Writes
-import play.api.libs.json.Reads
-import play.api.libs.json.JsValue
 import play.api.libs.json.JsResult
+import play.api.libs.json.JsString
+import play.api.libs.json.JsValue
+import play.api.libs.json.Reads
+import play.api.libs.json.Writes
 
 /**
  * Request messages which can be send by a Tool-Developer
@@ -23,7 +24,6 @@ case class RunFilter(filterId: UUID) extends DeveloperRequest
 case class CreateGenerator(imageId: UUID, options: String) extends DeveloperRequest
 case class RunModelRelease(model: String) extends DeveloperRequest
 case class CancelWorkByUser(id: String) extends DeveloperRequest
-
 
 object DeveloperRequest extends Logging {
 
@@ -89,10 +89,11 @@ object DeveloperRequest extends Logging {
  * Response messages which can be send to a Tool-Developer
  */
 sealed trait DeveloperResponse extends Response
-case class GeneratorImageNotFoundFailure(message: String = "Generator Image was not found") extends DeveloperResponse
-case class ExecuteGeneratorError(message: String = "Generator or Filter was not found") extends DeveloperResponse
-case class ExecuteFilterError(message: String = "Filter was not found") extends DeveloperResponse
-case class ServiceUnavailable(message: String = "Service not available") extends DeveloperResponse
+sealed trait ErrorResponse extends DeveloperResponse { def message: String }
+case class GeneratorImageNotFoundFailure(message: String = "Generator Image was not found") extends ErrorResponse
+case class ExecuteGeneratorError(message: String = "Generator or Filter was not found") extends ErrorResponse
+case class ExecuteFilterError(message: String = "Filter was not found") extends ErrorResponse
+case class ServiceUnavailable(message: String = "Service not available") extends ErrorResponse
 case class JobInfo(id: String, job: Job, state: String) extends DeveloperResponse
 case class JobInfoList(jobs: List[JobInfo], running: Int, pending: Int, waiting: Int, canceled: Int) extends DeveloperResponse
 case class JobLogMessage(message: String, sort: String) extends DeveloperResponse
@@ -100,26 +101,70 @@ case class JobLog(job: String, messages: Queue[JobLogMessage] = Queue.empty) ext
   override def toString: String = messages.map(log => log.message).mkString
 }
 
-object DeveloperResponse {
-  implicit val writeGeneratorImageNotFoundFailure: Writes[GeneratorImageNotFoundFailure] = Json.writes[GeneratorImageNotFoundFailure]
-  implicit val writeGeneratorNotFoundFailure: Writes[ExecuteGeneratorError] = Json.writes[ExecuteGeneratorError]
-  implicit val writeFilterNotFoundFailure: Writes[ExecuteFilterError] = Json.writes[ExecuteFilterError]
-  implicit val writeServiceUnavailable: Writes[ServiceUnavailable] = Json.writes[ServiceUnavailable]
-  implicit val writeJobInfo: Writes[JobInfo] = Json.writes[JobInfo]
-  implicit val writeJobInfoList: Writes[JobInfoList] = Json.writes[JobInfoList]
-  implicit val writeJobLogMessage: Writes[JobLogMessage] = Json.writes[JobLogMessage]
-  implicit val writeJobLog: Writes[JobLog] = Json.writes[JobLog]
+object DeveloperResponse extends Logging {
+  private val attribute = "type"
 
   implicit val write = new Writes[DeveloperResponse] {
-    override def writes(response: DeveloperResponse): JsObject = response match {
-      case s: GeneratorImageNotFoundFailure => Json.toJson(s).as[JsObject] + (("type", JsString("Error")))
-      case s: ExecuteGeneratorError => Json.toJson(s).as[JsObject] + (("type", JsString("Error")))
-      case s: ExecuteFilterError => Json.toJson(s).as[JsObject] + (("type", JsString("Error")))
-      case s: ServiceUnavailable => Json.toJson(s).as[JsObject] + (("type", JsString("Error")))
-      case s: JobInfo => Json.toJson(s).as[JsObject] + (("type", JsString("JobInfo")))
-      case s: JobInfoList => Json.toJson(s).as[JsObject] + (("type", JsString("JobInfoList")))
-      case s: JobLogMessage => Json.toJson(s).as[JsObject] + (("type", JsString("JobLogMessage")))
-      case s: JobLog => Json.toJson(s).as[JsObject] + (("type", JsString("JobLog")))
+    override def writes(response: DeveloperResponse): JsObject = {
+      try {
+        processWrites(response)
+      } catch {
+        case e: Throwable =>
+          error("Failed parse jsob")
+          createErrorResponse("Unknown parse error")
+      }
+    }
+
+    private def processWrites(response: DeveloperResponse): JsObject = {
+      response match {
+        case s: ErrorResponse => createErrorResponse(s.message)
+        case s: JobInfo => createJobInfo(s) + ((attribute, JsString("JobInfo")))
+        case s: JobInfoList => createJobInfoList(s) + ((attribute, JsString("JobInfoList")))
+        case s: JobLogMessage => createJobLogMessage(s) + ((attribute, JsString("JobLogMessage")))
+        case s: JobLog => createJobLog(s) + ((attribute, JsString("JobLog")))
+        case _ =>
+          error("Unknown DeveloperResponse: " + response.toString)
+          createErrorResponse("Unknown object to parse")
+      }
+    }
+
+    private def createErrorResponse(message: String): JsObject = {
+      Json.obj(
+        attribute -> "Error",
+        "message" -> message
+      )
+    }
+
+    private def createJobInfoList(o: JobInfoList): JsObject = {
+      Json.obj(
+        "jobs" -> JsArray(o.jobs.map(createJobInfo)),
+        "running" -> o.running,
+        "pending" -> o.pending,
+        "waiting" -> o.waiting,
+        "canceled" -> o.canceled
+      )
+    }
+
+    private def createJobInfo(o: JobInfo): JsObject = {
+      Json.obj(
+        "id" -> o.id,
+        "job" -> o.job,
+        "state" -> o.state
+      )
+    }
+
+    private def createJobLogMessage(o: JobLogMessage): JsObject = {
+      Json.obj(
+        "message" -> o.message,
+        "sort" -> o.sort
+      )
+    }
+
+    private def createJobLog(o: JobLog): JsObject = {
+      Json.obj(
+        "job" -> o.job,
+        "messages" -> JsArray(o.messages.map(createJobLogMessage))
+      )
     }
   }
 }
