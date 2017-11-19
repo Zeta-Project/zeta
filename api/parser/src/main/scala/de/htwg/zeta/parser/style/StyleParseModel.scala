dc242346c114ccd455f2e9d2973b62ea049e05a5
Simon Keller <simon.keller@skillworks.de>
2017-11-19 14:36:39 +0100

    define/implement all valid style attributes

--- api/parser/src/main/scala/de/htwg/zeta/parser/style/StyleParseModel.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/style/StyleParseModel.scala
@@ -4,10 +4,24 @@ package de.htwg.zeta.parser.style
 sealed trait StyleAttribute {
   val attributeName: String
 }
-case class LineColor(color: String) extends StyleAttribute { val attributeName = "line-color" }
-case class LineStyle(style: String) extends StyleAttribute { val attributeName = "line-style" }
-case class LineWidth(width: Int)    extends StyleAttribute { val attributeName = "line-width" }
-
+case class LineColor(color: String)                 extends StyleAttribute { val attributeName = "line-color" }
+case class LineStyle(style: String)                 extends StyleAttribute { val attributeName = "line-style" }
+case class LineWidth(width: Int)                    extends StyleAttribute { val attributeName = "line-width" }
+case class Transparency(transparency: Double)       extends StyleAttribute { val attributeName = "transparency" }
+case class BackgroundColor(color: String)           extends StyleAttribute { val attributeName = "background-color" }
+case class FontColor(color: String)                 extends StyleAttribute { val attributeName = "font-color" }
+case class FontName(name: String)                   extends StyleAttribute { val attributeName = "font-name" }
+case class FontSize(size: Int)                      extends StyleAttribute { val attributeName = "font-size" }
+case class FontBold(bold: String)                   extends StyleAttribute { val attributeName = "font-bold" }
+case class FontItalic(italic: String)               extends StyleAttribute { val attributeName = "font-italic" }
+case class GradientOrientation(orientation: String) extends StyleAttribute { val attributeName = "gradient-orientation" }
+case class GradientAreaColor(color: String)         extends StyleAttribute { val attributeName = "gradient-area-color" }
+case class GradientAreaOffset(offset: String)       extends StyleAttribute { val attributeName = "gradient-area-offset" }
+// case class Allowed(allowed: Boolean)                extends StyleAttribute { val attributeName = "allowed" }
+// case class UnAllowed(unAllowed: Boolean)            extends StyleAttribute { val attributeName = "unallowed" }
+// case class Selected(selected: Boolean)              extends StyleAttribute { val attributeName = "selected" }
+// case class MultiSelected(multiSelected: Boolean)    extends StyleAttribute { val attributeName = "multiselected" }
+// case class Highlighting(highlighting: String)       extends StyleAttribute { val attributeName = "highlighting" }
 
 case class StyleParseModel(
     name: String,
--- api/parser/src/main/scala/de/htwg/zeta/parser/style/StyleParserImpl.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/style/StyleParserImpl.scala
@@ -3,7 +3,7 @@ package de.htwg.zeta.parser.style
 
 class StyleParserImpl extends StyleParser {
 
-  private val leftBraces  = literal("{")
+  private val leftBraces = literal("{")
   private val rightBraces = literal("}")
   private val eq = literal("=")
   private val comma = literal(",")
@@ -21,14 +21,16 @@ class StyleParserImpl extends StyleParser {
   }
 
   private def attributes: Parser[List[StyleAttribute]] = {
-    rep(lineColor | lineStyle | lineWidth).flatMap { attributes =>
-      Parser { in =>
-        findDuplicates(attributes) match {
-          case Nil => Success(attributes, in)
-          case duplicateAttributes => failureDuplicateAttributes(duplicateAttributes, in)
+    rep(lineColor | lineStyle | lineWidth | transparency | backgroundColor | fontColor | fontName | fontSize
+      | fontBold | fontItalic | gradientOrientation | gradientAreaColor | gradientAreaOffset)
+      .flatMap { attributes =>
+        Parser { in =>
+          findDuplicates(attributes) match {
+            case Nil => Success(attributes, in)
+            case duplicateAttributes => failureDuplicateAttributes(duplicateAttributes, in)
+          }
         }
       }
-    }
   }
 
   def findDuplicates(attributeList: List[StyleAttribute]): List[String] = {
@@ -43,26 +45,32 @@ class StyleParserImpl extends StyleParser {
       |'${duplicates.mkString(", ")}'
       """.stripMargin, in)
 
-  /*
-  val validStyleAttributes = List("description", "transparency", "background-color", "line-color", "line-style", "line-width",
-    "font-color", "font-name", "font-size", "font-bold", "font-italic", "gradient-orientation", "gradient-area-color",
-    "gradient-area-offset", "allowed", "unallowed", "selected", "multiselected", "highlighting")
-   */
   private def name = literal("style") ~> ident
   private def description = literal("description") ~ eq ~> argument_string
   private def lineColor = literal("line-color") ~ eq ~> argument ^^ (arg => LineColor(arg))
   private def lineStyle = literal("line-style") ~ eq ~> argument ^^ (arg => LineStyle(arg))
   private def lineWidth = literal("line-width") ~ eq ~> argument_int ^^ (arg => LineWidth(arg))
+  private def transparency = literal("transparency") ~ eq ~> argument_double ^^ (arg => Transparency(arg))
+  private def backgroundColor = literal("background-color") ~ eq ~> argument ^^ (arg => BackgroundColor(arg))
+  private def fontColor = literal("font-color") ~ eq ~> argument ^^ (arg => FontColor(arg))
+  private def fontName = literal("font-name") ~ eq ~> argument ^^ (arg => FontName(arg))
+  private def fontSize = literal("font-size") ~ eq ~> argument_int ^^ (arg => FontSize(arg))
+  private def fontBold = literal("font-bold") ~ eq ~> argument ^^ (arg => FontBold(arg))
+  private def fontItalic = literal("font-italic") ~ eq ~> argument ^^ (arg => FontItalic(arg))
+  private def gradientOrientation = literal("gradient-orientation") ~ eq ~> argument ^^ (arg => GradientOrientation(arg))
+  private def gradientAreaColor = literal("gradient-area-color") ~ eq ~> argument ^^ (arg => GradientAreaColor(arg))
+  private def gradientAreaOffset = literal("gradient-area-offset") ~ eq ~> argument ^^ (arg => GradientAreaOffset(arg))
+  // private def allowed = literal("allowed") ~ eq ~> argument ^^ (arg => Allowed(arg))
+  // private def unAllowed = literal("unallowed") ~ eq ~> argument ^^ (arg => UnAllowed(arg))
+  // private def selected = literal("selected") ~ eq ~> argument ^^ (arg => Selected(arg))
+  // private def multiSelected = literal("multiselected") ~ eq ~> argument ^^ (arg => MultiSelected(arg))
+  // private def highlighting = literal("highlighting") ~ eq ~> argument ^^ (arg => Highlighting(arg))
 
   private def parentStyles = literal("extends") ~> ident ~ rep(comma ~> ident) ^^ (parents => parents._1 :: parents._2)
 
   // todo: 1. define all valid attribute keys (see above)
   // todo: 4. tests ...
   // todo: 5. function: InternalStyleModel -> StyleModel
-  private def gradientOrientation = literal("gradient-orientation") ~ eq ~> argument
-  private def backgroundColor = literal("background-color") ~ eq ~> argument
-  private def fontSize = literal("font-size") ~ eq ~> argument_int
-
 
 
 }
