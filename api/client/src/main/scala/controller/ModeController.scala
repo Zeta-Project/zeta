c9a423daeb80c58cb3a27fff6d836a14a27411bd
Philipp Daniels <philipp.daniels@gmail.com>
2017-11-22 03:44:12 +0100

    Close #14 by migrate AceGrammar definition to JavaScript

--- api/client/src/main/scala/controller/ModeController.scala
+++ api/client/src/main/scala/controller/ModeController.scala
@@ -5,10 +5,6 @@ import java.util.UUID
 object ModeController {
 
   def getAllModesForModel(modelId: UUID): Map[String, scalajs.js.Any] = {
-    Map[String, scalajs.js.Any](
-      "diagram" -> TestLanguage.langForModel(modelId),
-      "shape" -> TestLanguage.langForModel(modelId),
-      "style" -> StyleLanguage.langForModel(modelId)
-    )
+    Map[String, scalajs.js.Any]()
   }
 }
--- webapp/src/code-editor.js
+++ webapp/src/code-editor.js
@@ -3,14 +3,96 @@ import './webpage';
 import './code-editor/codeEditor.css';
 import 'jquery-ui-themes/themes/flick/jquery-ui.css';
 
-import jQuery from "jquery";
+import $ from "jquery";
 import 'brace';
 import 'brace/ext/language_tools';
 import 'brace/theme/xcode';
 import 'brace/mode/scala';
-import './code-editor/ace-grammar';
+import { styleLanguage, testLanguage } from './code-editor/ace-grammar';
 import 'bootbox';
 import './code-editor/jquery-ui';
-import './code-editor/code-editor-main';
 
-global.jQuery = jQuery;
+$(document).ready(() => {
+    fetch('/rest/v1/meta-models/' + metaModelId, {
+        method: 'GET',
+        credentials: 'same-origin'
+      })
+      .then(response => response.json()) // TODO check response-code for ok
+      .then(metaModel => {
+        console.log(metaModel);
+        if (dslType in modesForModel) {
+          const editor = initAce();
+          $("#btn-save").click(() => saveCode());
+          editor.setValue(metaModel.dsl[dslType].code);
+          const session = ace.createEditSession(metaModel.dsl[dslType].code, modesForModel[dslType]);
+          editor.setSession(session);
+        } else {
+          $('#editor').text('No language "' + dslType + '" defined.');
+        }
+      })
+      .catch(err => console.log("error loading MetaModel: " + err));
+  });
+  
+  
+  function saveCode() {
+    const text = ace.edit("editor").getValue();
+  
+    fetch('/rest/v1/meta-models/' + metaModelId + '/' + dslType, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'PUT',
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          "code": text
+        })
+      })
+      .then(response => alert("Saving succeed")) // TODO check response-code for ok
+      .catch(err => "Saving failed: " + err);
+  }
+  
+  const modesForModel = {
+    'diagram': testLanguage,
+    'shape': testLanguage,
+    'style': styleLanguage
+  }
+  
+  function initAce() {
+    const editor = ace.edit("editor");
+    editor.setTheme("ace/theme/xcode");
+    editor.getSession().setMode("ace/mode/scala");
+    editor.$blockScrolling = Number.PositiveInfinity;
+    editor.setOptions({
+      "enableBasicAutocompletion": true,
+      "enableLiveAutocompletion": true
+    });
+  
+    const container = $('<div class="container"></div>');
+    $('#editor').append(container);
+  
+    const panel = $('<div class="panel panel-default"></div>');
+    container.append(panel);
+  
+    const header = $('<div class="panel-heading"></div>');
+    panel.append(header);
+  
+    const headerTitle = $('<h3 class="panel-title editor-title"></h3>');
+    header.append(headerTitle);
+  
+    const strong = $('<strong>' + dslType + '</strong>');
+    header.append(strong);
+  
+    const btnSave = $('<span id="btn-save" class="btn btn-default glyphicon glyphicon-floppy-disk typcnbtn pull-right">Save Document</span>');
+    header.append(btnSave);
+  
+    const body = $('<div class="panel-body editor-body"></div>');
+    panel.append(body);
+  
+    const bodyInner = $('<div style="background-color: gray"></div>');
+    body.append(bodyInner);
+  
+    const divEditor = $('<div id="aceId" class="editor"></div>');
+    bodyInner.append(divEditor);
+  
+    return editor;
+  }
