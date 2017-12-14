c717d8d9ad20f4e18826ace0013dd827739cbafc
Jan Bodendorf <bodendorf.jan@gmail.com>
2017-12-14 10:57:53 +0100

    #29 more beautify tests

--- webapp/src/graphical-editor/model/generator/ConnectionDefinitionGenerator.test.js
+++ webapp/src/graphical-editor/model/generator/ConnectionDefinitionGenerator.test.js
@@ -3,97 +3,97 @@ import ConnectionDefinitionGenerator from './ConnectionDefinitionGenerator';
 const defaultTestId = "1234"
 
 describe('createLabel', () => {
-    function create(connections) {
-        const generator = new ConnectionDefinitionGenerator(connections)
-        return generator;
-    }
+  function create(connections) {
+      const generator = new ConnectionDefinitionGenerator(connections)
+      return generator;
+  }
 
-    test('with empty connetions', () => {
-      const generator = create([])
+  test('with empty connetions', () => {
+    const generator = create([])
 
-      expect(generator.getLabels('Connection1')).toEqual([])
-    })
+    expect(generator.getLabels('Connection1')).toEqual([])
+  })
 
-    test('with empty placing', () => {
-      const connections = [
-        {
-          "name": "Connection1",
-          "placings": []
-        }
-      ]
+  test('with empty placing', () => {
+    const connections = [
+      {
+        "name": "Connection1",
+        "placings": []
+      }
+    ]
 
-      const generator = create(connections)
+    const generator = create(connections)
 
-      expect(generator.getLabels('Connection1')).toEqual([])
-    })
+    expect(generator.getLabels('Connection1')).toEqual([])
+  })
 
-    test('with one full defined connection and one placing', () => {
-        const connections = [
+  test('with one full defined connection and one placing', () => {
+    const connections = [
+      {
+        "name": "Connection1",
+        "placings": [
           {
-            "name": "Connection1",
-            "placings": [
-              {
-                "positionDistance": 1,
-                "positionOffset": 1.0,
-                "shape": {
-                  "textBody": "Hallo",
-                  "id": defaultTestId,
-                  "type": "Label",
-                }
-              }
-            ]
+            "positionDistance": 1,
+            "positionOffset": 1.0,
+            "shape": {
+              "textBody": "Hallo",
+              "id": defaultTestId,
+              "type": "Label",
+            }
           }
         ]
-                
-        const generator = create(connections)
+      }
+    ]
+              
+    const generator = create(connections)
 
-        expect(generator.getLabels('Connection1')).toEqual(
-            [
-              {
-                position: 1.0,
-                attrs: {
-                    rect: {fill: 'transparent'},
-                    text: {
-                    y: 1,
-                    text: "Hallo"
-                    }
-                },
-                id: defaultTestId
-            }
-          ]
-        )
-    }) 
-
-    test('with one full defined connection and two placing', () => {
-      const connections = [
-        {
-          "name": "Connection1",
-          "placings": [
-            {
-              "positionDistance": 1,
-              "positionOffset": 1.0,
-              "shape": {
-                "textBody": "Hallo",
-                "id": "placing1",
-                "type": "Label",
-              }
+    expect(generator.getLabels('Connection1')).toEqual(
+        [
+          {
+            position: 1.0,
+            attrs: {
+                rect: {fill: 'transparent'},
+                text: {
+                y: 1,
+                text: "Hallo"
+                }
             },
-            {
-              "positionDistance": 1,
-              "positionOffset": 1.0,
-              "shape": {
-                "textBody": "Hallo",
-                "id": "placing2",
-                "type": "Label",
-              }
-            }
-          ]
+            id: defaultTestId
         }
       ]
-              
-      const generator = create(connections)
+    )
+  }) 
 
-      expect(generator.getLabels('Connection1')).toEqual(
+  test('with one full defined connection and two placing', () => {
+    const connections = [
+      {
+        "name": "Connection1",
+        "placings": [
+          {
+            "positionDistance": 1,
+            "positionOffset": 1.0,
+            "shape": {
+              "textBody": "Hallo",
+              "id": "placing1",
+              "type": "Label",
+            }
+          },
+          {
+            "positionDistance": 1,
+            "positionOffset": 1.0,
+            "shape": {
+              "textBody": "Hallo",
+              "id": "placing2",
+              "type": "Label",
+            }
+          }
+        ]
+      }
+    ]
+            
+    const generator = create(connections)
+
+    expect(generator.getLabels('Connection1')).toEqual(
       [
         {
           position: 1.0,
