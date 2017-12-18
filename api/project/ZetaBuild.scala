ba3d06e7eb157ff87e7788f0d8639e91319e7baf
aullik <nicolaswehrle@gmx.net>
2017-12-18 09:42:26 +0100

    #8 update ZetaBuild.scala
    refactor imports
    add linterSettings
    add scalaVersion
    add defaultSettings
    add defaultProject method that adds defaultSettings to project

--- api/project/ZetaBuild.scala
+++ api/project/ZetaBuild.scala
@@ -1,4 +1,17 @@
-import sbt._
+import org.scalastyle.sbt.ScalastylePlugin.autoImport.scalastyle
+import org.scalastyle.sbt.ScalastylePlugin.autoImport.scalastyleFailOnError
+import sbt.taskKey
+import sbt.Project
+import sbt.project
+import sbt.file
+import sbt.Compile
+import sbt.Keys
+import sbt.Def
+import sbt.Keys.scalacOptions
+import sbt.Keys.compile
+import wartremover.WartRemover.autoImport.Wart
+import wartremover.WartRemover.autoImport.wartremoverWarnings
+import wartremover.WartRemover.autoImport.Warts
 
 object ZetaBuild {
 
@@ -7,14 +20,9 @@ object ZetaBuild {
   lazy val persistence = project
   lazy val server = project
 
-  /**
-   * change project to current file
-   */
-  def inCurrent(project: Project) = project in file(".")
-
   val compileScalastyle = taskKey[Unit]("compileScalastyle")
 
-  val scalaOptions = Keys.scalacOptions ++= Seq(
+  val scalaOptions = scalacOptions ++= Seq(
     "-deprecation", // Emit warning and location for usages of deprecated APIs.
     "-feature", // Emit warning and location for usages of features that should be imported explicitly.
     "-unchecked", // Enable additional warnings where generated code depends on assumptions.
@@ -27,4 +35,28 @@ object ZetaBuild {
     "-Ywarn-numeric-widen" // Warn when numerics are widened.
   )
 
+  val linterSettings = Seq(
+    scalastyleFailOnError := true,
+    ZetaBuild.compileScalastyle := scalastyle.in(Compile).toTask("").value,
+    compile in Compile := ((compile in Compile) dependsOn ZetaBuild.compileScalastyle).value,
+    wartremoverWarnings ++= Warts.unsafe.filterNot(_ == Wart.NonUnitStatements)
+  )
+
+  val scalaVersion = Keys.scalaVersion := "2.11.7"
+
+  val defaultSettings: Seq[Def.SettingsDefinition] = linterSettings ++ Seq(
+    scalaOptions,
+    scalaVersion
+  )
+
+
+  /**
+   * change project to current file
+   */
+  def inCurrent(project: Project): Project = project in file(".")
+
+  /**
+   * change project to current file and add defaultSettings
+   */
+  def defaultProject(project: Project): Project = inCurrent(project).settings(defaultSettings: _*)
 }
