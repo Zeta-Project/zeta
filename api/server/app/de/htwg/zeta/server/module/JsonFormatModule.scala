b3aaba6ca4c4091cc24358a22275852f22e5e5ce
Patrick Leber <patrick.leber@freenet.de>
2017-12-20 20:08:05 +0100

    fixing #55

--- api/server/app/de/htwg/zeta/server/module/JsonFormatModule.scala
+++ api/server/app/de/htwg/zeta/server/module/JsonFormatModule.scala
@@ -42,7 +42,7 @@ class JsonFormatModule extends ScalaModule {
     bind[TimedTaskFormat].toInstance(new TimedTaskFormat)
     bind[AttributeTypeFormat].toInstance(new AttributeTypeFormat(sString = sString, sBoolean = sBoolean, sInt = sInt, sDouble = sDouble, sUnit = "Unit"))
     bind[AttributeValueFormat].toInstance(new AttributeValueFormat(sString = sString, sBoolean = sBoolean, sInt = sInt, sDouble = sDouble))
-    bind[EnumFormat].toInstance(new EnumFormat(sValueNames = "values"))
+    bind[EnumFormat].toInstance(new EnumFormat)
   }
 
   @Provides
