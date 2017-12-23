5f1a52feb2cc60c0de2e5112b744d0184137da08
aullik <nicolaswehrle@gmx.net>
2017-12-23 05:57:47 +0100

    #59 Implement SilhouetteLoginInfoRepository to Wrap around LoginInfoRepository
    in server, replace all calls to LoginInfoRepository with calls to SilhouetteLoginInfoRepository

---
+++ api/server/app/de/htwg/zeta/server/silhouette/SilhouetteLoginInfoRepository.scala
@@ -0,0 +1,34 @@
+package de.htwg.zeta.server.silhouette
+
+import java.util.UUID
+import javax.inject.Inject
+
+import scala.concurrent.Future
+
+import com.mohiva.play.silhouette.api.LoginInfo
+import de.htwg.zeta.persistence.authInfo.ZetaLoginInfo
+import de.htwg.zeta.persistence.general.LoginInfoRepository
+
+/**
+ * Persistence to save the LoginInfo and the User-Id it belongs to.
+ */
+class SilhouetteLoginInfoRepository @Inject()(
+    repo: LoginInfoRepository
+) {
+
+  /** Create a LoginInfo.
+   *
+   * @param loginInfo The LoginInfo.
+   * @param id        The id of the user.
+   * @return Unit-Future, when successful.
+   */
+  def create(loginInfo: LoginInfo, id: UUID): Future[Unit] = repo.create(ZetaLoginInfo(loginInfo), id)
+
+  /** Get a user that matches the specified LoginInfo.
+   *
+   * @param loginInfo The LoginInfo.
+   * @return The id of the User.
+   */
+  def read(loginInfo: LoginInfo): Future[UUID] = repo.read(ZetaLoginInfo(loginInfo))
+
+}
