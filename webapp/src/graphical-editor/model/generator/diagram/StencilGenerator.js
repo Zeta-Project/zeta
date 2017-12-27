75a760654861e785ea4a8b6a6f4f732e9d2326ae
Philipp Daniels <philipp.daniels@gmail.com>
2017-12-27 13:10:31 +0100

    #32 Implement generator for Stencil.shapes with JavaScript

--- webapp/src/graphical-editor/model/generator/diagram/StencilGenerator.js
+++ webapp/src/graphical-editor/model/generator/diagram/StencilGenerator.js
@@ -1,24 +1,132 @@
-function createGroups(diagram) {
-    let i = 1;
-    return diagram.model.nodes.reduce((result, node) => {
-        const key = getVarName(node.palette);
-        if (!result[key]) {
-            result[key] = { index: i++, label: node.palette };
-        }
+import joint from 'jointjs';
+
+function createGroups(nodes) {
+    return getPalettes(nodes).reduce((result, palette, i) => {
+        const key = getVarName(palette);
+        result[key] = { index: i + 1, label: palette };
         return result;
     }, {});
 }
 
+function getPalettes(nodes) {
+    const palettes = nodes.reduce((result, node) => {
+        result[node.palette] = true;
+        return result;
+    }, {});
+    return Object.keys(palettes);
+}
+
 function getVarName(string) {
+    string = string.replace(new RegExp("\\W", "g"), '');
     return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
+const stringToTypeMapper = {
+    'String': 'StringType',
+    'Boolean': 'BoolType',
+    'Int': 'IntType',
+    'Double': 'DoubleType',
+    'Unit': 'UnitType',
+}
+
+class ShapesGenerator {
+
+    constructor(shapeStyleGenerator) {
+        this.shapeStyleGenerator = shapeStyleGenerator;
+    }
+
+    create(nodes, classes) {
+        return getPalettes(nodes).reduce((result, palette) => {
+            result[getVarName(palette)] = this.createShapeList(palette, nodes, classes);
+            return result;
+        }, {});
+    }
+
+    createShapeList(palette, nodes, classes) {
+        return nodes.filter(n => n.palette === palette)
+            .map(node => this.createShapeEntry(node, classes));
+    }
+
+    createShapeEntry(node, classes) {
+        const shapeName = node.shape.name.replace(new RegExp("\\W", "g"), '');
+        const attributes = this.createShapeAttributes(node, classes);
+        const shape = new joint.shapes.zeta[shapeName](attributes);
+        this.setShapeAttributes(shape, shapeName, node);
+        return shape;
+    }
+
+    createShapeAttributes(node, classes) {
+        return Object.assign(
+            {
+                nodeName: node.name,
+                mClass: node.mClass,
+                mClassAttributeInfo: this.createMClassAttributeInfo(node, classes),
+            },
+            this.createOptionalMcoreAttributes(node),
+        );
+    }
+
+    createMClassAttributeInfo(node, classes) {
+        const mClass = classes.find(c => c.name === node.mClass);
+        return mClass && mClass.attributes ? mClass.attributes.map(a => this.createMClassAttribute(a, node)) : [];
+    }
+
+    createMClassAttribute(attribute, node) {
+        return Object.assign(
+            {
+                name: attribute.name,
+                type: stringToTypeMapper[attribute.typ] ? stringToTypeMapper[attribute.typ] : null,
+            },
+            this.createMClassAttributeText(attribute, node),
+        );
+    }
+
+    createMClassAttributeText(attribute, node) {
+        const vars = node.shape.vars ? node.shape.vars : [];
+        const entry = vars.find(v => v.value === attribute.name);
+        return entry ? { id: entry.key } : {};
+    }
+
+    createOptionalMcoreAttributes(node) {
+        return node.onCreate && node.onCreate.askFor ? {
+            mcoreAttributes: [
+                this.createMcoreAttribute(node.onCreate.askFor),
+            ]
+        } : {};
+    }
+
+    createMcoreAttribute(name) {
+        return {
+            mcore: name,
+            cellPath: ['attrs', '.label', 'text'],
+        };
+    }
+
+    setShapeAttributes(shape, shapeName, node) {
+        shape.attr(this.shapeStyleGenerator.getShapeStyle(shapeName));
+        const vals = node.shape.vals ? node.shape.vals : [];
+        vals.map(v => {
+            shape.attr({ [v.key]: { text: v.value } });
+        });
+    }
+}
+
+/**
+ * 
+ */
 export default class StencilGenerator {
-    constructor(diagram) {
-        this.diagram = diagram;
+    constructor(diagram, metaModel, shapeStyleGenerator) {
+        this.metaModel = metaModel;
+        this.nodes = diagram.model && diagram.model.nodes ? diagram.model.nodes : [];
+        this.shapesGenerator = new ShapesGenerator(shapeStyleGenerator);
     }
 
     get groups() {
-        return this.diagram.model && this.diagram.model.nodes ? createGroups(this.diagram) : {};
+        return createGroups(this.nodes);
+    }
+
+    get shapes() {
+        const classes = this.metaModel && this.metaModel.classes ? this.metaModel.classes : [];
+        return this.shapesGenerator.create(this.nodes, classes);
     }
 }
--- webapp/src/graphical-editor/model/generator/diagram/StencilGenerator.test.js
+++ webapp/src/graphical-editor/model/generator/diagram/StencilGenerator.test.js
@@ -1,6 +1,8 @@
-import StencilGenerator from './StencilGenerator';
+jest.mock('jointjs');
 
 describe('Stencil.groups', () => {
+    const StencilGenerator = require('./StencilGenerator').default;
+
     test('without palettes', () => {
         const Stencil = new StencilGenerator({});
         expect(Stencil.groups).toEqual({});
@@ -11,15 +13,15 @@ describe('Stencil.groups', () => {
             model: {
                 nodes: [
                     {
-                        palette: 'GENERAL',
+                        palette: '-General-',
                     }
                 ]
             }
         });
         expect(Stencil.groups).toEqual({
-            'gENERAL': {
+            'general': {
                 index: 1,
-                label: 'GENERAL',
+                label: '-General-',
             },
         });
     });
@@ -29,22 +31,22 @@ describe('Stencil.groups', () => {
             model: {
                 nodes: [
                     {
-                        palette: 'GENERAL',
+                        palette: 'General',
                     },
                     {
-                        palette: 'CUSTOM',
+                        palette: 'Custom',
                     }
                 ]
             }
         });
         expect(Stencil.groups).toEqual({
-            'gENERAL': {
+            'general': {
                 index: 1,
-                label: 'GENERAL',
+                label: 'General',
             },
-            'cUSTOM': {
+            'custom': {
                 index: 2,
-                label: 'CUSTOM',
+                label: 'Custom',
             },
         });
     });
@@ -54,26 +56,280 @@ describe('Stencil.groups', () => {
             model: {
                 nodes: [
                     {
-                        palette: 'GENERAL',
+                        palette: 'General',
                     },
                     {
-                        palette: 'GENERAL',
+                        palette: 'General',
                     },
                     {
-                        palette: 'CUSTOM',
+                        palette: 'Custom',
                     }
                 ]
             }
         });
         expect(Stencil.groups).toEqual({
-            'gENERAL': {
+            'general': {
                 index: 1,
-                label: 'GENERAL',
+                label: 'General',
             },
-            'cUSTOM': {
+            'custom': {
                 index: 2,
-                label: 'CUSTOM',
+                label: 'Custom',
             },
+        });
+    });
+});
+
+describe('Stencil.shapes', () => {
+    const StencilGenerator = require('./StencilGenerator').default;
+    class Box {
+        constructor(parameter) {
+            this.parameter = parameter;
+            this.attr = jest.fn();
+        }
+    }
+    class ShapeStyle {
+        getShapeStyle() {
+            return {};          
+        }
+    }
+    require('jointjs').shapes.zeta = { Box };
+
+    test('without palettes', () => {
+        const Stencil = new StencilGenerator({});
+        expect(Stencil.shapes).toEqual({});
+    });
+
+    test('with palette', () => {
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        name: 'BoxNode',
+                        mClass: 'BoxModel',
+                        palette: 'General',
+                        shape: {
+                            name: 'Box',
+                        },
+                    }
+                ]
+            }
+        }, {}, new ShapeStyle());
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(1);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({})
+            expect(shape.parameter).toEqual({
+                'nodeName': 'BoxNode',
+                'mClass': 'BoxModel',
+                'mClassAttributeInfo': [],
+            });
+        });
+    });
+
+    test('with palette and onCreate', () => {
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        name: 'BoxNode',
+                        mClass: 'BoxModel',
+                        palette: 'General',
+                        shape: {
+                            name: 'Box',
+                        },
+                        onCreate: {
+                            askFor: 'BoxModelAttribute'
+                        }
+                    }
+                ]
+            }
+        }, {}, new ShapeStyle());
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(1);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({})
+            expect(shape.parameter).toEqual({
+                'nodeName': 'BoxNode',
+                'mClass': 'BoxModel',
+                'mcoreAttributes': [
+                    {
+                        'mcore': 'BoxModelAttribute',
+                        'cellPath': ['attrs', '.label', 'text'],
+                    }
+                ],
+                'mClassAttributeInfo': [],
+            });
+        });
+    });
+
+    test('with palette and meta-model attribute', () => {
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        name: 'BoxNode',
+                        mClass: 'BoxModel',
+                        palette: 'General',
+                        shape: {
+                            name: 'Box',
+                        },
+                    }
+                ]
+            }
+        }, {
+            classes: [
+                {
+                    name: 'BoxModel',
+                    attributes: [
+                        {
+                            name: 'BoxModelAttribute',
+                            typ: 'String',
+                        }
+                    ]
+                }
+            ],
+        }, new ShapeStyle());
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(1);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({})
+            expect(shape.parameter).toEqual({
+                'nodeName': 'BoxNode',
+                'mClass': 'BoxModel',
+                'mClassAttributeInfo': [
+                    {
+                        'name': 'BoxModelAttribute',
+                        'type': 'StringType',
+                    }
+                ],
+            });
+        });
+    });
+
+    test('with palette and meta-model attribute and vars', () => {
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        palette: 'General',
+                        mClass: 'BoxModel',
+                        shape: {
+                            name: 'Box',
+                            vars: [
+                                {
+                                    key: 'text_id',
+                                    value: 'BoxModelAttribute',
+                                }
+                            ] 
+                        },
+                    }
+                ]
+            }
+        }, {
+            classes: [
+                {
+                    name: 'BoxModel',
+                    attributes: [
+                        {
+                            name: 'BoxModelAttribute',
+                            typ: 'String',
+                        }
+                    ]
+                }
+            ],
+        }, new ShapeStyle());
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(1);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({})
+            expect(shape.parameter).toEqual({
+                'nodeName': undefined,
+                'mClass': 'BoxModel',
+                'mClassAttributeInfo': [
+                    {
+                        'name': 'BoxModelAttribute',
+                        'type': 'StringType',
+                        'id': 'text_id',
+                    }
+                ],
+            });
+        });
+    });
+
+    test('with palette and shape-style', () => {
+        const shapeStyle = new ShapeStyle();
+        shapeStyle.getShapeStyle = function(shape) {
+            return shape === 'Box' ? { font: 'Verdana' } : {};
+        }
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        palette: 'General',
+                        shape: {
+                            name: 'Box',
+                        },
+                    }
+                ]
+            }
+        }, {}, shapeStyle);
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(1);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({ 'font': 'Verdana' })
+        });
+    });
+
+    test('with palette and vals', () => {
+        const Stencil = new StencilGenerator({
+            model: {
+                nodes: [
+                    {
+                        palette: 'General',
+                        shape: {
+                            name: 'Box',
+                            vals: [
+                                {
+                                    key: 'text_id',
+                                    value: 'label',
+                                }
+                            ] 
+                        },
+                    }
+                ]
+            }
+        }, {}, new ShapeStyle());
+        expect(Stencil.shapes).toHaveProperty('general');
+        expect(Stencil.shapes.general).toBeInstanceOf(Array);
+        expect(Stencil.shapes.general).not.toEqual([]);
+        Stencil.shapes.general.forEach(shape => {
+            expect(shape).toBeInstanceOf(Box);
+            expect(shape.attr.mock.calls).toHaveLength(2);
+            expect(shape.attr.mock.calls[0]).toHaveLength(1);
+            expect(shape.attr.mock.calls[0][0]).toEqual({});
+            expect(shape.attr.mock.calls[1]).toHaveLength(1);
+            expect(shape.attr.mock.calls[1][0]).toEqual({ 'text_id': {'text': 'label' } });
         });
     });
 });
