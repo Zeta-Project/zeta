fb61a8318694ba063355bd6b980e3af84f4a2ac0
Markus Heilig <markusheilig@gmx.net>
2018-02-06 11:24:17 +0100

    #72 support for empty string parsing

--- api/parser/src/main/scala/de/htwg/zeta/parser/diagram/DiagramParseTree.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/diagram/DiagramParseTree.scala
@@ -1,5 +1,6 @@
 package de.htwg.zeta.parser.diagram
 
 
-case class PaletteParseTree(name: String, nodes: Seq[String])
 case class DiagramParseTree(name: String, palettes: Seq[PaletteParseTree])
+case class PaletteParseTree(name: String, nodes: Seq[NodeParseTree])
+case class NodeParseTree(name: String)
