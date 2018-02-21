23ef55c5dbe1df24a258112ec37245dd5ecc52b3
Markus Heilig <markusheilig@gmx.net>
2018-02-21 09:05:09 +0100

    #75 adjust geo model parsing + add statictext

--- api/parser/src/main/scala/de/htwg/zeta/parser/shape/parsetree/GeoModelParseTrees.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/shape/parsetree/GeoModelParseTrees.scala
@@ -5,50 +5,69 @@ import de.htwg.zeta.parser.shape.parsetree.GeoModelAttributes._
 
 object GeoModelParseTrees {
 
-  abstract class GeoModelParseTree(val style: Option[Style])
-
-  trait Children {
+  trait GeoModelParseTree {
+    val style: Option[Style]
     val children: List[GeoModelParseTree]
   }
 
-  case class EllipseParseTree(override val style: Option[Style],
+  case class EllipseParseTree(style: Option[Style],
                               position: Position,
                               size: Size,
-                              children: List[GeoModelParseTree]) extends GeoModelParseTree(style) with Children
+                              children: List[GeoModelParseTree]) extends GeoModelParseTree
 
-  case class TextfieldParseTree(override val style: Option[Style],
+  case class TextfieldParseTree(style: Option[Style],
                                 identifier: Identifier,
-                                multiline: Multiline,
                                 position: Position,
                                 size: Size,
-                                align: Align) extends GeoModelParseTree(style)
+                                multiline: Option[Multiline],
+                                align: Option[Align],
+                                editable: Option[Editable],
+                                children: List[GeoModelParseTree]) extends GeoModelParseTree
+
+  case class StatictextParseTree(style: Option[Style],
+                                 size: Size,
+                                 position: Position,
+                                 text: Text,
+                                 children: List[GeoModelParseTree]) extends GeoModelParseTree
 
   case class RepeatingBoxParseTree(editable: Editable,
                                    foreach: For,
-                                   children: List[GeoModelParseTree]) extends GeoModelParseTree(None) with Children
+                                   children: List[GeoModelParseTree]) extends GeoModelParseTree {
+    override val style: Option[Style] = None
+  }
 
-  case class LineParseTree(override val style: Option[Style],
-                           from: Point,
-                           to: Point) extends GeoModelParseTree(style)
+  case class LineParseTree(style: Option[Style],
+                           startPoint: Point,
+                           endPoint: Point,
+                           children: List[GeoModelParseTree]) extends GeoModelParseTree
 
-  case class PolylineParseTree(override val style: Option[Style],
-                               points: List[Point]) extends GeoModelParseTree(style) {
+  case class PolylineParseTree(style: Option[Style],
+                               points: List[Point],
+                               children: List[GeoModelParseTree]) extends GeoModelParseTree {
     require(points.size >= 2)
   }
 
-  case class PolygonParseTree(override val style: Option[Style],
-                              curvedPoints: List[CurvedPoint]) extends GeoModelParseTree(style) {
-    require(curvedPoints.size >= 2)
+  case class PolygonParseTree(style: Option[Style],
+                              points: List[Point],
+                              children: List[GeoModelParseTree]) extends GeoModelParseTree {
+    require(points.size >= 2)
   }
 
-  case class RectangleParseTree(override val style: Option[Style],
+  case class RectangleParseTree(style: Option[Style],
                                 position: Position,
                                 size: Size,
-                                curve: Option[Curve],
-                                children: List[GeoModelParseTree]) extends GeoModelParseTree(style) with Children
+                                children: List[GeoModelParseTree]) extends GeoModelParseTree
 
-  case class HorizontalLayoutParseTree(override val style: Option[Style], children: List[GeoModelParseTree]) extends GeoModelParseTree(style) with Children
+  case class RoundedRectangleParseTree(style: Option[Style],
+                                       position: Position,
+                                       size: Size,
+                                       curve: Curve,
+                                       children: List[GeoModelParseTree]) extends GeoModelParseTree
 
-  case class VerticalLayoutParseTree(override val style: Option[Style], children: List[GeoModelParseTree]) extends GeoModelParseTree(style) with Children
+  case class HorizontalLayoutParseTree(style: Option[Style],
+                                       children: List[GeoModelParseTree]) extends GeoModelParseTree
+
+  case class VerticalLayoutParseTree(style: Option[Style],
+                                     children: List[GeoModelParseTree]) extends GeoModelParseTree
 
 }
