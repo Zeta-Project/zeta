66f61c5937c3474bc210b400ec7138401bc6f573
Simon Keller <simkelle@htwg-konstanz.de>
2018-02-23 09:55:29 +0100

    #75 fix rgb color to int in transformer

--- api/parser/src/test/scala/de/htwg/zeta/parser/style/StyleParserTransformerTest.scala
+++ api/parser/src/test/scala/de/htwg/zeta/parser/style/StyleParserTransformerTest.scala
@@ -29,8 +29,17 @@ class StyleParserTransformerTest extends FlatSpec with Matchers {
       |}
     """.stripMargin
 
-  "A converter" should "build a style" in {
+  val styleToTestColors: String =
+    """
+      |style Y {
+      |  description = "Style for a connection between an interface and its implementing class"
+      |  background-color = orange
+      |  line-color = gray
+      |  font-color = green
+      |}
+    """.stripMargin
 
+  "A converter" should "build a style" in {
     val styleParser = parserToTest.parseStyles(styleToTestSuccess)
     styleParser.successful shouldBe true
     val styleTrees = styleParser.get
@@ -39,15 +48,27 @@ class StyleParserTransformerTest extends FlatSpec with Matchers {
 
     deprecatedStyle.description shouldBe Some("\"Style for a connection between an interface and its implementing class\"")
     deprecatedStyle.line_width shouldBe Some(1)
-    deprecatedStyle.background_color.map(_.getRGBValue) shouldBe Some("255.0255.0255.0")
+    deprecatedStyle.background_color.map(_.getRGBValue) shouldBe Some("255,255,255")
     deprecatedStyle.line_style shouldBe Some(DASH)
     deprecatedStyle.line_width shouldBe Some(1)
-    deprecatedStyle.font_color.map(_.getRGBValue) shouldBe Some("0.00.00.0")
+    deprecatedStyle.font_color.map(_.getRGBValue) shouldBe Some("0,0,0")
     deprecatedStyle.font_name shouldBe Some("Helvetica")
     deprecatedStyle.font_size shouldBe Some(20)
     deprecatedStyle.font_bold shouldBe Some(true)
     deprecatedStyle.font_italic shouldBe Some(true)
     deprecatedStyle.gradient_orientation shouldBe Some(VERTICAL)
+  }
+
+  "A converter" should "build a style with special colors" in {
+    val styleParser = parserToTest.parseStyles(styleToTestColors)
+    styleParser.successful shouldBe true
+    val styleTrees = styleParser.get
+    val styles = StyleParseTreeTransformer.transform(styleTrees).getOrElse(List())
+    val deprecatedStyle: Style = styles.head
+
+    deprecatedStyle.background_color.map(_.getRGBValue) shouldBe Some("255,165,0")
+    deprecatedStyle.line_color.map(_.getRGBValue) shouldBe Some("128,128,128")
+    deprecatedStyle.font_color.map(_.getRGBValue) shouldBe Some("0,128,0")
   }
 
 }
