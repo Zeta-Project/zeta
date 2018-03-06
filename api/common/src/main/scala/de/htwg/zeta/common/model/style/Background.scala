93092fc25a51b8ca8ad2f8a35d7f95c8bd6ff530
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-06 11:04:01 +0100

    #88 write transformer for new style model

--- api/common/src/main/scala/de/htwg/zeta/common/model/style/Background.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/model/style/Background.scala
@@ -1,3 +1,7 @@
 package de.htwg.zeta.common.model.style
 
 case class Background(color: Color)
+
+object Background {
+  val defaultColor: Color = Color("0,0,0")
+}
--- api/common/src/main/scala/de/htwg/zeta/common/model/style/Color.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/model/style/Color.scala
@@ -1,3 +1,15 @@
 package de.htwg.zeta.common.model.style
 
 case class Color(string: String)
+
+object Color {
+  def getRGBValue(color: javafx.scene.paint.Color): String = {
+    val r = (color.getRed * 255.0).round.toInt
+    val g = (color.getGreen * 255.0).round.toInt
+    val b = (color.getBlue * 255.0).round.toInt
+
+    s"$r,$g,$b"
+  }
+
+  def apply(color: javafx.scene.paint.Color): Color = new Color(getRGBValue(color))
+}
