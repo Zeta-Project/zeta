92f600c42f811978fb1e57355311cf3f3142ce66
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-06 13:04:57 +0100

    #88 rename reference/pointer check

--- api/parser/src/test/scala/de/htwg/zeta/parser/check/FindInvalidReferencesTest.scala
+++ api/parser/src/test/scala/de/htwg/zeta/parser/check/FindInvalidReferencesTest.scala
@@ -6,36 +6,36 @@ import org.scalatest.Inside
 import org.scalatest.Matchers
 
 //noinspection ScalaStyle
-class FindInvalidPointersTest extends FreeSpec with Matchers with Inside {
+class FindInvalidReferencesTest extends FreeSpec with Matchers with Inside {
 
   val elementToId: Element => Id = element => element.name
   val availableIds: List[String] = List("t1", "t2", "t3", "t4")
-  val findInvalidPointers = new FindInvalidPointers[Element](elementToId, availableIds)
+  val findInvalidReferences = new FindInvalidReferences[Element](elementToId, availableIds)
 
   "Check for invalid ids will result in" - {
 
     "an empty list when" - {
 
-      "the pointer list is empty" in {
-        val invalidIds = findInvalidPointers(List.empty)
+      "the reference list is empty" in {
+        val invalidIds = findInvalidReferences(List.empty)
         invalidIds shouldBe empty
       }
 
       "the element list contains a single id" in {
         val ids = List(Element("t1"))
-        val invalidIds = findInvalidPointers(ids)
+        val invalidIds = findInvalidReferences(ids)
         invalidIds shouldBe empty
       }
 
       "the element list contains valid ids" in {
         val ids = List(Element("t1"), Element("t2"), Element("t3"), Element("t4"))
-        val invalidIds = findInvalidPointers(ids)
+        val invalidIds = findInvalidReferences(ids)
         invalidIds shouldBe empty
       }
 
       "the element list contains valid ids (with duplicates)" in {
         val ids = List(Element("t1"), Element("t2"), Element("t1"), Element("t2"))
-        val invalidIds = findInvalidPointers(ids)
+        val invalidIds = findInvalidReferences(ids)
         invalidIds shouldBe empty
       }
 
@@ -45,14 +45,14 @@ class FindInvalidPointersTest extends FreeSpec with Matchers with Inside {
 
       "a single element with that id is not found" in {
         val ids = List(Element("t5"), Element("t2"))
-        val invalidIds = findInvalidPointers(ids)
+        val invalidIds = findInvalidReferences(ids)
         invalidIds should have size 1
         invalidIds should contain("t5")
       }
 
       "multiple elements with that id is not found" in {
         val ids = List(Element("t5"), Element("t7"))
-        val invalidIds = findInvalidPointers(ids)
+        val invalidIds = findInvalidReferences(ids)
         invalidIds should have size 2
         invalidIds should contain("t5")
         invalidIds should contain("t7")
