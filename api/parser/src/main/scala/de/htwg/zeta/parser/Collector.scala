dd4dbd360d80e7306706d83ab3d18bbdeb8d60fe
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-06 13:40:41 +0100

    #88 fix collector javadocs

--- api/parser/src/main/scala/de/htwg/zeta/parser/Collector.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/Collector.scala
@@ -8,21 +8,39 @@ object Collector {
 
 class Collector(private val list: List[Any]) {
 
+  /**
+   * Get all values by type => use for ParserConf like
+   * - min(1)
+   * - max(x), x > 1
+   * - range(_, y), y > 1
+   * - exact(x), x > 1
+   *
+   * @param classTag Erased class of given type T.
+   * @tparam T Type to be found.
+   * @return List of all values for type T.
+   */
   //noinspection ScalaStyle
-  // get all values by type => use for ParserConf like
-  // - min(1)
-  // - max(x), x > 1
-  // - range(_, y), y > 1
-  // - exact(x), x > 1
   def *[T](implicit classTag: ClassTag[T]): List[T] = list.collect { case x: T => x }
 
+  /**
+   * Get single value by type (optional) => use for ParserConf like
+   * - optional()
+   *
+   * @param classTag Erased class of given type T.
+   * @tparam T Type to be found.
+   * @return Optional value for type T.
+   */
   //noinspection ScalaStyle
-  // get single value by type (optional) => use for ParserConf like
-  // - optional()
   def ?[T](implicit classTag: ClassTag[T]): Option[T] = list.collectFirst { case x: T => x }
 
+  /**
+   * Get single value by type (throws) => use for ParserConf like
+   * - once()
+   *
+   * @param classTag Erased class of given type T.
+   * @tparam T Type to be found.
+   * @return One single value for type T.
+   */
   //noinspection ScalaStyle
-  // get single value by type (throws) => use for ParserConf like
-  // - once()
   def ![T](implicit classTag: ClassTag[T]): T = ?(classTag).get
 }
