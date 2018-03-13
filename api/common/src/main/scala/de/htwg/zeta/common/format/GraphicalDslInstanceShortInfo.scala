3373cf574d27f73bc4cede7d887bf704b2936eca
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-13 18:38:35 +0100

    #96 rename attribute meta model to gdsl instance

--- api/common/src/main/scala/de/htwg/zeta/common/format/GraphicalDslInstanceShortInfo.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/format/GraphicalDslInstanceShortInfo.scala
@@ -13,10 +13,10 @@ import play.api.libs.json.__
  * Represents concise information on a model, used for REST-API overview uri
  *
  * @param id          the id of the graphical dsl instance
- * @param metaModelId the id of the graphical dsl
+ * @param graphicalDslId the id of the graphical dsl
  * @param name        the name of the graphical dsl instance
  */
-case class GraphicalDslInstanceShortInfo(id: UUID, metaModelId: UUID, name: String)
+case class GraphicalDslInstanceShortInfo(id: UUID, graphicalDslId: UUID, name: String)
 
 object GraphicalDslInstanceShortInfo {
 
