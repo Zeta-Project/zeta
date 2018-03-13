17f82fa4f4bc36770b0b2dbba817c51708f62cdc
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-14 00:12:08 +0100

    #87 common parser methods refactoring - remove unused methods

--- api/parser/src/main/scala/de/htwg/zeta/parser/CommonParserMethods.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/CommonParserMethods.scala
@@ -7,9 +7,7 @@ import scala.util.parsing.combinator.JavaTokenParsers
 import scala.util.{Failure => TryFailure}
 import scala.util.{Success => TrySuccess}
 
-/**
- * commonly used parsing methods
- */
+// scalastyle:off non.ascii.character.disallowed
 trait CommonParserMethods extends JavaTokenParsers {
 
   // language independent
@@ -21,22 +19,35 @@ trait CommonParserMethods extends JavaTokenParsers {
   val comma = ","
   val eq = "="
 
-  // basic stuff
-  // scalastyle:off non.ascii.character.disallowed
+  def variable: Parser[String] = "[a-züäöA-ZÜÄÖ]+([-_][a-züäöA-ZÜÄÖ]+)*".r <~ "\\s*".r ^^ {
+    _.toString
+  }
 
-  def attribute: Parser[(String, String)] = variable ~ argument <~ ",?".r ^^ { case v ~ a => (v.toString, a.toString) }
-  def variable: Parser[String] = "[a-züäöA-ZÜÄÖ]+([-_][a-züäöA-ZÜÄÖ]+)*".r <~ "\\s*".r ^^ { _.toString }
   def argument_double: Parser[Double] = "[+-]?\\d+(\\.\\d+)?".r ^^ { dou => dou.toDouble }
+
   def argument_int: Parser[Int] = "[+-]?\\d+".r ^^ { dou => dou.toInt }
-  def natural_number: Parser[Int] = "\\d+".r ^^ { _.toInt }
+
+  def natural_number: Parser[Int] = "\\d+".r ^^ {
+    _.toInt
+  }
+
   def argument: Parser[String] =
-    "((([a-züäöA-ZÜÄÖ]|[0-9])+(\\.([a-züäöA-ZÜÄÖ]|[0-9])+)*)|(\".*\")|([+-]?\\d+(\\.\\d+)?))".r ^^ { _.toString }
+    "((([a-züäöA-ZÜÄÖ]|[0-9])+(\\.([a-züäöA-ZÜÄÖ]|[0-9])+)*)|(\".*\")|([+-]?\\d+(\\.\\d+)?))".r ^^ {
+      _.toString
+    }
 
   def argument_boolean: Parser[Boolean] = argument_boolean_true | argument_boolean_false ^^ (bool => bool)
-  private def argument_boolean_true: Parser[Boolean] = "(false|no|n)".r ^^ (bool => false)
-  private def argument_boolean_false: Parser[Boolean] = "(true|yes|y)".r ^^ (bool => true)
+
+  private def argument_boolean_true: Parser[Boolean] = "(false|no|n)".r ^^ (_ => false)
+
+  private def argument_boolean_false: Parser[Boolean] = "(true|yes|y)".r ^^ (_ => true)
 
   def argument_color: Parser[Color] = "(.+)".r.flatMap(parseColor)
+
+  def argument_string: Parser[String] =
+    "\".*\"".r ^^ {
+      _.toString
+    }
 
   private def parseColor(colorString: String): Parser[Color] = {
     Parser { in =>
@@ -47,49 +58,4 @@ trait CommonParserMethods extends JavaTokenParsers {
     }
   }
 
-  def argument_string: Parser[String] =
-    "\".*\"".r ^^ { _.toString }
-  def argument_classic: Parser[String] = """\s*\=\s*""".r ~> argument ^^ { _.toString }
-  def argument_advanced_explicit: Parser[String] =
-    """(?s)\((\w+([-_]\w+)*\s*=\s*([a-zA-ZüäöÜÄÖ]+|(\".*\")|([+-]?\d+(\.\d+)?)),?[\s\n]*)+\)""".r ^^ { _.toString }
-  def argument_advanced_implicit: Parser[String] =
-    """(?s)\((([a-zA-ZüäöÜÄÖ]+|(\".*\")|([+-]?\d+(\.\d+)?)),?\s*)+\)""".r ^^ { _.toString }
-  def argument_wrapped: Parser[String] = "\\{[^\\{\\}]*\\}".r ^^ { _.toString }
-  def arguments: Parser[String] =
-    argument_classic | argument_advanced_explicit | argument_advanced_implicit | argument_wrapped
-  def attributeAsString: Parser[String] = variable ~ arguments ^^ { case v ~ arg => v + arg }
-  def attributePair: Parser[(String, String)] = variable ~ arguments ^^ { case v ~ a => (v, a) }
-
-  /**
-   * special cases - grammar is nasty..
-   */
-  def compartmentinfo_attribute: Parser[String] = {
-    compartmentinfo_attribute_layout | compartmentinfo_attribute_stretching | compartmentinfo_attribute_spacing | compartmentinfo_attribute_margin |
-      compartmentinfo_attribute_invisible | compartmentinfo_attribute_id
-  }
-  def compartmentinfo_attribute_layout: Parser[String] = {
-    "layout\\s*=\\s*(fixed|vertical|horizontal|fit)".r ^^ { _.toString }
-  }
-  def compartmentinfo_attribute_stretching: Parser[String] = {
-    "stretching\\s*\\(\\s*horizontal\\s*=\\s*(yes|y|true|no|n|false)\\s*,\\s*vertical\\s*=\\s*(yes|y|true|no|n|false)\\)".r ^^ { _.toString }
-  }
-  def compartmentinfo_attribute_spacing: Parser[String] = "spacing\\s*=\\s*\\d+".r ^^ { _.toString }
-  def compartmentinfo_attribute_margin: Parser[String] = "margin\\s*=\\s*\\d+".r ^^ { _.toString }
-  def compartmentinfo_attribute_invisible: Parser[String] = "invisible\\s*=\\s*invisible".r ^^ { _.toString }
-  def compartmentinfo_attribute_id: Parser[String] = "id\\s*=\\s*.+".r ^^ { _.toString }
-
-  /**
-   * Some explicit usages
-   */
-  def split_compartment: Parser[List[String]] = "compartment\\s*[\\{]".r ~> rep(compartmentinfo_attribute) <~ "[\\}]".r ^^ { list => list }
-
-  def idAsString: Parser[String] = "(id|ID)\\s*=?\\s*".r ~> argument ^^ { arg => arg }
-  /**
-   * takes a String and parses a boolean value out of it -> if string is yes|true|y
-   * @param b the stringargument
-   */
-  def matchBoolean(b: String): Boolean = b match {
-    case `b` if b toLowerCase () matches "yes|true|y" => true
-    case _ => false
-  }
 }
