80ece86e1002bedd99c83fc368b5c0ed5e4aeff8
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-14 02:17:10 +0100

    #87 implement format classes for common style

---
+++ api/common/src/main/scala/de/htwg/zeta/common/format/project/gdsl/StylesFormat.scala
@@ -0,0 +1,26 @@
+package de.htwg.zeta.common.format.project.gdsl
+
+import de.htwg.zeta.common.format.project.gdsl.style.StyleFormat
+import de.htwg.zeta.common.models.project.gdsl.style.Style
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsResult
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.libs.json.OFormat
+import play.api.libs.json.Reads
+import play.api.libs.json.Writes
+
+@SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
+class StylesFormat(
+    styleFormat: StyleFormat,
+    sStyles: String = "styles"
+) extends OFormat[List[Style]] {
+
+  override def writes(clazz: List[Style]): JsObject = Json.obj(
+    sStyles -> Writes.seq(styleFormat).writes(clazz)
+  )
+
+  override def reads(json: JsValue): JsResult[List[Style]] =
+    (json \ sStyles).validate(Reads.list(styleFormat))
+
+}
---
+++ api/common/src/main/scala/de/htwg/zeta/common/format/project/gdsl/style/StyleFormat.scala
@@ -0,0 +1,50 @@
+package de.htwg.zeta.common.format.project.gdsl.style
+
+import de.htwg.zeta.common.models.project.gdsl.style.Style
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsResult
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import play.api.libs.json.OFormat
+
+@SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
+class StyleFormat(
+    backgroundFormat: BackgroundFormat,
+    fontFormat: FontFormat,
+    lineFormat: LineFormat,
+    sName: String = "name",
+    sDescription: String = "description",
+    sBackground: String = "background",
+    sFont: String = "font",
+    sLine: String = "line",
+    sTransparency: String = "transparency"
+) extends OFormat[Style] {
+
+  override def writes(clazz: Style): JsObject = Json.obj(
+    sName -> clazz.name,
+    sDescription -> clazz.description,
+    sBackground -> backgroundFormat.writes(clazz.background),
+    sFont -> fontFormat.writes(clazz.font),
+    sLine -> lineFormat.writes(clazz.line),
+    sTransparency -> clazz.transparency
+  )
+
+  override def reads(json: JsValue): JsResult[Style] = for {
+    name <- (json \ sName).validate[String]
+    description <- (json \ sDescription).validate[String]
+    background <- (json \ sBackground).validate(backgroundFormat)
+    font <- (json \ sFont).validate(fontFormat)
+    line <- (json \ sLine).validate(lineFormat)
+    transparency <- (json \ sTransparency).validate[Double]
+  } yield {
+    Style(
+      name,
+      description,
+      background,
+      font,
+      line,
+      transparency
+    )
+  }
+
+}
