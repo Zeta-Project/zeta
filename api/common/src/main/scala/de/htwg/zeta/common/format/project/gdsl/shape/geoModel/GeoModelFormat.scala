bbc1080b56fa492a09be92d3d15094395e762eab
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-20 17:40:01 +0100

    #87 register all geo model formatters to geo model formatter

--- api/common/src/main/scala/de/htwg/zeta/common/format/project/gdsl/shape/geoModel/GeoModelFormat.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/format/project/gdsl/shape/geoModel/GeoModelFormat.scala
@@ -8,7 +8,18 @@ import play.api.libs.json.JsValue
 import play.api.libs.json.OFormat
 
 class GeoModelFormat(
+    lineFormat: LineFormat,
+    polylineFormat: PolylineFormat,
+    polygonFormat: PolygonFormat,
+    rectangleFormat: RectangleFormat,
+    roundedRectangleFormat: RoundedRectangleFormat,
     ellipseFormat: EllipseFormat,
+    staticTextFormat: StaticTextFormat,
+    textFieldFormat: TextFieldFormat,
+    compartementFormat: CompartementFormat,
+    repeatingBoxFormat: RepeatingBoxFormat,
+    horizontalLayoutFormat: HorizontalLayoutFormat,
+    verticalLayoutFormat: VerticalLayoutFormat,
     sType: String
 ) extends OFormat[GeoModel] {
 
@@ -18,13 +29,35 @@ class GeoModelFormat(
 
   override def reads(json: JsValue): JsResult[GeoModel] =
     (json \ sType).validate[String].getOrElse("") match {
+      case lineFormat.vType => lineFormat.reads(json)
+      case polylineFormat.vType => polylineFormat.reads(json)
+      case polygonFormat.vType => polygonFormat.reads(json)
+      case rectangleFormat.vType => rectangleFormat.reads(json)
+      case roundedRectangleFormat.vType => roundedRectangleFormat.reads(json)
       case ellipseFormat.vType => ellipseFormat.reads(json)
+      case staticTextFormat.vType => staticTextFormat.reads(json)
+      case textFieldFormat.vType => textFieldFormat.reads(json)
+      case compartementFormat.vType => compartementFormat.reads(json)
+      case repeatingBoxFormat.vType => repeatingBoxFormat.reads(json)
+      case horizontalLayoutFormat.vType => horizontalLayoutFormat.reads(json)
+      case verticalLayoutFormat.vType => verticalLayoutFormat.reads(json)
     }
 
 }
 object GeoModelFormat {
   val geoModelFormat: GeoModelFormat = new GeoModelFormat(
+    LineFormat(geoModelFormatProvider),
+    PolylineFormat(geoModelFormatProvider),
+    PolygonFormat(geoModelFormatProvider),
+    RectangleFormat(geoModelFormatProvider),
+    RoundedRectangleFormat(geoModelFormatProvider),
     EllipseFormat(geoModelFormatProvider),
+    StaticTextFormat(geoModelFormatProvider),
+    TextFieldFormat(geoModelFormatProvider),
+    CompartementFormat(geoModelFormatProvider),
+    RepeatingBoxFormat(geoModelFormatProvider),
+    HorizontalLayoutFormat(geoModelFormatProvider),
+    VerticalLayoutFormat(geoModelFormatProvider),
     "type"
   )
 
