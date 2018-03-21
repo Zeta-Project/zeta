7bcb34c092dcae2942e6b6a2a806d7846f709172
Markus Heilig <markusheilig@gmx.net>
2018-03-21 15:50:54 +0100

    refactor CheckGraphCycles

--- api/parser/src/main/scala/de/htwg/zeta/parser/style/check/CheckGraphCycles.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/style/check/CheckGraphCycles.scala
@@ -1,5 +1,6 @@
 package de.htwg.zeta.parser.style.check
 
+import de.htwg.zeta.parser.check.Check.Id
 import de.htwg.zeta.parser.check.ErrorCheck
 import de.htwg.zeta.parser.check.ErrorCheck.ErrorMessage
 import de.htwg.zeta.parser.check.FindGraphCycles
@@ -9,12 +10,9 @@ case class CheckGraphCycles(styleTrees: List[StyleParseTree]) extends ErrorCheck
 
   override def check(): List[ErrorMessage] = {
     val findCycles = FindGraphCycles[StyleParseTree](_.name, id => styleTrees.find(_.name == id), _.parentStyles)
-    val ids = findCycles(styleTrees)
-    if (ids.isEmpty) {
-      List()
-    } else {
-      val idList = ids.mkString(", ")
-      List(s"The following styles defines a graph circle with its parent styles: $idList")
+    findCycles(styleTrees) match {
+      case Nil => Nil
+      case ids: List[Id] => List(s"The following styles defines a graph circle with its parent styles: ${ids.mkString(", ")}")
     }
   }
 
