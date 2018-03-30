97062bca2a68909387a9660d7b81a8389d30341c
Simon Keller <simkelle@htwg-konstanz.de>
2018-03-30 21:04:05 +0200

    #108 add test for concept node identifier checker

--- api/parser/src/test/scala/de/htwg/zeta/parser/ConceptCreatorHelper.scala
+++ api/parser/src/test/scala/de/htwg/zeta/parser/ConceptCreatorHelper.scala
@@ -1,25 +1,34 @@
 package de.htwg.zeta.parser
 
+import scala.collection.immutable.ListMap
+
 import de.htwg.zeta.common.models.project.concept.Concept
 import de.htwg.zeta.common.models.project.concept.elements.AttributeType
 import de.htwg.zeta.common.models.project.concept.elements.AttributeValue
 import de.htwg.zeta.common.models.project.concept.elements.MAttribute
 import de.htwg.zeta.common.models.project.concept.elements.MClass
 import de.htwg.zeta.common.models.project.concept.elements.MReference
+import de.htwg.zeta.common.models.project.concept.elements.Method
 
 //noinspection ScalaStyle
 object ConceptCreatorHelper {
 
+  private val baseClassRealization: MReference = cReference("BaseClassRealization", "Klasse", "InterfaceKlasse")
+  private val realizationReference: MReference = cReference("Realization", "InterfaceKlasse", "AbstractKlasse")
+  private val inheritanceReference: MReference = cReference("Inheritance", "Klasse", "AbstractKlasse")
   val exampleConcept = new Concept(
     classes = List(
       cClass("AbstractKlasse", List(cAttribute("text11"), cAttribute("text21"), cAttribute("text31"))),
       cClass("InterfaceKlasse", List(cAttribute("text113"), cAttribute("text213"), cAttribute("text313"))),
-      cClass("Klasse", List(cAttribute("text1"), cAttribute("text2"), cAttribute("text3")))
+      cClass("Klasse", List(cAttribute("text1"), cAttribute("text2"), cAttribute("text3")),
+        List(cMethod("methodUnit", AttributeType.UnitType), cMethod("methodString", AttributeType.StringType)),
+        List(baseClassRealization.name, inheritanceReference.name)
+      )
     ),
     references = List(
-      cReference("BaseClassRealization", "Klasse", "InterfaceKlasse"),
-      cReference("Realization", "InterfaceKlasse", "AbstractKlasse"),
-      cReference("Inheritance", "Klasse", "AbstractKlasse")
+      baseClassRealization,
+      realizationReference,
+      inheritanceReference
     ),
     enums = List(),
     attributes = List(
@@ -49,15 +58,21 @@ object ConceptCreatorHelper {
     methods = List()
   )
 
-  def cClass(name: String, attributes: List[MAttribute]): MClass = new MClass(
+  def cClass(name: String, attributes: List[MAttribute]): MClass = cClass(name, attributes, List(), List())
+
+  def cClass(
+      name: String,
+      attributes: List[MAttribute],
+      methods: List[Method],
+      outputReferenceNames: List[String]): MClass = new MClass(
     name = name,
     description = "",
     abstractness = true,
     superTypeNames = List(),
     inputReferenceNames = List(),
-    outputReferenceNames = List(),
+    outputReferenceNames = outputReferenceNames,
     attributes = attributes,
-    methods = List()
+    methods = methods
   )
 
   def cAttribute(name: String): MAttribute = new MAttribute(
@@ -71,6 +86,14 @@ object ConceptCreatorHelper {
     expression = "",
     ordered = true,
     transient = false
+  )
+
+  def cMethod(name: String, returnType: AttributeType): Method = new Method(
+    name = name,
+    parameters = ListMap(),
+    description = "default method",
+    returnType = returnType,
+    code = "nice code"
   )
 
 }
