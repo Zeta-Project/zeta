a7fc88cb2ba6861c7570ed4e239c6c78a37a1056
Markus Heilig <markusheilig@gmx.net>
2018-06-01 08:05:33 +0200

    remove spray-parser + JS-generator stuff

--- api/server/app/de/htwg/zeta/server/generator/model/diagram/methodes/OnDelete.scala
+++
@@ -1,7 +0,0 @@
-package de.htwg.zeta.server.generator.model.diagram.methodes
-
-/**
- *  ???
- * @param actionBlock ??
- */
-case class OnDelete(override val actionBlock: ActionBlock) extends Methode
--- api/server/app/de/htwg/zeta/server/generator/model/style/color/Transparent.scala
+++
@@ -1,5 +0,0 @@
-package de.htwg.zeta.server.generator.model.style.color
-
-case object Transparent extends Color with ColorWithTransparency {
-  override def getRGBValue = """transparent"""
-}
