8e03dc728b8b264eb1bc311e4ebc233ea286c57b
Marcel Luz <luz.marcel@ymail.com>
2018-06-02 18:36:04 +0200

    #182: first version of twirl-templates for klima-model

---
+++ api/codeGenerator/src/test/scala/de/htwg/zeta/codeGenerator/KlimaCodeGeneratorTest.scala
@@ -0,0 +1,28 @@
+package de.htwg.zeta.codeGenerator
+
+import org.scalatest.FreeSpec
+
+class KlimaCodeGeneratorTest extends FreeSpec {
+
+  private val generatorToTest = KlimaCodeGenerator
+
+  "A KlimaCodeGenerator" - {
+    "should generate" - {
+      "a Value as ModelClass" in {
+        println(generatorToTest.generateValue())
+      }
+      "a Link as ModelClass" in {
+        println(generatorToTest.generateLink())
+      }
+      "a MapLink as ModelClass" in {
+        println(generatorToTest.generateMapLink())
+      }
+      "a ReferenceLink as ModelClass" in {
+        println(generatorToTest.generateReferenceLink())
+      }
+      "a Entity as ModelClass" in {
+        println(generatorToTest.generateEntity())
+      }
+    }
+  }
+}
