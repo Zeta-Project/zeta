b14624a4d45f81e0364b1524fe2155925920a68a
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-03 16:19:21 +0200

    #183 append this parser in front of other parsers

--- api/parser/src/test/scala/de/htwg/zeta/parser/CommentParserTest.scala
+++ api/parser/src/test/scala/de/htwg/zeta/parser/CommentParserTest.scala
@@ -1,5 +1,6 @@
 package de.htwg.zeta.parser
 
+import de.htwg.zeta.parser.style.StyleParser
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers
 
@@ -7,29 +8,49 @@ class CommentParserTest extends FreeSpec with Matchers {
 
   "A comment parser should" - {
     "strip out any inline or multiline comment" in {
-      val input = """
-       |This is some
-       |// hello world comment
-       |stuff which // inline comment
-       |is interrupted /* multiline
-       |comment */ by many annoying
-       |comments.""".stripMargin
+      val input =
+        """This is some
+         |// hello world comment
+         |stuff which // inline comment
+         |is interrupted /* multiline
+         |comment */ by many annoying
+         |comments.""".stripMargin
 
       val expectedOutput =
         """This is some
          |stuff which is interrupted by many annoying
          |comments."""
-        .stripMargin
+          .stripMargin
 
       val parser = CommentParser()
       val result = parser.parseAll(parser.parsedText, input)
       result.get shouldBe expectedOutput
     }
+
+    "append this parser in front of other parsers" - {
+      "and success if all is correct" in {
+        val input: String =
+          """
+           |style ParentStyle { description = "Parent style" }
+           |// comment 1
+           |style ChildStyle extends ParentStyle { // comment 2
+           |  description = "Style which extends a single parent style"
+           |  /* comment 3 */
+           |}
+          """.stripMargin
+
+        val parser = CommentParser()
+        val result = parser.parseAll(parser.parsedText, input).get
+        StyleParser.parseStyles(result)
+      }
+    }
+
     "fail on not closed multiline comments" in {
-      val input ="""This is some
+      val input =
+        """This is some
          |/* multiline
          |comment which isn't closed."""
-        .stripMargin
+          .stripMargin
 
       val parser = CommentParser()
       val result = parser.parseAll(parser.parsedText, input)
