a3d9ae3ad0cadb4ebc2f21c9ae18349efe6138be
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-04 13:59:49 +0200

    #183 transformer between failure positions after comment stripping

--- api/parser/src/main/scala/de/htwg/zeta/parser/CommentParser.scala
+++ api/parser/src/main/scala/de/htwg/zeta/parser/CommentParser.scala
@@ -1,35 +1,74 @@
 package de.htwg.zeta.parser
 
+import scala.util.parsing.input.CharSequenceReader
+
 import de.htwg.zeta.parser.CommentParser.CommentResult
 import de.htwg.zeta.parser.CommentParser.ParsedResult
+import de.htwg.zeta.parser.CommentParser.StrippedResult
 import de.htwg.zeta.parser.CommentParser.TextResult
 
 class CommentParser extends CommonParserMethods {
 
-  def parsedText: Parser[String] = rep(comment | text) ^^
-    (_.collect({ case TextResult(a) => a }).mkString)
+  override def skipWhitespace: Boolean = false
+
+  def parsedText: Parser[StrippedResult] = rep(comment | text) ^^
+    (b => StrippedResult(
+      b.collect({ case TextResult(a) => a }).mkString,
+      b,
+      (s, i) => {
+        val failureArray = s.toCharArray
+        val rawArray = b.map(_.text).mkString.toCharArray
+
+        var p1 = 0
+        var p2 = 0
+        while(p2 < rawArray.length && p1 < failureArray.length && p1 < i) {
+          val f = failureArray(p1)
+          val r = rawArray(p2)
+          if(r == f) {
+            p1+=1
+            p2+=1
+          } else {
+            p2+=1
+          }
+        }
+
+        p2
+      }
+    ))
 
   private def text: Parser[ParsedResult] = anyString ^^ (a => TextResult(a))
 
   private def comment: Parser[ParsedResult] = lineComment | multilineComment
 
   private def multilineComment: Parser[CommentResult] =
-    "/*" ~> anyString <~ "*/" ^^ (a => CommentResult(a))
+    """/\*.+\*/""".r ^^ (a => CommentResult(a))
 
   private def lineComment: Parser[CommentResult] =
-    "//" ~> stringUntilLineBreak ^^ (a => CommentResult(a))
+    """//[^\v]+""".r ^^ (a => CommentResult(a))
 
   private def stringUntilLineBreak: Parser[String] =
     """[^\v]+""".r ^^ (_.toString)
 
   private def anyString: Parser[String] =
-    """[^(//)(/*)]+""".r ^^ (_.toString)
+    """[^(//)(/\*)]+""".r ^^ (_.toString)
 
 }
 object CommentParser {
   def apply(): CommentParser = new CommentParser()
 
-  trait ParsedResult
-  case class TextResult(input: String) extends ParsedResult
-  case class CommentResult(comment: String) extends ParsedResult
+  trait ParsedResult {
+    val text: String
+
+    def size(): (Int, Int) = (lineIndex(text), rowIndex(text))
+
+    def chars(): Int = text.length
+
+    private def rowIndex(input: String): Int = input.lines.toList.last.length
+
+    private def lineIndex(input: String): Int = input.lines.size
+  }
+  case class TextResult(text: String) extends ParsedResult
+  case class CommentResult(text: String) extends ParsedResult
+
+  case class StrippedResult(text: String, structure: List[ParsedResult], recalculatePosition: (String, Int) => Int)
 }
--- api/parser/src/test/scala/de/htwg/zeta/parser/CommentParserTest.scala
+++ api/parser/src/test/scala/de/htwg/zeta/parser/CommentParserTest.scala
@@ -1,5 +1,8 @@
 package de.htwg.zeta.parser
 
+import scala.util.parsing.input.CharSequenceReader
+
+import de.htwg.zeta.parser.style.StyleParser
 import de.htwg.zeta.parser.style.StyleParser
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers
@@ -7,6 +10,7 @@ import org.scalatest.Matchers
 class CommentParserTest extends FreeSpec with Matchers {
 
   "A comment parser should" - {
+    val parser = CommentParser()
     "strip out any inline or multiline comment" in {
       val input =
         """This is some
@@ -22,9 +26,8 @@ class CommentParserTest extends FreeSpec with Matchers {
          |comments."""
           .stripMargin
 
-      val parser = CommentParser()
       val result = parser.parseAll(parser.parsedText, input)
-      result.get shouldBe expectedOutput
+      result.get.text shouldBe expectedOutput
     }
 
     "append this parser in front of other parsers" - {
@@ -39,9 +42,85 @@ class CommentParserTest extends FreeSpec with Matchers {
            |}
           """.stripMargin
 
-        val parser = CommentParser()
         val result = parser.parseAll(parser.parsedText, input).get
-        StyleParser.parseStyles(result)
+        val styleResult = StyleParser.parseStyles(result.text)
+        styleResult.successful shouldBe true
+      }
+      "and should fail with correct line if following parser fails" - {
+        "with sample 1" in {
+          val input: String =
+            """style ParentStyle { description = "Parent style" }
+             |// comment 1
+             |style ChildStyle extends ParentStyle { // comment 2
+             |  /* description = "Style which extends a single parent style"*/
+             |}""".stripMargin
+
+          val result = parser.parseAll(parser.parsedText, input).get
+          val styleResult = StyleParser.parseStyles(result.text)
+          val styleResultFailure = styleResult match {
+            case StyleParser.Failure(msg, pos: StyleParser.Input) =>
+              val newInput = new CharSequenceReader(input, result.recalculatePosition(pos.source.toString, pos.offset))
+              val failure = StyleParser.Failure(msg, newInput)
+              println(failure)
+              failure
+          }
+          styleResultFailure.successful shouldBe false
+          styleResultFailure.next.offset shouldBe 185
+        }
+        "with sample 2" in {
+          val input: String =
+            """style ParentStyle { description = "Parent style" }
+             | // comment 1
+             |style ChildStyle extends ParentStyle { // comment 2
+             |     description = "Style which extends a single parent style/* ignored */
+             |}""".stripMargin
+
+          val result = parser.parseAll(parser.parsedText, input).get
+          val styleResult = StyleParser.parseStyles(result.text)
+          val styleResultFailure = styleResult match {
+            case StyleParser.Failure(msg, pos: StyleParser.Input) =>
+              val newInput = new CharSequenceReader(input, result.recalculatePosition(pos.source.toString, pos.offset))
+              val failure = StyleParser.Failure(msg, newInput)
+              println(failure)
+              failure
+            case StyleParser.Error(msg, pos) =>
+              val newInput = new CharSequenceReader(input, result.recalculatePosition(pos.source.toString, pos.offset))
+              val failure = StyleParser.Failure(msg, newInput)
+              println(failure)
+              failure
+          }
+          styleResultFailure.successful shouldBe false
+          styleResultFailure.next.offset shouldBe 139
+        }
+        "with sample 3" in {
+          val input: String =
+            """style ParentStyle { description = /* really ugly comment */ "Parent style" }
+             | /* pre comment with some space afterwards */      // comment 1
+             |style ChildStyle extends ParentStyle { // comment 2
+             |  description = "Style which /* really ugly comment */ extends"
+             |     line-color = red/* ignored */
+             |     line-width = "a"
+             |  // a comment after failing line
+             |     // and another one
+             |}""".stripMargin
+
+          val result = parser.parseAll(parser.parsedText, input).get
+          val styleResult = StyleParser.parseStyles(result.text)
+          val styleResultFailure = styleResult match {
+            case StyleParser.Failure(msg, pos: StyleParser.Input) =>
+              val newInput = new CharSequenceReader(input, result.recalculatePosition(pos.source.toString, pos.offset))
+              val failure = StyleParser.Failure(msg, newInput)
+              println(failure)
+              failure
+            case StyleParser.Error(msg, pos) =>
+              val newInput = new CharSequenceReader(input, result.recalculatePosition(pos.source.toString, pos.offset))
+              val failure = StyleParser.Failure(msg, newInput)
+              println(failure)
+              failure
+          }
+          styleResultFailure.successful shouldBe false
+          styleResultFailure.next.offset shouldBe 315
+        }
       }
     }
 
@@ -52,7 +131,6 @@ class CommentParserTest extends FreeSpec with Matchers {
          |comment which isn't closed."""
           .stripMargin
 
-      val parser = CommentParser()
       val result = parser.parseAll(parser.parsedText, input)
       result.successful shouldBe false
     }
