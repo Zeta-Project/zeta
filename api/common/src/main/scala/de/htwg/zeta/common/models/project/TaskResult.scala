c2b33bca329a6fdcf9b64158e2fa3ab597edb08f
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-07 00:12:33 +0200

    #183 extend rest api with new position

--- api/common/src/main/scala/de/htwg/zeta/common/models/project/TaskResult.scala
+++ api/common/src/main/scala/de/htwg/zeta/common/models/project/TaskResult.scala
@@ -3,10 +3,11 @@ package de.htwg.zeta.common.models.project
 case class TaskResult(
     errorDsl: String,
     messages: List[String],
+    position: Option[(Int, Int)],
     success: Boolean
 )
 object TaskResult {
-  def error(errorDsl: String, messages: List[String]) = TaskResult(errorDsl, messages, success = false)
+  def error(errorDsl: String, messages: List[String], position: Option[(Int, Int)]): TaskResult = TaskResult(errorDsl, messages, position, success = false)
 
-  def success() = TaskResult(errorDsl = "", List(), success = true)
+  def success(): TaskResult = TaskResult(errorDsl = "", List(), None, success = true)
 }
