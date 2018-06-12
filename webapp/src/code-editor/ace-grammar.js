5b3a8a2110859fbe0e4c2194543abe98cdad9fdb
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-12 19:57:43 +0200

    #173 update highlighting in diagram editor

--- webapp/src/code-editor/ace-grammar.js
+++ webapp/src/code-editor/ace-grammar.js
@@ -149,96 +149,44 @@ const shapeGrammar = {
     Parser: defaultParser
 };
 
-const testGrammar = {
+const diagramGrammar = {
     RegExpID: "RegExp::",
-
-    // defines the mapping of tokens to editor styles
-    Style: {
-        [TOKEN.DECORATOR]: "constant.support",
-        [TOKEN.COMMENT]: "comment",
-        [TOKEN.KEYWORD]: "keyword",
-        [TOKEN.BUILTIN]: "constant.support",
-        [TOKEN.OPERATOR]: "operator",
-        [TOKEN.IDENTIFIER]: "identifier",
-        [TOKEN.NUMBER]: "constant.numeric",
-        [TOKEN.STRING]: "string",
-        [TOKEN.HEREDOC]: "string"
-    },
-
+    Style: defaultStyle,
     // defines the mapping of token patterns and token configuration to an associated tokenID
     Lex: {
-        [TOKEN.COMMENT]: commentToken([["#", null]]),
-        [TOKEN.HEREDOC]: blockToken([
-            ["'''"],
-            ["\"\"\""],
-            ["RegExp::/([rubRUB]|(ur)|(br)|(UR)|(BR))?('{3}|\"{3})/", 6],
-        ]),
+        [TOKEN.COMMENT]: commentToken([["//", null], ["/*", "*/"]]),
+        [TOKEN.HEREDOC]: blockToken([["/**", "*/"]]),
         [TOKEN.IDENTIFIER]: simpleToken("RegExp::/[_A-Za-z][_A-Za-z0-9]*/"),
         [TOKEN.NUMBER]: simpleToken([
-            "RegExp::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?[jJ]?/",
-            "RegExp::/\\d+\\.\\d*[jJ]?/",
-            "RegExp::/\\.\\d+[jJ]?/",
+            // floats
+            "RegExp::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?/",
+            "RegExp::/\\d+\\.\\d*/",
+            "RegExp::/\\.\\d+/",
             // integers
             // hex
-            "RegExp::/0x[0-9a-fA-F]+[lL]?/",
+            "RegExp::/#[0-9a-fA-F]{6,8}/",
             // binary
-            "RegExp::/0b[01]+[lL]?/",
+            "RegExp::/0b[01]+L?/",
             // octal
-            "RegExp::/0o[0-7]+[lL]?/",
+            "RegExp::/0o[0-7]+L?/",
             // decimal
-            "RegExp::/[1-9]\\d*(e[\\+\\-]?\\d+)?[lL]?[jJ]?/",
+            "RegExp::/[1-9]\\d*(e[\\+\\-]?\\d+)?L?/",
             // just zero
-            "RegExp::/0(?![\\dx])/",
+            "RegExp::/0(?![\\dx])/"
         ]),
-        [TOKEN.STRING]: escapedBlockToken([
-            // start, end of string (can be the matched regex group ie. 1 )
-            ["RegExp::/(['\"])/", 1],
-            ["RegExp::/([rubRUB]|(ur)|(br)|(UR)|(BR))?(['\"])/", 6]
-        ]),
-        [TOKEN.OPERATOR]: simpleToken([
-            "(", ")", "[", "]", "{", "}", ",", ":", "`", "=", ";", ".",
-            "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=",
-            ">>=", "<<=", "//=", "**=", "@",
-        ], false, true),
+        [TOKEN.ATOM]: simpleToken(["true", "false"]),
+        [TOKEN.STRING]: escapedBlockToken([["RegExp::/(['\"])/", 1]]),
+        [TOKEN.OPERATOR]: simpleToken([""], false, true),
+        [TOKEN.DELIMITER]: simpleToken(["{", "}"], false, true),
         [TOKEN.DECORATOR]: simpleToken("RegExp::/@[_A-Za-z][_A-Za-z0-9]*/"),
-        [TOKEN.KEYWORD]: simpleToken([
-            "assert", "break", "class", "continue",
-            "def", "del", "elif", "else", "except", "finally",
-            "for", "from", "global", "if", "import",
-            "lambda", "pass", "raise", "return",
-            "try", "while", "with", "yield", "as"
-        ], true),
-        [TOKEN.BUILTIN]: simpleToken([
-            "abs", "all", "any", "bin", "bool", "bytearray", "callable", "chr",
-            "classmethod", "compile", "complex", "delattr", "dict", "dir", "divmod",
-            "enumerate", "eval", "filter", "float", "format", "frozenset",
-            "getattr", "globals", "hasattr", "hash", "help", "hex", "id",
-            "input", "int", "isinstance", "issubclass", "iter", "len",
-            "list", "locals", "map", "max", "memoryview", "min", "next",
-            "object", "oct", "open", "ord", "pow", "property", "range",
-            "repr", "reversed", "round", "set", "setattr", "slice",
-            "sorted", "staticmethod", "str", "sum", "super", "tuple",
-            "type", "vars", "zip", "__import__", "NotImplemented",
-            "Ellipsis", "__debug__"
-        ], true),
+        [TOKEN.PROPERTY]: simpleToken("RegExp::/[_A-Za-z][_A-Za-z0-9]*/"),
+        [TOKEN.KEYWORD]: simpleToken(["diagram"], true),
+        [TOKEN.BUILTIN]: simpleToken(["palette"], true),
     },
-
-    //defines what to parse and in what order
-    Parser: [
-        TOKEN.COMMENT,
-        TOKEN.HEREDOC,
-        TOKEN.NUMBER,
-        TOKEN.STRING,
-        TOKEN.DECORATOR,
-        TOKEN.OPERATOR,
-        "delimiter",
-        TOKEN.KEYWORD,
-        TOKEN.BUILTIN,
-        TOKEN.IDENTIFIER,
-    ]
+    Parser: defaultParser
 };
 
 const styleLanguage = AceGrammar.getMode(styleGrammar);
 const shapeLanguage = AceGrammar.getMode(shapeGrammar);
-const testLanguage = AceGrammar.getMode(testGrammar);
-export {styleLanguage, testLanguage, shapeLanguage}
+const diagramLanguage = AceGrammar.getMode(diagramGrammar);
+export {styleLanguage, diagramLanguage, shapeLanguage}
