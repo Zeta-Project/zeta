3361d28be264ee1553e881697daf41e60ec3199c
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-12 15:04:09 +0200

    #188 catch beautifier failures to avoid general failure on parse exception

--- api/codeGenerator/src/main/scala/de/htwg/zeta/codeGenerator/ScalaCodeBeautifier.scala
+++ api/codeGenerator/src/main/scala/de/htwg/zeta/codeGenerator/ScalaCodeBeautifier.scala
@@ -1,9 +1,26 @@
 package de.htwg.zeta.codeGenerator
 
+import grizzled.slf4j.Logging
 import scalariform.formatter.ScalaFormatter
+import scalariform.parser.ScalaParserException
 
-object ScalaCodeBeautifier {
+object ScalaCodeBeautifier extends Logging {
 
-  def format(source: String): String = ScalaFormatter.format(source).trim
+  /**
+   * Format the given scala source code. If code parsing failed, the
+   * raw source is returned and only an error is logged.
+   *
+   * @param source raw scala source
+   * @return formatted scala source
+   */
+  def format(source: String): String = try {
+    ScalaFormatter.format(source).trim
+  } catch {
+    // catch exception to avoid failure on wrong generation
+    // TODO should be handled in future
+    case e: ScalaParserException =>
+      logger.error(e)
+      source
+  }
 
 }
