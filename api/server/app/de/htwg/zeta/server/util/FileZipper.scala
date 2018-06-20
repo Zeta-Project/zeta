b2d13aa7ca7e8363e5f499cfa9c0def078c836b5
Markus Heilig <markusheilig@gmx.net>
2018-06-20 11:01:41 +0200

    finish source code export

--- api/server/app/de/htwg/zeta/server/util/FileZipper.scala
+++ api/server/app/de/htwg/zeta/server/util/FileZipper.scala
@@ -1,38 +1,28 @@
 package de.htwg.zeta.server.util
 
-import java.io.OutputStream
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
-import scala.annotation.tailrec
-import scala.util.Try
-
-import akka.stream.scaladsl.StreamConverters
-import akka.stream.scaladsl.Source
-import akka.util.ByteString
 import de.htwg.zeta.common.models.entity.File
 
 
 object FileZipper {
 
-  // http://localhost:9000/rest/v2/models/aedf1d78-21db-48b4-8865-fea445df1b62/downloadSourceCode
-  def stream(files: List[File]): Source[ByteString, Unit] = {
-    StreamConverters.asOutputStream()
-      .mapMaterializedValue(os => {
-        val zip = new ZipOutputStream(os)
-        try {
-          for {file <- files} {
-            zip.putNextEntry(new ZipEntry(file.name))
-            zip.write("test".map(_.toByte).toArray)
-            zip.closeEntry()
-          }
-        } catch {
-          case e: Throwable =>
-            println(s"error $e")
-        } finally {
-          zip.close()
-        }
-      })
+  def zip(files: List[File]): ByteArrayInputStream = {
+    val os = new ByteArrayOutputStream()
+    val zip = new ZipOutputStream(os)
+    try {
+      for {file <- files} {
+        zip.putNextEntry(new ZipEntry(s"${file.name}"))
+        zip.write(new String(file.content).getBytes)
+        zip.closeEntry()
+      }
+    } finally {
+      zip.close()
+    }
+    new ByteArrayInputStream(os.toByteArray)
   }
 
 }
