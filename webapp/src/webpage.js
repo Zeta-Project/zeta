081d387f7e7b82f707a8071e808bfbf83a570754
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-21 20:56:37 +0200

    #201 move link to generators webapp to bottom and fix logo size

--- webapp/src/webpage.js
+++ webapp/src/webpage.js
@@ -8,5 +8,6 @@ import 'bootstrap';
 
 import '../assets/favicon.png';
 import '../assets/zeta_logo.png';
+import '../assets/zeta_logo_small.png';
 import '../assets/petri_test.png';
 import './webpage.css';
