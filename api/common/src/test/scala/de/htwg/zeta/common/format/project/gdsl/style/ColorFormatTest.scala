3a209b3e22f67962d8c7ac70af4fd97b5e0f0c3e
Simon Keller <simkelle@htwg-konstanz.de>
2018-06-22 06:21:44 +0200

    #203 color format unit test

---
+++ api/common/src/test/scala/de/htwg/zeta/common/format/project/gdsl/style/ColorFormatTest.scala
@@ -0,0 +1,30 @@
+package de.htwg.zeta.common.format.project.gdsl.style
+
+import de.htwg.zeta.common.models.project.gdsl.style.Color
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.libs.json.JsSuccess
+import play.api.libs.json.Json
+
+//noinspection ScalaStyle
+class ColorFormatTest extends FreeSpec with Matchers {
+
+  "A ColorFormatTest should" - {
+    "write an object" in {
+      val result = ColorFormat().writes(Color(23, 24, 25, 26))
+      result.toString() shouldBe
+        """{"r":23,"g":24,"b":25,"a":26,"rgb":"rgb(23,24,25)","rgba":"rgba(23,24,25,26.0)","hex":"#171819"}"""
+    }
+    "read an object" in {
+      val result = ColorFormat().reads(JsString("rgba(23,24,25,26)"))
+      result shouldBe JsSuccess(Color(23, 24, 25, 26))
+    }
+    "fail in reading an invalid input" in {
+      val result = ColorFormat().reads(JsString("invalid"))
+      result.isSuccess shouldBe false
+    }
+  }
+
+}
