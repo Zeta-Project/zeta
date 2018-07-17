cbf76bd67507e51b2bf66ccc9ca81ca3f94e3afe
Simon Keller <simkelle@htwg-konstanz.de>
2018-07-17 20:27:03 +0200

    #208 handle online user to area access and send updates over socket

--- api/server/app/de/htwg/zeta/server/actor/OnlineUserActor.scala
+++ api/server/app/de/htwg/zeta/server/actor/OnlineUserActor.scala
@@ -1,9 +1,17 @@
 package de.htwg.zeta.server.actor
 
+import scala.collection.mutable
+
 import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
+import de.htwg.zeta.server.actor.OnlineUserActor.AreaState
+import de.htwg.zeta.server.actor.OnlineUserActor.ClientInfo
+import de.htwg.zeta.server.actor.OnlineUserActor.ClientOffline
+import de.htwg.zeta.server.actor.OnlineUserActor.ClientOnline
+import de.htwg.zeta.server.silhouette.ZetaIdentity
+import grizzled.slf4j.Logging
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,11 +20,44 @@ class OnlineUserActorFactory @Inject()(implicit actorSystem: ActorSystem) {
   val onlineActor: ActorRef = actorSystem.actorOf(OnlineUserActor.props())
 }
 
-class OnlineUserActor() extends Actor {
+class OnlineUserActor() extends Actor with Logging {
+  private case class AreaAccess(
+      instance: ActorRef,
+      user: ZetaIdentity,
+      areaName: String
+  )
+
+  private val onlineUsers: mutable.Set[AreaAccess] = mutable.Set()
+
   override def receive: Receive = {
-    case s: String => print(s)
+    case ClientOnline(id, area) =>
+      logger.info(s"Online: ${id.fullName} in $area")
+      onlineUsers.add(AreaAccess(sender(), id, area))
+      onlineClientsInArea(area).foreach(u => u ! onlineClientsInArea(id, area))
+    case ClientOffline(id, area) =>
+      logger.info(s"Offline: ${id.fullName} in $area")
+      onlineUsers.remove(AreaAccess(sender(), id, area))
+      onlineClientsInArea(area).foreach(u => u ! onlineClientsInArea(id, area))
   }
+
+  private def onlineClientsInArea(self: ZetaIdentity, area: String) =
+    AreaState(
+      onlineUsers.filter(_.areaName == area)
+        .filter(_.user.email != self.email)
+        .map(a => ClientInfo(a.user.fullName))
+        .toList.distinct
+    )
+
+  private def onlineClientsInArea(area: String): List[ActorRef] =
+    onlineUsers.filter(_.areaName == area)
+      .map(_.instance).toList.distinct
 }
 object OnlineUserActor {
+  case class ClientOnline(identity: ZetaIdentity, area: String)
+  case class ClientOffline(identity: ZetaIdentity, area: String)
+
+  case class ClientInfo(fullName: String)
+  case class AreaState(onlineClients: List[ClientInfo])
+
   def props(): Props = Props(new OnlineUserActor())
 }
--- webapp/src/code-editor/online-socket.js
+++ webapp/src/code-editor/online-socket.js
@@ -1,16 +1,17 @@
 export class OnlineSocket {
 
-    constructor() {
+    constructor(area) {
         const service = this;
+        this.area = area;
         try {
             this.webSocket = new WebSocket("ws://localhost:9000/socket");
             this.webSocket.onopen = function (openEvent) {
                 console.log("WebSocket OPEN: " + JSON.stringify(openEvent, null, 4));
-
                 service.pingOnlineStatus();
             };
-            this. webSocket.onclose = function (closeEvent) {
+            this.webSocket.onclose = function (closeEvent) {
                 console.log("WebSocket CLOSE: " + JSON.stringify(closeEvent, null, 4));
+                service.pingOfflineStatus();
             };
             this.webSocket.onerror = function (errorEvent) {
                 console.log("WebSocket ERROR: " + JSON.stringify(errorEvent, null, 4));
@@ -25,11 +26,18 @@ export class OnlineSocket {
     }
 
     pingOnlineStatus() {
-        if (this.webSocket.readyState !== WebSocket.OPEN) {
-            console.error("webSocket is not open: " + this.webSocket.readyState);
-            return;
-        }
-        const msg = "<-";
+        const msg = JSON.stringify({
+            name: "online",
+            value: this.area
+        });
+        this.webSocket.send(msg);
+    }
+
+    pingOfflineStatus() {
+        const msg = JSON.stringify({
+            name: "offline",
+            value: this.area
+        });
         this.webSocket.send(msg);
     }
 
