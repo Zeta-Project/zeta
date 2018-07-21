4f2849e52bfc500e781311790e0a38f46a3d1377
aullik <nicolaswehrle@gmx.net>
2018-07-21 23:50:46 +0200

    add GameModelGenerator

---
+++ api/codeGenerator/src/main/scala/de/htwg/zeta/codeGenerator/generation/model/GameModelGenerator.scala
@@ -0,0 +1,13 @@
+package de.htwg.zeta.codeGenerator.generation.model
+
+import de.htwg.zeta.codeGenerator.generation.model.html.GameEntityTemplate
+import de.htwg.zeta.codeGenerator.model.GeneratedFile
+import de.htwg.zeta.codeGenerator.model.GeneratedFolder
+
+object GameModelGenerator {
+  def generate(): GeneratedFolder = {
+    val cont = GameEntityTemplate().body
+    val file = GeneratedFile("GameEntity", GeneratedFile.scala, cont)
+    GeneratedFolder("gameEntity", List(file), Nil)
+  }
+}
