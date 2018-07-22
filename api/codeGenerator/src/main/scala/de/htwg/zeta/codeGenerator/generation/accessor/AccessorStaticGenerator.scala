b6cbf31a6fe377a9e825c0cf26d58a54be7b5d13
aullik <nicolaswehrle@gmx.net>
2018-07-22 21:07:25 +0200

    add AccessorStaticGenerator

---
+++ api/codeGenerator/src/main/scala/de/htwg/zeta/codeGenerator/generation/accessor/AccessorStaticGenerator.scala
@@ -0,0 +1,44 @@
+package de.htwg.zeta.codeGenerator.generation.accessor
+
+import de.htwg.zeta.codeGenerator.generation.accesor.html.InputValueAccessorTemplate
+import de.htwg.zeta.codeGenerator.generation.accesor.html.MapValueAccessorTemplate
+import de.htwg.zeta.codeGenerator.generation.accesor.html.OutputValueAccessorTemplate
+import de.htwg.zeta.codeGenerator.generation.accesor.html.ValueAccessorTemplate
+import de.htwg.zeta.codeGenerator.model.AnchorEnum
+import de.htwg.zeta.codeGenerator.model.AnchorEnum.Period
+import de.htwg.zeta.codeGenerator.model.AnchorEnum.Team
+import de.htwg.zeta.codeGenerator.model.GeneratedFile
+import de.htwg.zeta.codeGenerator.model.GeneratedFolder
+
+object AccessorStaticGenerator {
+  val valueAccessorPackage = "valueAccessor"
+
+  def generate(): GeneratedFolder = {
+    val perTypeFiles = List(Team, Period).flatMap(start => List(
+      generateValueAccessor(start),
+      generateInputValueAccessor(start),
+      generateOutputValueAccessor(start)
+    ))
+
+    val files = generateMapValueAccessor :: perTypeFiles
+
+    GeneratedFolder(valueAccessorPackage, files, Nil)
+  }
+
+
+  private def generateValueAccessor(start: AnchorEnum): GeneratedFile = {
+    GeneratedFile(s"${start}ValueAccessor", GeneratedFile.scala, ValueAccessorTemplate(start).body)
+  }
+  private def generateInputValueAccessor(start: AnchorEnum): GeneratedFile = {
+    GeneratedFile(s"${start}InputValueAccessor", GeneratedFile.scala, InputValueAccessorTemplate(start).body)
+  }
+  private def generateOutputValueAccessor(start: AnchorEnum): GeneratedFile = {
+    GeneratedFile(s"${start}OutputValueAccessor", GeneratedFile.scala, OutputValueAccessorTemplate(start).body)
+  }
+
+  private def generateMapValueAccessor(): GeneratedFile = {
+    GeneratedFile("MapValueAccessor", GeneratedFile.scala, MapValueAccessorTemplate().body)
+
+  }
+
+}
