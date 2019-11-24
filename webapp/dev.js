9304c411987cd5e8a1f2ff23aa6cdf102522c243
Manuele Lorusso <manuele.lorusso@gmail.com>
2019-11-24 13:54:46 +0100

    corrected license path removed unused config

--- webapp/dev.js
+++ webapp/dev.js
@@ -15,10 +15,6 @@ app.use('/app', polyserve.getApp({
   'entrypoint': path.join(__dirname, "src", "app.html")
 }));
 
-app.use('/yzeta', polyserve.getApp({
-  'entrypoint': path.join(__dirname, "src", "yzeta.html")
-}));
-
 app.use('/static', server.app);
 
 app.listen(8080, function () {
