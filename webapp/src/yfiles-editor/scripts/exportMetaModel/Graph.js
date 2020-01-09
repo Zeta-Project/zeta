d988f56f258b834077a8e9b2ac6bbb3c2c9a386f
Manuele Lorusso <manuele.lorusso@gmail.com>
2020-01-09 15:14:08 +0100

    code cleaning added first duplicateMethods validation function

--- webapp/src/yfiles-editor/scripts/exportMetaModel/Graph.js
+++ webapp/src/yfiles-editor/scripts/exportMetaModel/Graph.js
@@ -87,13 +87,12 @@ export default (function () {
 
     //Todo mCoreUtil need to check validity like above (getInputReferenceNames)
     Graph.prototype.getInputReferenceNames = function (node) {
-        /*if (this.graph.inDegree(node) > 0) {
+        if (this.graph.inDegree(node) > 0) {
             return this.graph.inEdgesAt(node).map(edge => {
                 let label = edge.labels.find(label => label !== undefined);
                 return label.text;
             })
-        } else*/
-        {
+        } else {
             return []
         }
     };
@@ -205,64 +204,32 @@ export default (function () {
         return duplicateAttributes;
     };
 
-
     /*
-     Convert a attribute type into json
-    */
-    // Graph.prototype.attributeTypeToJson = function (type) {
-    //     if (type === 'String' || type === 'Bool' || type === 'Int' || type === 'Double') {
-    //         return type;
-    //     } else {
-    //         return {
-    //             type: 'enum',
-    //             name: type
-    //         }
-    //     }
-    // };
-    //
-    // /*
-    //  Convert a attribute value into json
-    // */
-    // Graph.prototype.attributeValueToJson = function (type, value) {
-    //     switch (type) {
-    //         case 'Bool':
-    //             return {
-    //                 type: 'Bool',
-    //                 value: (value === 'true')
-    //             };
-    //         case 'Int':
-    //             if (!this.isNumeric(value)) {
-    //                 value = 0;
-    //             }
-    //             return {
-    //                 type: 'Int',
-    //                 value: parseInt(value)
-    //             };
-    //         case 'Double':
-    //             if (!this.isNumeric(value)) {
-    //                 value = 0.0;
-    //             }
-    //             return {
-    //                 type: 'Double',
-    //                 value: parseFloat(value)
-    //             };
-    //         case 'String':
-    //             return {
-    //                 type: 'String',
-    //                 value
-    //             };
-    //         default:
-    //             return {
-    //                 type: 'enum',
-    //                 enumName: type,
-    //                 valueName: value
-    //             };
-    //     }
-    // };
-    //
-    // Graph.prototype.isNumeric = function (n) {
-    //     return !isNaN(parseFloat(n)) && isFinite(n);
-    // };
+    Returns all methods which are assigned more than once inside an element.
+    Overloaded methods are valid.
+     */
+
+    Graph.prototype.getDuplicateNodeMethods = function () {
+        let duplicateMethods = [];
+        let methods = [];
+
+        this.getNodes().forEach(element => {
+            this.getNodeAttributes(element).forEach(attribute => {
+                if (methods.includes(attribute.name)) {
+                    duplicateMethods.push({
+                        nodeName: Graph.prototype.getNodeName(element),
+                        attributeName: attribute.name
+                    });
+                } else {
+                    methods.push(attribute.name)
+                }
+            })
+            // clear attributes array for new element
+            methods = [];
+        });
+
+        return duplicateMethods;
+    };
 
     /*
       Returns the sourceDeletionDeletesTarget value of the reference.
@@ -277,49 +244,6 @@ export default (function () {
     Graph.prototype.getTargetDeletionDeletesSource = function(reference) {
       return reference.attributes[Constants.field.TARGET_DELETION_DELETES_SOURCE] || false;
     };
-
-
-    Returns all input references of the element.
-
-  Graph.prototype.getInputReferenceNames = function(element) {
-    return this.graph.getConnectedLinks(element, {
-      inbound: true
-    }).filter(function(link) {
-      return mCoreUtil.isReference(link);
-    }).map(input =>
-      input.attributes.name
-    );
-  };
-
-
-
-       Returns the attributes of the cell.
-       Default value is parsed from String because the inspector can't display the correct input
-       based on the selected type, because the type is in a List
-       See: http://stackoverflow.com/questions/37742721/display-field-based-on-another-in-jointjs
-
-     Graph.prototype.getNodeAttributes = function(cell) {
-       let attributes, key, mAttributes, value, _i, _len, _ref;
-       mAttributes = [];
-       if (cell.attributes[Constants.field.ATTRIBUTES] != null) {
-         _ref = cell.attributes[Constants.field.ATTRIBUTES];
-         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-           attributes = _ref[_i];
-           mAttributes.push({});
-           for (key in attributes) {
-
-             if (key === 'default') {
-               mAttributes[mAttributes.length - 1][key] = this.attributeValueToJson(attributes.typ, attributes[key]);
-             } else if(key === 'typ') {
-               mAttributes[mAttributes.length - 1]['type'] = this.attributeTypeToJson(attributes.typ);
-             } else {
-               mAttributes[mAttributes.length - 1][key] = attributes[key];
-             }
-           }
-         }
-       }
-       return mAttributes;
-     };
 
   */
 
