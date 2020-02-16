81eb21b9841c2fcc84123f39f05596427dba1584
Manuele Lorusso <manuele.lorusso@gmail.com>
2020-02-16 10:03:00 +0100

    changed the way of concept loading in zeta

--- webapp/src/yfiles-editor/concept-editor.js
+++ webapp/src/yfiles-editor/concept-editor.js
@@ -1,5 +1,11 @@
 import {YFilesZeta} from "./scripts/app";
+import {ZetaApiWrapper} from "./scripts/ZetaApiWrapper";
 
 let loadedMetaModel = window.loadedMetaModel;
+
+const zetaApiWrapper = new ZetaApiWrapper();
+zetaApiWrapper.getConceptDefinition(loadedMetaModel.uuid).then(data => {
+        loadedMetaModel.concept = data
+    });
 
 new YFilesZeta(loadedMetaModel);
