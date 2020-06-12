67505bb400cc9af655a7d51c04ee102047a70902
patrick sterk <patrick.sterk@protonmail.com>
2020-06-12 20:48:32 +0200

    quick fix strage bug for transient token cache Actor

--- api/server/test/de/htwg/zeta/server/actor/TransientTokenCacheActorSpec.scala
+++ api/server/test/de/htwg/zeta/server/actor/TransientTokenCacheActorSpec.scala
@@ -2,13 +2,11 @@ package de.htwg.zeta.server.actor
 
 import java.util.concurrent.TimeUnit
 import java.util.UUID
-
 import scala.concurrent.duration.Duration
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Failure
 import scala.util.Success
-
 import akka.actor.ActorSystem
 import akka.util.Timeout
 import de.htwg.zeta.server.model.TokenCache
@@ -22,7 +20,7 @@ class TransientTokenCacheActorSpec extends AnyFlatSpec with Matchers {
   val timeout: Timeout = Duration(1, TimeUnit.MINUTES)
   var createActor: TokenCache = new TokenCacheActor(system,timeout)
   var uid : UUID = UUID.randomUUID()
-  var userToken : UUID = _
+  var userToken : Option[UUID] = None
 
   "ActorTokenCache" should "create instances" in {
     createActor.getClass shouldBe classOf[TokenCacheActor]
@@ -31,14 +29,19 @@ class TransientTokenCacheActorSpec extends AnyFlatSpec with Matchers {
   it should "create uid" in {
     val future = createActor.create(uid)
     future.onComplete{
-        case Success(u) => userToken = u
-        case _ => ()
+        case Success(u) => {
+          userToken = Some(u)
+          userToken.isEmpty shouldBe false
+        }
+        case _ => {
+          false shouldBe true
+        }
     }
     Await.result(future,Duration.Inf)
-    userToken != null shouldBe true
   }
+
   it should "read values" in {
-    val future = createActor.read(userToken)
+    val future = createActor.read(userToken.get)
     future.onComplete{
       case Success(u) => u shouldBe uid
       case _ => ()
@@ -46,19 +49,25 @@ class TransientTokenCacheActorSpec extends AnyFlatSpec with Matchers {
     Await.result(future,Duration.Inf)
 
   }
+
   it should "delete values" in {
     var deleteResult = false
     val future = createActor.delete(uid)
-      .map { _: Unit => true }
       .andThen {
         case Success(_) => deleteResult = true
         case _ => ()
       }
     Await.result(future, Duration.Inf)
     deleteResult shouldBe true
-    createActor.read(uid).andThen {
-        case Failure(_) => deleteResult = true
-        case _ => ()
-      }
+    var notFound = false
+    val futureNot = createActor.read(uid)
+    futureNot.onComplete {
+      case Failure(_) => notFound = true
+      case _ => notFound = false
+    }
+    intercept[Exception] {
+      Await.result(futureNot, Duration.Inf)
+      notFound shouldBe true
+    }
   }
 }
