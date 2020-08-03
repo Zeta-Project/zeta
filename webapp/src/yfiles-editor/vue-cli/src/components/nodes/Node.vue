7706ce7bce7d5478d487a4a2de716d9de1ceefbc
Fabian Götz <fabian_goetz@outlook.com>
2020-08-03 16:29:00 +0200

    updated color of nodes

--- webapp/src/yfiles-editor/vue-cli/src/components/nodes/Node.vue
+++ webapp/src/yfiles-editor/vue-cli/src/components/nodes/Node.vue
@@ -10,8 +10,8 @@
                 <g>
                     <rect x="0" y="50" width="100%" :height=" 30 " />
                     <g>
-                        <image v-if="attributes_open==false" x="0" y="50" v-on:click="attributes_open=change_status(attributes_open)" xlink:href="../../assets/triangle.svg" />
-                        <image v-if="attributes_open==true"  x="0" y="50" v-on:click="attributes_open=change_status(attributes_open)" xlink:href="../../assets/triangle_90deg_rotated.svg" />
+                        <image v-if="!attributes_open" x="0" y="50" v-on:click="attributes_open=change_status(attributes_open)" xlink:href="../../assets/triangle.svg" />
+                        <image v-if="attributes_open"  x="0" y="50" v-on:click="attributes_open=change_status(attributes_open)" xlink:href="../../assets/triangle_90deg_rotated.svg" />
                     </g>
                     <text x="20" y="70" :style="{ fontSize:'16px', fill: 'black' }" >Attributes:</text>
                     <image :x="layout.width - 22" y="55" width="18" xlink:href="../../assets/add-sign.svg" v-on:click="()=>methods.addAttributeToNode(tag, 'default')"/>
@@ -38,8 +38,8 @@
                 <g>
                     <rect x="1" :y="82 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" width="100%" :height="30" />              
                     <g v-on:click="operations_open=change_status(operations_open)">
-                        <image v-if="operations_open==false" x="0" :y="82 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" xlink:href="../../assets/triangle.svg" />
-                        <image v-if="operations_open==true"  x="0" :y="82 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" xlink:href="../../assets/triangle_90deg_rotated.svg" />
+                        <image v-if="!operations_open" x="0" :y="82 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" xlink:href="../../assets/triangle.svg" />
+                        <image v-if="operations_open"  x="0" :y="82 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" xlink:href="../../assets/triangle_90deg_rotated.svg" />
                     </g>
                     <text x="20" :y="102 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" :style="{  fontSize: '16px', fill: 'black' }" >Operation:</text>
                     <image :x="layout.width - 22" :y="87 + 30 * (attributes_open ? Object.keys(tag.attributes).length : 0)" width="18" xlink:href="../../assets/add-sign.svg" v-on:click="()=>methods.addOperationToNode(tag, 'default')"/>
