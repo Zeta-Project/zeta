9ca083efc227f3faabf7bfc04be5e3cce8064885
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-11-03 14:56:07 +0100

    added test

---
+++ frontend/src/components/zetalayout/ZetaLayout.test.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import ZetaLayout from 'ZetaLayout.vue'
+
+describe('ZetaLayout.vue', () => {
+    it('init test', () => {
+        const wrapper = shallowMount(ZetaLayout)
+    })
+})
