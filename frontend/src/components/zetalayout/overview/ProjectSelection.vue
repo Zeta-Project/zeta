c90f458cad62fef7b2b74dbdc2a7a8998f36db2b
Jonas Wirth <44641733+wijon@users.noreply.github.com>
2020-11-30 22:19:14 +0100

    Start refactoring ProjectSelection to make use of Vuetify (WIP)

--- frontend/src/components/zetalayout/overview/ProjectSelection.vue
+++ frontend/src/components/zetalayout/overview/ProjectSelection.vue
@@ -1,91 +1,52 @@
 <template>
   <div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <strong>Projects</strong>
-      </div>
+    <v-card>
+      <v-card-title>
+        Projects
+      </v-card-title>
 
-      <div class="list-group" v-if="metaModels !== undefined && metaModels.length">
-
-        <div v-for="metamodel in metaModels" v-bind:key="metamodel.id">
-<!--          <ProjectSelectionRow-->
-<!--              v-bind:id="metamodel.id"-->
-<!--              v-bind:name="metamodel.name"-->
-<!--              v-bind:is-selected="gdslProject && metamodel.id === gdslProject.id"-->
-<!--          />-->
-          <ProjectSelectionRow
-                        v-bind:id="metamodel.id"
-                        v-bind:name="metamodel.name"
-                        v-bind:is-selected="gdslProject && metamodel.id === gdslProject.id" />
-<!--        </div>-->
-        </div>
-
-      </div>
-
-      <div class="panel-body" v-else>
+      <v-card-text v-if="!metaModels || metaModels.length === 0">
         There are no projects.
+      </v-card-text>
+
+      <v-list v-else>
+        <v-list-item-group>
+          <v-list-item
+              v-for="(metamodel, i) in metaModels"
+              :key="i">
+
+            <ProjectSelectionRow
+                v-bind:id="metamodel.id"
+                v-bind:name="metamodel.name"
+                v-bind:is-selected="gdslProject && metamodel.id === gdslProject.id"/>
+
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+      <div class="ma-2">
+        <v-text-field
+            id="inputProjectName"
+            v-model="inputProjectName"
+            :append-icon="'mdi-plus-box'"
+            :append-outer-icon="'mdi-import'"
+            outlined
+            clearable
+            label="New project name"
+            type="text"
+            @click:append="createProject"
+            @click:append-outer="toggleDialog"
+            v-on:keyup.enter="createProject"/>
       </div>
+    </v-card>
 
-      <div class="panel-footer">
-        <form>
-          <div class="input-group">
-            <input v-model="inputProjectName" v-on:keyup.enter="createProject" type="text" class="form-control"
-                   id="inputProjectName" placeholder="New project name" autocomplete="off">
-            <span class="input-group-btn">
-                <button v-on:click="createProject" type=button id="btnCreateMetaModel" class="btn btn-default"
-                        data-toggle="#tooltip" title="Create project">
-                  <span class="glyphicon glyphicon-plus" aria-hidden=true></span>
-                </button>
-              <!-- launch import modal -->
-                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#importModal"
-                        title="Import project">
-                  <span class="glyphicon glyphicon-import" aria-hidden=true></span>
-                </button>
-              </span>
-          </div>
-          <div>
-
-
-            <!-- import modal -->
-            <div class="modal fade" id="importModal" tabindex="-1" role="dialog" aria-labelledby="importModalLabel"
-                 aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header modal-header-info">
-                    <span class="modal-title" id="importModalLabel">Import project</span>
-                    <button id="close-import-modal" type="button" class="close" data-dismiss="modal"
-                            aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <div class="form-group">
-                      <!-- drop area -->
-                      <input v-on:change="onFileChange" ref="file" type="file" accept=".zeta" name="file" id="file">
-                      <!-- Drag and Drop container-->
-                      <div v-on:click="openFileManager" v-on:drop="dropFile" class="upload-area" id="uploadfile">
-                        <span id="uploadtext">{{ uploadText }}</span>
-                      </div>
-                    </div>
-                    <input v-model="importProjectName" type="text" class="form-control" id="importProjectName"
-                           placeholder="New Project Name" autocomplete="off">
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    <button v-on:click="importProject" id="start-import-btn" type="button" class="btn btn-info"
-                            data-dismiss="modal" :disabled="!(isValidZetaProjectFile() && isValidProjectName())">
-                      Import
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- end import modal -->
-
-        </form>
-      </div>
-    </div>
+    <v-dialog v-model="showDialog" max-width="500px">
+      <v-card>
+        <v-card-actions>
+          <v-btn color="primary" @click.stop="toggleDialog">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -104,10 +65,14 @@ export default {
       inputProjectName: "",
       uploadText: "Drag and Drop .zeta file here...",
       importProjectName: "",
-      file: null
+      file: null,
+      showDialog: false
     }
   },
   methods: {
+    toggleDialog() {
+      this.showDialog = !this.showDialog;
+    },
     createProject() {
       ProjectUtils.createProject(this.inputProjectName)
     },
