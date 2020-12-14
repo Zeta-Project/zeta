febda9fdc3debe8367182195d40458581a8101b1
nistephan <nicolai.stephan@gmx.net>
2020-12-14 16:58:42 +0100

    replaced bootstrap in SignIn.vue

--- frontend/src/components/accountlayout/signin/SignIn.vue
+++ frontend/src/components/accountlayout/signin/SignIn.vue
@@ -1,41 +1,51 @@
 <template>
-  <fieldset class="col-md-6 col-md-offset-3">
-    <div>
-      <form @submit.prevent="login">
-        <legend>Sign in with your credentials</legend>
-        <div class="form-group">
-          <input required v-model="username" type="email" placeholder="Email" class="form-control input-lg"/>
-        </div>
-        <div class="form-group">
-          <input required v-model="password" type="password" placeholder="password"
-                 class="form-control input-lg"/>
-        </div>
-        <div class="form-group">
-          <div class="checkbox">
-            <label for="rememberMe">
-              <input type="checkbox" id="rememberMe" value="true" checked="true" v-model="rememberMe">
-              Remember my login on this computer
-            </label>
-          </div>
-        </div>
-        <div class="form-group">
-          <button id="submit" type="submit" value="submit" class="btn btn-lg btn-primary btn-block">Sign In</button>
-        </div>
-        <div>
-          <p class="not-a-member">Not a member?
-            <router-link to="/account/signUp">Sign Up Now</router-link> |
-            <router-link to="/account/password/forgot">Forgot your password?</router-link>
-          </p>
-        </div>
-        <div>
-          <p class="disclaimer-links">
-            <a href="https://www.htwg-konstanz.de/en/info/disclaimer/" target="_blank">Disclaimer</a> &
-            <a href="https://www.htwg-konstanz.de/en/info/imprint/" target="_blank">Imprint</a>
-          </p>
-        </div>
-      </form>
-    </div>
-  </fieldset>
+  <v-container class="col-md-4 col-md-offset-3">
+    <v-form
+        ref="form"
+        @submit="validate"
+        lazy-validation
+    >
+      <p class="font-weight-bold">
+        Sign in with your credentials
+      </p>
+      <v-text-field
+          v-model="username"
+          :rules="emailRules"
+          label="Email"
+          required
+      ></v-text-field>
+      <v-text-field
+          v-model="password"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPassword ? 'text' : 'password'"
+          @click:append="showPassword = !showPassword"
+          :rules="passwordRules"
+          label="Password"
+          required
+      ></v-text-field>
+      <v-checkbox
+          v-model="rememberMe"
+          label="Remember my login on this computer"
+      ></v-checkbox>
+      <v-btn
+          type="submit"
+          @submit="validate"
+          block
+          color="primary"
+      >
+        Sign In
+      </v-btn>
+      <br></br>
+      <p class="not-a-member">Not a member?
+        <router-link to="/account/signUp">Sign Up Now</router-link> |
+        <router-link to="/account/password/forgot">Forgot your password?</router-link>
+      </p>
+      <p class="disclaimer-links">
+        <a href="https://www.htwg-konstanz.de/en/info/disclaimer/" target="_blank">Disclaimer</a> &
+        <a href="https://www.htwg-konstanz.de/en/info/imprint/" target="_blank">Imprint</a>
+      </p>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
@@ -50,7 +60,15 @@ export default {
     return {
       username: "",
       password: "",
-      rememberMe: true
+      rememberMe: true,
+      emailRules: [
+        v => !!v || 'Email is required',
+        v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ],
+      showPassword: false
     }
   },
   methods: {
@@ -59,11 +77,11 @@ export default {
       this.$store.dispatch(AUTH_REQUEST, {username, password, rememberMe}).then(() => {
         this.$router.push({ path: '/'}).catch(err => {})
       }).catch(reason => EventBus.$emit('errorMessage', reason))
-    }
+    },
+    validate () {
+      if(this.$refs.form.validate())
+        this.login()
+    },
   }
 }
 </script>
-
-<style>
-
-</style>
