41bd9b7a44ec7ba9a8cebe95deaf733f48a93b16
nistephan <nicolai.stephan@gmx.net>
2021-04-16 12:37:16 +0200

    changed petri net, check if we can create an edge depending on shape dsl

--- frontend/src/components/zetalayout/model/model-editor/model/utils/NodeCandidateProvider.js
+++ frontend/src/components/zetalayout/model/model-editor/model/utils/NodeCandidateProvider.js
@@ -1,57 +1,34 @@
 import {
     DefaultPortCandidate,
     FreeNodePortLocationModel,
-    IInputModeContext,
-    INode,
     List,
     PortCandidateProviderBase,
     PortCandidateValidity
-  } from 'yfiles'
-  
+} from 'yfiles'
+
 
 /** Please refer to https://github.com/yWorks/yfiles-for-html-demos/tree/master/demos/input/portcandidateprovider for implementation details */
 export default class NodeCandidateProvider extends PortCandidateProviderBase {
 
-    constructor(node) {
-        console.log("creating NodeCadidate")
+    constructor(node, target) {
         super()
         this.node = node
+        this.target = target
     }
-    
+
     getPortCandidates(context) {
-        // TODO:
-        // 1. get the current node name
-        // 2. get the target of the current node name
-        // 3. set item validity based on that
-
-        console.log("context: ");
-        console.log(context);
-
-        console.log("node");
-        console.log(this.node);
-
         const candidates = new List()
         const graph = context.graph
         // Create the candidate for each port
         if (graph !== null) {
-          this.node.ports.forEach(port => {
-            const portCandidate = new DefaultPortCandidate(port)
-            portCandidate.validity =
-              graph.degree(port) === 0 ? PortCandidateValidity.VALID : PortCandidateValidity.INVALID
-            candidates.add(portCandidate)
-          })
-        }
-        // If no candidates have been created so far, create a single invalid candidate as fallback
-        if (candidates.size === 0) {
-          const item = new DefaultPortCandidate(
-            this.node,
-            FreeNodePortLocationModel.NODE_CENTER_ANCHORED
-          )
-          item.validity = PortCandidateValidity.INVALID
-          candidates.add(item)
-        }
-    
-        return candidates
-      }
+            const item = new DefaultPortCandidate(
+                this.node,
+                FreeNodePortLocationModel.NODE_CENTER_ANCHORED
+            )
+            item.validity = this.node.tag.className == this.target ? PortCandidateValidity.VALID : PortCandidateValidity.INVALID
 
+            candidates.add(item)
+        }
+        return candidates
+    }
 }
